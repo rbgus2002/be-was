bf6d1eb801977f038232b6e162ddabc53794c2de
dydwo0740 <dydwo0740@naver.com>
2023-07-10 17:09:48 +0900

    end Mission1

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -11,7 +11,7 @@ import org.slf4j.LoggerFactory;
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
-    private Socket connection;
+    private     Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
