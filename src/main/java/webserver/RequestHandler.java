a5ac2f1357967bfa8656b044eafae038a99c6f63
ajsthfldu <ajsthfldu@outlook.com>
2023-07-10 11:24:17 +0900

    implement response index.html

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,9 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,7 +24,8 @@ public class RequestHandler implements Runnable {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+
+            byte[] body = Files.readAllBytes(Paths.get("/Users/sunshine/Projects/be-was/src/main/resources/templates/index.html"));
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
