66d5ee5171704612de01981775d977abebfb6929
dohyeon-han <limited.hyeon@gmail.com>
2023-07-10 13:39:28 +0900

    feat: index.html 응답

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,18 +1,15 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.Socket;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
-    private Socket connection;
+    private final Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -22,10 +19,20 @@ public class RequestHandler implements Runnable {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+        try (InputStream in = connection.getInputStream();
+             BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+             OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            String requestLine = reader.readLine();
+            String[] requestParts = requestLine.split(" ");
+            String requestUrl = requestParts[1];
+
+            // 요청한 파일 읽기
+            InputStream fileInputStream = getClass().getResourceAsStream("/templates/" + requestUrl);
+            assert fileInputStream != null;
+            byte[] body = fileInputStream.readAllBytes();
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
