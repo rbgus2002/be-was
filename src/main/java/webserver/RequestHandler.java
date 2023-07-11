9a75561055cdcd9669d2f5a9e7f89654d6326e22
kimahhh <kimahyunn132@gmail.com>
2023-07-11 13:37:33 +0900

    Feat: 정적인 html 파일 응답 및 HTTP Request 내용 출력

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,17 +1,15 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import utils.FileIOUtils;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
-
     private Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
@@ -23,9 +21,17 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            // 사용자 요청에 대한 처리 구현
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+            String line = br.readLine();
+            logger.debug("request line: {}", line);
+            FileIOUtils fileIOUtils = new FileIOUtils();
+            byte[] body = fileIOUtils.loadTemplatesFromPath(line);
+            while (!line.equals("")) {
+                line = br.readLine();
+                logger.debug("header: {}", line);
+            }
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
