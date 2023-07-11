bb4f585232144ddfdec7dfc998dd7e55a67ed359
dydwo0740 <dydwo0740@naver.com>
2023-07-11 14:04:54 +0900

    finish

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -6,6 +6,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.concurrent.Callable;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,7 +29,15 @@ public class RequestHandler implements Runnable {
             String line = br.readLine();
             logger.debug("line = {}", line);
             String[] tokens = line.split(" ");
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            if(tokens[1].startsWith("/user/create")){
+                String[] info = tokens[1].split("\\?");
+                String[] userInfo = info[1].split("&");
+                String userId = userInfo[0].substring(userInfo[0].indexOf('=') + 1, userInfo[0].length());
+                String password = userInfo[1].substring(userInfo[1].indexOf('=') + 1, userInfo[1].length());
+                String name = userInfo[2].substring(userInfo[2].indexOf('=') + 1, userInfo[2].length());
+                String email = userInfo[3].substring(userInfo[3].indexOf('=') + 1, userInfo[3].length());
+                User user = new User(userId, password, name, email);
+            }
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = Files.readAllBytes(new File("/Users/dydwo0740/Desktop/develop/be-was/src/main/resources/templates" + tokens[1]).toPath());
             response200Header(dos, body.length);
