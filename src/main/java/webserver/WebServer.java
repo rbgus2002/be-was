f9965adcc32d92e9b4b04efcc27164626e3c198d
dohyeon-han <limited.hyeon@gmail.com>
2023-07-11 14:02:03 +0900

    fix: 스레드 풀을 한 번 생성하도록 수정

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -3,6 +3,7 @@ package webserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
@@ -20,6 +21,8 @@ public class WebServer {
             port = Integer.parseInt(args[0]);
         }
 
+        ExecutorService executor = Executors.newWorkStealingPool();
+
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
@@ -27,10 +30,13 @@ public class WebServer {
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                ExecutorService executor = Executors.newWorkStealingPool();
                 executor.execute(new RequestHandler(connection));
                 executor.shutdown();
             }
+        } catch (IOException e) {
+            logger.error("서버가 동작하는 중 에러가 발생했습니다: {}", e.getMessage());
+        } finally {
+            executor.shutdownNow();
         }
     }
 }
