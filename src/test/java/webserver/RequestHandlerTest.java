8b93c28804058a8ed4d8dc9e2891d9935d761b52
jst0951 <jst0951@gmail.com>
2023-07-12 17:41:26 +0900

    RequestHandler에 대한 Test로 변경

---
+++ src/test/java/webserver/RequestHandlerTest.java
@@ -0,0 +1,21 @@
+package webserver;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.DataOutputStream;
+
+class RequestHandlerTest {
+    @BeforeAll
+    public void setup() throws Exception {
+        WebServer.main(new String[]{});
+    }
+
+    @Test
+    @DisplayName("유저가 회원가입하면 Database에 계정이 생성된다")
+    public void signUp() {
+        // Given
+        System.out.println("hello!");
+    }
+}
