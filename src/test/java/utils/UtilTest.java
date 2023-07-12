5fcb3e885cb91562319dac01129451ab92165ca1
Jaehong Kim <kjhonggg95@gmail.com>
2023-07-12 10:52:28 +0900

    test: Request Line 으로부터 Path 를 추출하는 util 메서드

---
+++ src/test/java/utils/UtilTest.java
@@ -0,0 +1,21 @@
+package utils;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static utils.Util.getPathFromRequestLine;
+
+class UtilTest {
+
+    @Test
+    @DisplayName("Request line 으로부터 path 를 분리한다")
+    void getPath() {
+        String requestLine1 = "GET /favicon.ico HTTP/1.1";
+        Assertions.assertThat(getPathFromRequestLine(requestLine1)).isEqualTo("/favicon.ico");
+
+        String requestLine2 = "GET /index.html HTTP/1.1";
+        Assertions.assertThat(getPathFromRequestLine(requestLine2)).isEqualTo("/index.html");
+    }
+}
