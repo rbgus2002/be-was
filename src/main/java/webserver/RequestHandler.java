364913f0ffe15a8ac2b411d4626679f451986b38
rbgus2002 <rbgus2002@naver.com>
2023-07-13 10:20:36 +0900

    add : RequestHandler : 서버로 들어오는 HTTP 내용 출력 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,9 +1,6 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 
 import org.slf4j.Logger;
@@ -23,13 +20,25 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            // 요구사항 : 서버로 들어오는 요청을 log.debug를 이용해 출력한다
+            printRequestOfClient(in);
+
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             logger.error(e.getMessage());
+        }
+    }
+
+    private void printRequestOfClient(InputStream in) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        String line = br.readLine();
+        logger.debug("request = {}", line);
+        while (!line.equals("")){
+            line = br.readLine();
+            logger.debug("{}", line);
         }
     }
 
