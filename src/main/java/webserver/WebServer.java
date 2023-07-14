4aca05ae8049378ac2933254f39af0cdfa17c582
Jaehong Kim <kjhonggg95@gmail.com>
2023-07-14 13:44:14 +0900

    refactor: 스레드 풀 크기 상수로 지정

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
+    private static final int THREAD_NUMBER = 10;
 
     public static void main(String args[]) throws Exception {
         int port = 0;
@@ -20,7 +21,7 @@ public class WebServer {
             port = Integer.parseInt(args[0]);
         }
 
-        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        ExecutorService executorService = Executors.newFixedThreadPool(THREAD_NUMBER);
 
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
