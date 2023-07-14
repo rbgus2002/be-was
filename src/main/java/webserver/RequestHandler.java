3128d6a7c64982d20d4b23e83794e18519f0de9b
kwyoohae <gocksdl7979@gmail.com>
2023-07-14 12:51:06 +0900

    feat: HTTP Request Header의 값 출력

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,15 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.Buffer;
+
+import javax.xml.crypto.Data;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,7 +28,15 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            final InputStreamReader inputStreamReader = new InputStreamReader(in);
+            final BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            String input = bufferedReader.readLine();
+            while (input != null && !input.isBlank()) {
+                logger.debug("request = {}", input);
+                input = bufferedReader.readLine();
+            }
+
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
