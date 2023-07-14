6301b38d1007c6fdad7a9c4114bc67d16eef3077
jst0951 <jst0951@gmail.com>
2023-07-14 11:52:19 +0900

    입출력 각각 메서드가 처리하도록 구조화, Request 모델 이용

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -17,6 +17,9 @@ import org.slf4j.LoggerFactory;
 
 import static model.User.*;
 
+import webserver.model.Request;
+import webserver.model.Request.Method;
+
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
     private static final String STATIC_FILEPATH = "./src/main/resources/static";
@@ -29,49 +32,77 @@ public class RequestHandler implements Runnable {
     }
 
     public void run() {
-        logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
+        logger.debug("New Client Connect! Connected IP : {}, Port : {}",
+                connection.getInetAddress(),
                 connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+        try {
             // Request
-            // http 메세지를 저장하기 위한 StringBuilder 생성
-            StringBuilder sb = new StringBuilder();
-            BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-            String line = URLDecoder.decode(br.readLine(), StandardCharsets.UTF_8);
-            logger.debug("request: {}", line);
-            // 경로 parsing
-            String route = line.split(" ")[1];
-            logger.debug("route: {}", route);
-            // 나머지 확인
-            while(!line.equals("")) {
-                line = URLDecoder.decode(br.readLine(), StandardCharsets.UTF_8);
-                sb.append(line);
-                sb.append("\r\n");
-                logger.debug("header: {}", line);
-            }
+            Request request = parseRequest(connection);
 
             // Response
-            DataOutputStream dos = new DataOutputStream(out);
-            routeRequest(route, dos);
-
+            routeRequest(request, connection);
         } catch (Exception e) {
             logger.error(e.getMessage());
         }
-    }
-
-    private void routeRequest(String route, DataOutputStream dos) throws Exception {
-        if (route.endsWith(".html")) {
-            serveStaticFile(route, dos);
-        }
-        else if(route.startsWith("/user/create")) {
-            Map<String, String> queryParameterMap = parseQueryParameter(route);
-            userSingUp(queryParameterMap);
+        finally {
+            try {
+                connection.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
+    public Request parseRequest(Socket connection) throws IOException, IllegalArgumentException {
+        InputStream in = connection.getInputStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+        /*
+         StartLine
+         */
+        String[] tokens = readSingleHTTPLine(br).split(" ");
+
+        Method method = Method.valueOf(tokens[0]);
+        String targetUri = tokens[1];
+        String version = tokens[2].split("/")[1];
+
+        Map<String, String> queryParameterMap = parseQueryParameter(targetUri);
+
+        /*
+         Headers
+         */
+        Map<String, String> headerMap = new HashMap<>();
+        String line = readSingleHTTPLine(br);
+        while(!line.equals("")) {
+            tokens = line.split(": ");
+            headerMap.put(tokens[0], tokens[1]);
+            line = readSingleHTTPLine(br);
+        }
+
+        /*
+         Body
+         */
+        StringBuilder sb = new StringBuilder();
+        if(method == Method.PUT || method == Method.POST) {
+            line = readSingleHTTPLine(br);
+            while(!line.equals("")) {
+                sb.append(line);
+            }
+        }
+        String body = sb.toString();
+
+        return new Request(method, version, targetUri, queryParameterMap, headerMap, body);
+    }
+    public String readSingleHTTPLine(BufferedReader br) throws IOException {
+        return URLDecoder.decode(br.readLine(), StandardCharsets.UTF_8);
+    }
     private Map<String, String> parseQueryParameter(String route) {
         // ?를 기준으로 쿼리 스트링 분할
-        String queryString = route.split("\\?")[1];
+        String[] tokens = route.split("\\?");
+        if(tokens.length < 2) {
+            return null;
+        }
+        String queryString = tokens[1];
         // &를 기준으로 파라미터 분할
         String[] queryParameterList = queryString.split("&");
         // Map에 key-value 저장
@@ -84,14 +115,18 @@ public class RequestHandler implements Runnable {
         return queryParameterMap;
     }
 
-    private void userSingUp(Map<String, String> queryParameterMap) {
-        // User 객체 생성
-        User user = new User(queryParameterMap.get(USERID),
-                queryParameterMap.get(PASSWORD),
-                queryParameterMap.get(NAME),
-                queryParameterMap.get(EMAIL));
-        // DB 저장
-        Database.addUser(user);
+    public void routeRequest(Request request, Socket connection) throws Exception {
+        String targetUri = request.getTargetUri();
+
+        OutputStream out = connection.getOutputStream();
+        DataOutputStream dos = new DataOutputStream(out);
+
+        if (targetUri.endsWith(".html")) {
+            serveStaticFile(targetUri, dos);
+        }
+        else if(targetUri.startsWith("/user/create")) {
+            userSingUp(request.getQueryParameterMap());
+        }
     }
 
     private void serveStaticFile(String route, DataOutputStream dos) throws IOException {
@@ -106,6 +141,15 @@ public class RequestHandler implements Runnable {
         response200Header(dos, body.length);
         responseBody(dos, body);
     }
+    public void userSingUp(Map<String, String> queryParameterMap) throws NullPointerException {
+        // User 객체 생성
+        User user = new User(queryParameterMap.get(USERID),
+                queryParameterMap.get(PASSWORD),
+                queryParameterMap.get(NAME),
+                queryParameterMap.get(EMAIL));
+        // DB 저장
+        Database.addUser(user);
+    }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
@@ -117,7 +161,6 @@ public class RequestHandler implements Runnable {
             logger.error(e.getMessage());
         }
     }
-
     private void responseBody(DataOutputStream dos, byte[] body) {
         try {
             dos.write(body, 0, body.length);
--- src/test/java/webserver/RequestHandlerTest.java
+++ src/test/java/webserver/RequestHandlerTest.java
@@ -1,19 +1,31 @@
 package webserver;
 
+import db.Database;
+import model.User;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import webserver.model.Request;
+
+import static model.User.USERID;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.Socket;
+import java.util.Map;
 
 class RequestHandlerTest {
-    @BeforeAll
-    public void setup() throws Exception {
-        WebServer.main(new String[]{});
-    }
-
     @Test
     @DisplayName("유저가 회원가입하면 Database에 계정이 생성된다")
-    public void signUp() {
+    public void signUp() throws Exception {
         // Given
-        System.out.println("hello!");
+        String targetUri = "/user/create?userId=jst0951&password=password&name=정성태&email=jst0951@naver.com";
+        Request request = new Request(null, null, targetUri, null, null, null);
+        RequestHandler requestHandler = new RequestHandler(new Socket());
+
+        // When
+        requestHandler.routeRequest(request, new Socket());
+
+        // Then
+        assertThat(Database.findAll().size()).isEqualTo(1);
     }
 }
