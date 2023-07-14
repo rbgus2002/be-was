4057e6444ef29c7bf2d55972686fa77221da4ebd
Park Seonmi <sm000117@naver.com>
2023-07-14 12:53:28 +0900

    feat: Thread를 concurrent 패키지의 Executor로 변경

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -2,6 +2,9 @@ package webserver;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,10 +27,14 @@ public class WebServer {
 
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
+            ExecutorService executor = Executors.newFixedThreadPool(5);
             while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection));
-                thread.start();
+                executor.submit(new RequestHandler(connection));
             }
+            executor.shutdown();
+        } catch(Exception e) {
+            logger.debug(e.getMessage());
         }
+
     }
 }
