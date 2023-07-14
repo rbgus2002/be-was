c763921c1a206dba42e87935618a562ccb4178e4
Jaehong Kim <kjhonggg95@gmail.com>
2023-07-14 13:44:52 +0900

    test: request line 으로부터 method, url(path), 파라미터, 버전 추출

---
+++ src/test/java/utils/HttpRequestTest.java
@@ -0,0 +1,57 @@
+package utils;
+
+import http.HttpRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+import static utils.Parser.parseUrlFromRequestLine;
+
+class HttpRequestTest {
+
+    private final String requestLine = "GET /create?userId=jaehong&password=123 HTTP/1.1";
+    private HttpRequest httpRequest;
+
+    @BeforeEach
+    void setup() {
+        httpRequest = new HttpRequest(requestLine);
+    }
+
+    @Test
+    @DisplayName("Request line 으로부터 method 를 분리한다")
+    void getMethod() {
+        String method = httpRequest.getMethod();
+        assertThat(method).isEqualTo("GET");
+    }
+
+    @Test
+    @DisplayName("Request line 으로부터 url 를 분리한다")
+    void getUrl() {
+        String url = parseUrlFromRequestLine(requestLine);
+        assertThat(url).isEqualTo("/create?userId=jaehong&password=123");
+    }
+
+    @Test
+    @DisplayName("Request line 으로부터 path 를 분리한다")
+    void getPath() {
+        String path = httpRequest.getPath();
+        assertThat(path).isEqualTo("/create");
+    }
+
+    @Test
+    @DisplayName("Request line 으로부터 파라미터를 분리한다")
+    void getParams() {
+        Map<String, String> params = httpRequest.getParams();
+        assertThat(params).hasSize(2).contains(entry("userId", "jaehong"), entry("password", "123"));
+    }
+
+    @Test
+    @DisplayName("Request line 으로부터 HTTP version 을 분리한다")
+    void getVersion() {
+        String version = httpRequest.getVersion();
+        assertThat(version).isEqualTo("HTTP/1.1");
+    }
+}
