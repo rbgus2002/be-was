b1839a7043a02780dc4bc9b2a7600545bbdaef68
plusjob70 <plusjob70@naver.com>
2023-07-16 18:06:57 +0900

    feat(HttpRequest): params 필드 추가
    - GET 방식의 파라미터 처리를 위해 params 필드 추가

--- src/main/java/utils/HttpRequest.java
+++ src/main/java/utils/HttpRequest.java
@@ -12,49 +12,99 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 import static utils.StringUtils.NEW_LINE;
 
 public class HttpRequest {
-    private final String method;
-    private final String url;
-    private final String version;
-    private final Map<String, String> headers;
-    private final String body;
+    private Method method;
+    private String path;
+    private String version;
+    private Map<String, String> params = new HashMap<>();
+    private Map<String, String> headers = new HashMap<>();
+    private String body;
 
-    private HttpRequest(String method, String url, String version, Map<String, String> headers, String body) {
-        this.method = method;
-        this.url = url;
-        this.version = version;
-        this.headers = headers;
-        this.body = body;
+    private HttpRequest() {}
+
+    public String getPath() {
+        return path;
     }
 
-    public String getUrl() {
-        return url;
+    public Method getMethod() {
+        return method;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public Map<String, String> getParams() {
+        return params;
+    }
+
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    public String getBody() {
+        return body;
     }
 
     public static HttpRequest create(InputStream in) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(in, UTF_8));
+        HttpRequest httpRequest = new HttpRequest();
 
-        // Request Line 파싱
+        BufferedReader br = new BufferedReader(new InputStreamReader(in, UTF_8));
         String line = br.readLine();
+
+        parseRequestLine(line, httpRequest);
+
+        while ((line = br.readLine()) != null && !line.isEmpty()) {
+            parseHeader(line, httpRequest);
+        }
+
+        while (br.ready()) {
+            parseBody(br.readLine(), httpRequest);
+        }
+
+        return httpRequest;
+    }
+
+    private static void parseRequestLine(String line, HttpRequest httpRequest) {
         String[] requestLine = line.split("\\s");
         String method = requestLine[0];
-        String url = requestLine[1];
+        String uri = requestLine[1];
         String httpVersion = requestLine[2];
 
-        // Header 파싱
-        Map<String, String> headers = new HashMap<>();
-        while ((line = br.readLine()) != null && !line.isEmpty()) {
-            String[] headerForm = line.split(":\\s");
-            headers.put(headerForm[0], headerForm[1]);
+        if (!uri.matches("^/([a-zA-Z0-9\\-._~:/?#\\[\\]@!$&'()*+,;=%]+)?$")) {
+            throw new RuntimeException("잘못된 URI 형식");
         }
 
-        // Body 파싱
-        StringBuilder bodyBuilder = new StringBuilder();
-        while (br.ready()) {
-            bodyBuilder.append((char) br.read());
-        }
-        String body = bodyBuilder.toString();
+        Map<String, String> params = new HashMap<>();
+        String[] parts = uri.split("\\?");
 
-        return new HttpRequest(method, url, httpVersion, headers, body);
+        String path = parts[0];
+        if (parts.length > 1 && !parts[1].isEmpty()) {
+            parts = parts[1].split("&");
+
+            for (String param : parts) {
+                String[] keyValue = param.split("=");
+
+                if (keyValue.length > 1) {
+                    params.put(keyValue[0], keyValue[1]);
+                } else {
+                    params.put(keyValue[0], null);
+                }
+            }
+        }
+
+        httpRequest.method = Method.valueOf(method);
+        httpRequest.path = path;
+        httpRequest.version = httpVersion;
+        httpRequest.params = params;
+    }
+
+    private static void parseHeader(String line, HttpRequest httpRequest) {
+        String[] headerForm = line.split(":\\s");
+        httpRequest.headers.put(headerForm[0], headerForm[1]);
+    }
+
+    private static void parseBody(String line, HttpRequest httpRequest) {
+        httpRequest.body += line;
     }
 
     @Override
@@ -63,14 +113,25 @@ public class HttpRequest {
 
         sb.append(NEW_LINE)
                 .append("[Method] ").append(method).append(NEW_LINE)
-                .append("[URL] ").append(url).append(NEW_LINE)
+                .append("[URL] ").append(path).append(NEW_LINE)
                 .append("[Version] ").append(version).append(NEW_LINE);
 
         for (Entry<String, String> entry : headers.entrySet()) {
             sb.append("[Header] ").append(entry.getKey()).append(": ").append(entry.getValue()).append(NEW_LINE);
         }
+        for (Entry<String, String> entry : params.entrySet()) {
+            sb.append("[Param] ").append(entry.getKey()).append(": ").append(entry.getValue()).append(NEW_LINE);
+        }
         sb.append("[Body] ").append(body).append(NEW_LINE);
 
         return sb.toString();
+    }
+
+    public enum Method {
+        GET, POST, PUT, DELETE;
+
+        public static boolean isGetMethod(Method method) {
+            return method.equals(GET);
+        }
     }
 }
