5872011bb14e4b0920d29cfd530098db52557e93
kwyoohae <gocksdl7979@gmail.com>
2023-07-17 13:10:56 +0900

    refactor: Thread Pool 정적 변수로 변경

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -13,6 +13,7 @@ public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
     private static final int MAX_THREAD_POOL = 100;
+    private static final ExecutorService executorService = Executors.newFixedThreadPool(MAX_THREAD_POOL);
 
     public static void main(String args[]) throws Exception {
         int port = 0;
@@ -21,8 +22,6 @@ public class WebServer {
         } else {
             port = Integer.parseInt(args[0]);
         }
-
-        final ExecutorService executorService = Executors.newFixedThreadPool(MAX_THREAD_POOL);
 
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
