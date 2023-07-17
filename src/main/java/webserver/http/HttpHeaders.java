823ee932e8b8c4f7c5d7bd633e6ac9c494b882a5
Hypersand <on8214@gmail.com>
2023-07-17 13:52:00 +0900

    feat: RequestHeaders의 정보를 담고 있는 HttpHeaders 클래스 생성

---
+++ src/main/java/webserver/http/HttpHeaders.java
@@ -0,0 +1,36 @@
+package webserver.http;
+
+import utils.RequestParser;
+import utils.StringUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Map;
+
+import static utils.StringUtils.NEW_LINE;
+
+public class HttpHeaders {
+
+    private final Map<String, String> headers;
+
+    public HttpHeaders(Map<String, String> headers) {
+        this.headers = headers;
+    }
+
+    public static HttpHeaders create(BufferedReader br) throws IOException {
+        return new HttpHeaders(RequestParser.parseRequestHeaders(br));
+    }
+
+    public String show() {
+        StringBuilder sb = new StringBuilder();
+
+        headers.forEach((key, value) -> {
+            sb.append(key);
+            sb.append(" : ");
+            sb.append(value);
+            sb.append(NEW_LINE);
+        });
+
+        return sb.toString();
+    }
+}
