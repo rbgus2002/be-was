04d64bf6cf023e03ce706955a3d0130dc88b2ee2
kimahhh <kimahyunn132@gmail.com>
2023-07-17 17:35:12 +0900

    Refactor: Body 클래스에 있던 메소드 FileIOUtils 클래스로 이동
    Body 클래스를 삭제함에 따라 메소드 이동
    uri를 split으로 얻던 메소드 삭제

--- src/main/java/utils/FileIOUtils.java
+++ src/main/java/utils/FileIOUtils.java
@@ -1,10 +1,21 @@
 package utils;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class FileIOUtils {
+    public static final String HTML = "html";
+    public static final String CSS = "css";
     public static final String STATIC_RESOURCES = "src/main/resources/static";
     public static final String TEMPLATES_RESOURCES = "src/main/resources/templates";
-    public static String splitPath(String requestLine) {
-        return requestLine.split(" ")[1];
+
+    public static byte[] loadStaticFromPath(String uri) throws IOException {
+        return Files.readAllBytes(new File(STATIC_RESOURCES + uri).toPath());
+    }
+
+    public static byte[] loadTemplatesFromPath(String uri) throws IOException {
+        return Files.readAllBytes(new File(TEMPLATES_RESOURCES + uri).toPath());
     }
 
 }
--- src/test/java/utils/FileIOUtilsTest.java
+++ src/test/java/utils/FileIOUtilsTest.java
@@ -2,16 +2,32 @@ package utils;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.RequestHandler;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static utils.FileIOUtils.splitPath;
+import static utils.FileIOUtils.*;
 
 class FileIOUtilsTest {
+    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
     @Test
-    @DisplayName("HTTP Request Header의 첫번째 줄에서 파일명을 얻어야 한다")
-    void splitPathTest() {
-        assertEquals("./index.html", splitPath("GET ./index.html HTTP/1.1"));
+    @DisplayName("Header의 첫 번째 라인을 통해 static 디렉터리 안의 file을 load 할 수 있어야 한다")
+    void loadStaticFromPathTest() throws IOException {
+        byte[] body = loadStaticFromPath("/css/styles.css");
+        logger.debug("static file : {}", body);
+        assertNotNull(body);
+    }
+
+    @Test
+    @DisplayName("Header의 첫 번째 라인을 통해 templates 디렉터리 안의 file을 load 할 수 있어야 한다")
+    void loadTemplatesFromPathTest() throws IOException {
+        byte[] body = loadTemplatesFromPath("/index.html");
+        logger.debug("templates file : {}", body);
+        assertNotNull(body);
     }
 
 }
