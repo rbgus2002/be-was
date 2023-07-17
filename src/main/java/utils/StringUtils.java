4331863fd391e5f6ac2b168e2500b1eac3ac0674
ddingmin <ddingminyt@gmail.com>
2023-07-17 16:44:38 +0900

    feat: "다양한 상수 StringUtils 클래스에 추가"
    - NEW_LINE 이름 변경
    - FILE_SEPARATOR 추가
    - DOT, QUESTION_MARK, AMPERSAND, EQUALS_SIGN, COLON 추가

--- src/main/java/utils/StringUtils.java
+++ src/main/java/utils/StringUtils.java
@@ -1,7 +1,13 @@
 package utils;
 
 public class StringUtils {
-    public static final String NEWLINE = System.getProperty("line.separator");
+    public static final String NEW_LINE = System.getProperty("line.separator");
+    public static final String FILE_SEPARATOR = System.getProperty("file.separator");
+    public static final String DOT = "\\.";
+    public static final String QUESTION_MARK = "\\?";
+    public static final String AMPERSAND = "\\&";
+    public static final String EQUALS_SIGN = "\\=";
+    public static final String COLON = "\\:";
 
     private StringUtils() {
     }
