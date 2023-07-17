8221433ab08eda63c351a76fac310188cf5f7429
ijehyunpark <adam991217@naver.com>
2023-07-17 17:07:21 +0900

    Refactor: 공백 일관성 유지

--- src/main/java/exception/ExceptionName.java
+++ src/main/java/exception/ExceptionName.java
@@ -1,6 +1,7 @@
 package exception;
 
 public abstract class ExceptionName {
+
     public static final String FILE_NOT_FOUND = "해당 경로에 파일이 존재하지 않습니다.";
     public static final String WRONG_ARGUMENT = "잘못된 인자입니다.";
     public static final String ALREADY_REGISTERED = "이미 등록되어 있는 인스턴스입니다.";
