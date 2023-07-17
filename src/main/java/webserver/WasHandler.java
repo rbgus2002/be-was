6e0b8b51f3f6bd0fb80bcbea948ff4c5f51ed925
kwyoohae <gocksdl7979@gmail.com>
2023-07-17 10:47:31 +0900

    feat: RequestMapping이 붙은 메서드를 가져오는 로직 작성

---
+++ src/main/java/webserver/WasHandler.java
@@ -0,0 +1,38 @@
+package webserver;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import controller.Controller;
+import controller.annotation.RequestMapping;
+
+public class WasHandler {
+
+	private final HttpWasRequest httpWasRequest;
+	private final HttpWasResponse httpWasResponse;
+
+	public WasHandler(final HttpWasRequest httpWasRequest, final HttpWasResponse httpWasResponse) {
+		this.httpWasRequest = httpWasRequest;
+		this.httpWasResponse = httpWasResponse;
+	}
+
+	public void service() {
+
+	}
+
+	private List<Method> getResourcePathMethod() {
+		final String resourcePath = httpWasRequest.getResourcePath();
+
+		final Method[] declaredMethods = Controller.class.getDeclaredMethods();
+		return Arrays.stream(declaredMethods)
+			.filter(method -> method.isAnnotationPresent(RequestMapping.class))
+			.filter(method -> {
+				final RequestMapping requestMapping = method.getDeclaredAnnotation(RequestMapping.class);
+				final String path = requestMapping.path();
+				return path.equals(resourcePath);
+			})
+			.collect(Collectors.toList());
+	}
+}
