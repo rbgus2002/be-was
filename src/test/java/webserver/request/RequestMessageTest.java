0ae9a7d6898483b2745f46da512bb94b5ba3bdd9
ddingmin <ddingminyt@gmail.com>
2023-07-17 11:00:00 +0900

    test: "request 메시지 클래스에 파라미터를 담을 수 있는 기능을 테스트한다."

--- src/test/java/webserver/request/RequestMessageTest.java
+++ src/test/java/webserver/request/RequestMessageTest.java
@@ -17,12 +17,12 @@ class RequestMessageTest {
                 "Accept: application/json\n";
 
         // when
-        RequestMessage requestLine = new RequestMessage(message);
+        RequestMessage requestMessage = new RequestMessage(message);
 
-        String method = requestLine.getMethod();
-        String path = requestLine.getPath();
-        String version = requestLine.getVersion();
-        String host = requestLine.getMetaData("Host");
+        String method = requestMessage.getMethod();
+        String path = requestMessage.getPath();
+        String version = requestMessage.getVersion();
+        String host = requestMessage.getMetaData("Host");
 
         // then
         assertAll(
@@ -30,6 +30,29 @@ class RequestMessageTest {
                 () -> assertEquals("/test", path),
                 () -> assertEquals("HTTP/1.1", version),
                 () -> assertEquals("test.com", host)
+        );
+    }
+
+    @Test
+    @DisplayName("Path에 파라미터를 포함한 GET 메소드의 객체가 생성된다.")
+    void createGetWithParameters() {
+        // given
+        String message = "GET /test?user=ddingmin&password=1234&name=gildong HTTP/1.1\n" +
+                "Host: test.com\n" +
+                "Accept: application/json\n";
+
+        // when
+        RequestMessage requestMessage = new RequestMessage(message);
+
+        String user = requestMessage.getParameter("user");
+        String password = requestMessage.getParameter("password");
+        String name = requestMessage.getParameter("name");
+
+        // then
+        assertAll(
+                () -> assertEquals("ddingmin", user),
+                () -> assertEquals("1234", password),
+                () -> assertEquals("gildong", name)
         );
     }
 }
