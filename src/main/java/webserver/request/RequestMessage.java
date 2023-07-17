e747da1f70fb0987f527491e46f2b4511069fb88
ddingmin <ddingminyt@gmail.com>
2023-07-17 17:53:17 +0900

    feat: "RequestHandler 확장자 필드 구현"
    - 확장자가 존재하는지 확인하는 필드 구현
    - 확장자 존재 여부를 가져오는 기능 구현

--- src/main/java/webserver/request/RequestMessage.java
+++ src/main/java/webserver/request/RequestMessage.java
@@ -3,39 +3,50 @@ package webserver.request;
 import java.util.HashMap;
 import java.util.Map;
 
+import static utils.StringUtils.*;
+
 public class RequestMessage {
     private final String method;
     private final String path;
     private final String version;
+    private final boolean hasExtension;
     private final Map<String, String> metaData = new HashMap<>();
     private final Map<String, String> parameters = new HashMap<>();
 
     public RequestMessage(String header) {
-        String[] lines = header.split("\\n");
+        String[] lines = header.split(NEW_LINE);
 
         String[] tokens = lines[0].split(" ");
         this.method = tokens[0];
-        this.path = tokens[1].split("\\?", 2)[0];
+        this.path = tokens[1].split(QUESTION_MARK, 2)[0];
         this.version = tokens[2];
 
         saveParameters(tokens[1]);
         saveMetadata(lines);
+
+        if (path.split(DOT, 2).length == 2) {
+            hasExtension = true;
+            return;
+        }
+        hasExtension = false;
     }
 
     private void saveMetadata(String[] lines) {
         String[] tokens;
         for (int i = 1; i < lines.length; i++) {
-            tokens = lines[i].split(":");
+            tokens = lines[i].split(COLON);
             metaData.put(tokens[0].trim(), tokens[1].trim());
         }
     }
 
     private void saveParameters(String path) {
-        String[] tokens = path.split("\\?", 2);
+        String[] tokens = path.split(QUESTION_MARK, 2);
         if (tokens.length == 2) {
-            String[] stringParameters = tokens[1].split("&");
+            String[] stringParameters = tokens[1].split(AMPERSAND);
             for (String parameter : stringParameters) {
-                String[] keyValue = parameter.split("=", 2);
+                String[] keyValue = parameter.split(EQUALS_SIGN, 2);
+
+                // 키 벨류가 올바르지 않는 경우의 예외 처리
                 if (keyValue.length != 2) {
                     throw new IllegalArgumentException("Wrong query string format");
                 }
@@ -62,5 +73,9 @@ public class RequestMessage {
 
     public String getParameter(String key) {
         return parameters.get(key);
+    }
+
+    public boolean isHasExtension() {
+        return hasExtension;
     }
 }
