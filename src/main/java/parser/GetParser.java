45e26133832edd10d6d709a0728373cb67e3f84f
dydwo0740 <dydwo0740@naver.com>
2023-07-17 17:38:43 +0900

    pause

--- src/main/java/parser/GetParser.java
+++ src/main/java/parser/GetParser.java
@@ -9,8 +9,34 @@ public class GetParser implements Parser{
     public HTTPServletRequest getProperRequest(BufferedReader br) {
         return null;
     }
+    
 
-    private String findUrl(String startLine){
-        return startLine.split(" ")[1];
+    private String parseVersion(String startLine) {
+        return startLine.split(" ")[2];
+    }
+
+    private String parseUrl(String startLine) {
+        String[] tokens = startLine.split(" ");
+        if (tokens[1].contains("?")) {
+            String[] divideUrlAndQuery = tokens[1].split("\\?");
+            parseQuery(divideUrlAndQuery[1]);
+            return divideUrlAndQuery[0];
+        }
+        return tokens[1];
+    }
+
+    private String parseMethod(String startLine) {
+        String[] tokens = startLine.split(" ");
+        return tokens[0];
+    }
+
+    private void parseQuery(String parameter) {
+        String[] tokens = parameter.split("&");
+
+        for (String token : tokens) {
+            String key = token.substring(0, token.indexOf("="));
+            String value = token.substring(token.indexOf("=") + 1, token.length());
+            query.put(key, value);
+        }
     }
 }
---
+++ src/main/java/parser/PostParser.java
@@ -0,0 +1,12 @@
+package parser;
+
+import webserver.HTTPServletRequest;
+
+import java.io.BufferedReader;
+
+public class PostParser implements Parser{
+    @Override
+    public HTTPServletRequest getProperRequest(BufferedReader br) {
+        return null;
+    }
+}
