181b75a1e4f24238ba2cf92762dd1b4c6e81a7bb
윤상현 <yshyeon102@gmail.com>
2023-07-18 01:42:27 +0900

    test(HttpRequestParserTest): 기존 테스트 코드에 URL 검증 부분 추가
    #TODO
    - GET / POST를 제외한 다른 HTTP 메서드 검증 코드 추가

--- src/test/java/webserver/HttpRequestParserTest.java
+++ src/test/java/webserver/HttpRequestParserTest.java
@@ -4,6 +4,10 @@ import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import webserver.http.HttpMethod;
+import webserver.http.HttpRequest;
+import webserver.http.HttpVersion;
+import webserver.http.URL;
 
 import java.util.List;
 import java.util.Map;
@@ -19,7 +23,7 @@ class HttpRequestParserTest {
     @DisplayName("Http get 요청 메세지 Parsing Test ")
     @Test
     void getTest() {
-        String httpMessage = "GET / HTTP/1.1\r\n" +
+        String httpMessage = "GET /search?q=1,2 HTTP/1.1\r\n" +
                 "Host: localhost:8080\r\n" +
                 "Accept: image/gif, image/jpeg, */*\r\n" +
                 "Accept-Language: en-us\r\n" +
@@ -27,12 +31,18 @@ class HttpRequestParserTest {
                 "User-Agent: Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)\r\n\r\n";
 
         HttpRequest httpRequest = parser.parseHttpMessage(httpMessage);
-        Map<String, List<String>> metaData = httpRequest.getMetaData();
+
 
         SoftAssertions softAssertions = new SoftAssertions();
         softAssertions.assertThat(httpRequest.getMethod()).isEqualTo(HttpMethod.GET);
+
+        URL url = httpRequest.getURL();
+        softAssertions.assertThat(url.getPath()).isEqualTo("/");
+        softAssertions.assertThat(url.getQueryParameter()).hasSize(0);
+
         softAssertions.assertThat(httpRequest.getHttpVersion()).isEqualTo(HttpVersion.V1_1);
-        // TODO : URI 검증
+
+        Map<String, List<String>> metaData = httpRequest.getMetaData();
         softAssertions.assertThat(metaData).containsEntry("Host", List.of("localhost:8080"));
         softAssertions.assertThat(metaData).containsEntry("Accept", List.of("image/gif", "image/jpeg", "*/*"));
         softAssertions.assertThat(metaData).containsEntry("Accept-Language", List.of("en-us"));
@@ -56,17 +66,23 @@ class HttpRequestParserTest {
 
         HttpRequest httpRequest = parser.parseHttpMessage(httpMessage);
         Map<String, List<String>> metaData = httpRequest.getMetaData();
-        String body = httpRequest.getBody();
 
         SoftAssertions softAssertions = new SoftAssertions();
         softAssertions.assertThat(httpRequest.getMethod()).isEqualTo(HttpMethod.POST);
-        // TODO : URI 검증
+
+        URL url = httpRequest.getURL();
+        softAssertions.assertThat(url.getPath()).isEqualTo("/");
+        softAssertions.assertThat(url.getQueryParameter()).hasSize(0);
+
         softAssertions.assertThat(httpRequest.getHttpVersion()).isEqualTo(HttpVersion.V1_0);
+
         softAssertions.assertThat(metaData).containsEntry("Host", List.of("localhost:8080"));
         softAssertions.assertThat(metaData).containsEntry("Content-Type", List.of("application/json"));
         softAssertions.assertThat(metaData).containsEntry("Content-Length", List.of("44"));
         softAssertions.assertThat(metaData).containsEntry("Accept-Encoding", List.of("gzip", "deflate", "*/*"));
         softAssertions.assertThat(metaData).containsEntry("Accept", List.of("*/*"));
+
+        String body = httpRequest.getBody();
         softAssertions.assertThat(body).isEqualTo(
                 "Content-Length: 44\r\n\r\n" +
                         "{\r\n" +
