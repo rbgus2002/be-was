024d7e7ad0de6fde65bf5ccd05ad329ffb48caf3
Hypersand <on8214@gmail.com>
2023-07-17 21:41:11 +0900

    feat: finally 구문을 추가해 쓰레드 풀 shutdown 처리

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -1,14 +1,17 @@
 package webserver;
 
-import java.net.ServerSocket;
-import java.net.Socket;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
+    private static final int AVAILABLE_PROCESSORS = Runtime.getRuntime().availableProcessors();
 
     public static void main(String args[]) throws Exception {
         int port = 0;
@@ -18,6 +21,7 @@ public class WebServer {
             port = Integer.parseInt(args[0]);
         }
 
+        ExecutorService executorService = Executors.newFixedThreadPool(AVAILABLE_PROCESSORS);
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
@@ -25,9 +29,10 @@ public class WebServer {
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection));
-                thread.start();
+                executorService.execute(new RequestHandler(connection));
             }
+        } finally {
+            executorService.shutdown();
         }
     }
 }
