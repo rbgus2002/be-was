5a7e9c33b424a384d7576bb3c4de97c7df90f25b
ddingmin <ddingminyt@gmail.com>
2023-07-17 11:28:31 +0900

    feat: "request 메시지 클래스에 파라미터를 담을 수 있는 기능 구현"
    - Path와 쿼리 스트링을 파싱하는 기능
    - 파싱된 쿼리 스트링을 key와 value로 파싱하는 기능
    - value 값이 없는 경우 예외를 던짐

--- src/main/java/webserver/request/RequestMessage.java
+++ src/main/java/webserver/request/RequestMessage.java
@@ -8,18 +8,39 @@ public class RequestMessage {
     private final String path;
     private final String version;
     private final Map<String, String> metaData = new HashMap<>();
+    private final Map<String, String> parameters = new HashMap<>();
 
     public RequestMessage(String header) {
         String[] lines = header.split("\\n");
 
         String[] tokens = lines[0].split(" ");
         this.method = tokens[0];
-        this.path = tokens[1];
+        this.path = tokens[1].split("\\?", 2)[0];
         this.version = tokens[2];
 
+        saveParameters(tokens[1]);
+        saveMetadata(lines);
+    }
+
+    private void saveMetadata(String[] lines) {
+        String[] tokens;
         for (int i = 1; i < lines.length; i++) {
             tokens = lines[i].split(":");
             metaData.put(tokens[0].trim(), tokens[1].trim());
+        }
+    }
+
+    private void saveParameters(String path) {
+        String[] tokens = path.split("\\?", 2);
+        if (tokens.length == 2) {
+            String[] stringParameters = tokens[1].split("&");
+            for (String parameter : stringParameters) {
+                String[] keyValue = parameter.split("=", 2);
+                if (keyValue.length != 2) {
+                    throw new IllegalArgumentException("Wrong query string format");
+                }
+                parameters.put(keyValue[0], keyValue[1]);
+            }
         }
     }
 
@@ -37,5 +58,9 @@ public class RequestMessage {
 
     public String getMetaData(String key) {
         return metaData.get(key);
+    }
+
+    public String getParameter(String key) {
+        return parameters.get(key);
     }
 }
