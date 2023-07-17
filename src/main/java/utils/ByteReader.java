dec03686d9af75e073e2eea1ec4ff1ccc39598a1
윤상현 <yshyeon102@gmail.com>
2023-07-17 13:28:21 +0900

    feat(ByteReader): InputStream에 담긴 메세지 읽기 기능 추가

---
+++ src/main/java/utils/ByteReader.java
@@ -0,0 +1,21 @@
+package utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+import static utils.StringUtils.NEW_LINE;
+
+public class ByteReader {
+    public static String readInputStream(InputStream inputStream) throws IOException {
+        StringBuilder messageReader = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+        String line;
+        while ((line = br.readLine()) != null) {
+            messageReader.append(line).append(NEW_LINE);
+        }
+        return messageReader.toString();
+    }
+}
