c5b21e1ea763499a3e678c649d74e7e9e2096102
윤상현 <yshyeon102@gmail.com>
2023-07-17 10:56:48 +0900

    refactor: Main클래스를 찾기 쉽도록 Main 클래스 분리
    Main문이 담긴 클래스를 찾기 어려움.
    가장 상위 패키지에 Main클래스를 정의하고 WebServer를 실행하도록 분리함.

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -1,30 +1,30 @@
 package webserver;
 
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.logging.Handler;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class WebServer {
-    private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
-    private static final int DEFAULT_PORT = 8080;
+    public static final int DEFAULT_PORT = 8080;
+    public static final int NUM_OF_THREAD = 10;
 
-    public static void main(String args[]) throws Exception {
-        int port = 0;
-        if (args == null || args.length == 0) {
-            port = DEFAULT_PORT;
-        } else {
-            port = Integer.parseInt(args[0]);
-        }
+    private final Logger logger = LoggerFactory.getLogger(WebServer.class);
+    private final int port;
 
-        ExecutorService executorService = Executors.newFixedThreadPool(10);
+    public WebServer() {
+        port = DEFAULT_PORT;
+    }
 
+    public WebServer(int port) {
+        this.port = port;
+    }
+
+    public void run() throws Exception {
+        ExecutorService executorService = Executors.newFixedThreadPool(NUM_OF_THREAD);
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
 
