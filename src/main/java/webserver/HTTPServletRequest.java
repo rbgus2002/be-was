d88a9af7535e1d966e39e88a6dadd72e1ec20d84
dydwo0740 <dydwo0740@naver.com>
2023-07-17 17:14:39 +0900

    Post method에 대한 처리를감당하기 위해서, HTTPServletRequest 리팩초링

--- src/main/java/webserver/HTTPServletRequest.java
+++ src/main/java/webserver/HTTPServletRequest.java
@@ -1,6 +1,5 @@
 package webserver;
 
-import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,14 +9,13 @@ public class HTTPServletRequest {
     private final String method;
     private final String url;
     private final ConcurrentHashMap<String, String> query = new ConcurrentHashMap<>();
-
     private final String version;
     private static final Logger logger = LoggerFactory.getLogger(HTTPServletRequest.class);
 
 
     public HTTPServletRequest(String startLine) {
         method = parseMethod(startLine);
-        url = parseUri(startLine);
+        url = parseUrl(startLine);
         version = parseVersion(startLine);
     }
 
@@ -38,7 +36,7 @@ public class HTTPServletRequest {
         return version;
     }
 
-    private String parseUri(String startLine) {
+    private String parseUrl(String startLine) {
         String[] tokens = startLine.split(" ");
         if (tokens[1].contains("?")) {
             String[] divideUrlAndQuery = tokens[1].split("\\?");
