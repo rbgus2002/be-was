3ef3708a416b0e89cbefba4561d5bb64fd2608c4
Hypersand <on8214@gmail.com>
2023-07-17 13:56:08 +0900

    feat: RequestLine과 Headers 정보를 담고 있는 HttpRequest 클래스 생성

--- src/main/java/webserver/http/HttpRequest.java
+++ src/main/java/webserver/http/HttpRequest.java
@@ -1,13 +1,41 @@
 package webserver.http;
 
-public class HttpRequest {
-    private final String uri;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
-    public HttpRequest(String uri) {
-        this.uri = uri;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static utils.StringUtils.NEW_LINE;
+
+public class HttpRequest {
+    private final HttpRequestLine httpRequestLine;
+    private final HttpHeaders httpHeaders;
+
+    public HttpRequest(HttpRequestLine httpRequestLine, HttpHeaders httpHeaders) {
+        this.httpRequestLine = httpRequestLine;
+        this.httpHeaders = httpHeaders;
     }
 
-    public String getUri() {
-        return uri;
+    public HttpRequestLine getHttpRequestLine() {
+        return httpRequestLine;
+    }
+
+    public static HttpRequest create(InputStream in) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in, UTF_8));
+        HttpRequestLine requestLine = HttpRequestLine.create(br.readLine());
+        HttpHeaders headers = HttpHeaders.create(br);
+
+        return new HttpRequest(requestLine, headers);
+    }
+
+    public String show() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("METHOD : ").append(httpRequestLine.getMethod()).append(NEW_LINE);
+        sb.append("URI : ").append(httpRequestLine.getUri()).append(NEW_LINE);
+        sb.append("VERSION : ").append(httpRequestLine.getVersion()).append(NEW_LINE);
+        sb.append(httpHeaders.show());
+
+        return sb.toString();
     }
 }
