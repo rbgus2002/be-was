f59b42f3fe0cec1ac687e2b3c93796ffb1f05b00
kimahhh <kimahyunn132@gmail.com>
2023-07-17 17:33:41 +0900

    Rename: Body 클래스 삭제 및 Controller 클래스명 변경
    Body가 했던 일(Body 생성)을 Controller로 이동
    Controller 클래스명 UserController에서 Controller로 변경

---
+++ src/main/java/controller/Controller.java
@@ -0,0 +1,56 @@
+package controller;
+
+import service.UserService;
+
+import http.HttpRequest;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static utils.FileIOUtils.*;
+
+public class Controller {
+    private final UserService userService = new UserService();
+
+    public byte[] loadFileByRequest(HttpRequest httpRequest) throws IOException {
+        String uri = httpRequest.getUri();
+        String[] uris = uri.split("\\.");
+        switch (uris[uris.length - 1]) {
+            case HTML:
+                return loadTemplatesFromPath(uri);
+            case CSS:
+                return loadStaticFromPath(uri);
+            default:
+                return routeByUri(uri);
+        }
+    }
+
+    public byte[] routeByUri(String uri) {
+        String[] apis = uri.split("\\?");
+        if (apis[0].equals("/user/create")) {
+            return createUser(parseParams(apis[1]));
+        }
+        return null;
+    }
+
+    private Map<String, String> parseParams(String parameter) {
+        String[] params = parameter.split("&");
+        Map<String, String> information = new HashMap<>();
+        for (String param : params) {
+            String[] info = param.split("=");
+            information.put(info[0], info[1]);
+        }
+        return information;
+    }
+
+    public byte[] createUser(Map<String, String> parameters) {
+        userService.createUser(parameters);
+        try {
+            return loadTemplatesFromPath("/user/signup_success.html");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
