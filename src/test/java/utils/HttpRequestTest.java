4f592425636f91cc083dd35a9c4ab566ed9aaa44
Jaehong Kim <kjhonggg95@gmail.com>
2023-07-17 17:18:01 +0900

    test: HttpRequest 테스트
    StringBuilder 와 InputStream 으로 직접 HttpRequest 객체로 넘겨줄 InputStream 인스턴스를 만들고 이에 대한 테스트 진행

--- src/test/java/utils/HttpRequestTest.java
+++ src/test/java/utils/HttpRequestTest.java
@@ -1,57 +1,41 @@
 package utils;
 
 import http.HttpRequest;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import util.StringUtil;
 
+import java.io.*;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.*;
-import static http.HttpRequestParser.parseUrlFromRequestLine;
+import static util.StringUtil.*;
 
 class HttpRequestTest {
 
-    private final String requestLine = "GET /create?userId=jaehong&password=123 HTTP/1.1";
-    private HttpRequest httpRequest;
-
-    @BeforeEach
-    void setup() {
-        httpRequest = new HttpRequest(requestLine);
-    }
-
     @Test
-    @DisplayName("Request line 으로부터 method 를 분리한다")
-    void getMethod() {
+    @DisplayName("HttpRequest 테스트")
+    void test() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        sb.append(appendNewLine("GET /index.html HTTP/1.1"))
+                .append(appendNewLine("Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"))
+                .append(appendNewLine("Connection: keep-alive"));
+        InputStream in = new ByteArrayInputStream(sb.toString().getBytes());
+
+        HttpRequest httpRequest = new HttpRequest(in);
+
         String method = httpRequest.getMethod();
-        assertThat(method).isEqualTo("GET");
-    }
-
-    @Test
-    @DisplayName("Request line 으로부터 url 를 분리한다")
-    void getUrl() {
-        String url = parseUrlFromRequestLine(requestLine);
-        assertThat(url).isEqualTo("/create?userId=jaehong&password=123");
-    }
-
-    @Test
-    @DisplayName("Request line 으로부터 path 를 분리한다")
-    void getPath() {
         String path = httpRequest.getPath();
-        assertThat(path).isEqualTo("/create");
-    }
-
-    @Test
-    @DisplayName("Request line 으로부터 파라미터를 분리한다")
-    void getParams() {
-        Map<String, String> params = httpRequest.getParams();
-        assertThat(params).hasSize(2).contains(entry("userId", "jaehong"), entry("password", "123"));
-    }
-
-    @Test
-    @DisplayName("Request line 으로부터 HTTP version 을 분리한다")
-    void getVersion() {
         String version = httpRequest.getVersion();
+        Map<String, String> headers = httpRequest.getHeaders();
+
+        assertThat(method).isEqualTo("GET");
+        assertThat(path).isEqualTo("/index.html");
         assertThat(version).isEqualTo("HTTP/1.1");
+        assertThat(headers).hasSize(2)
+                .contains(entry("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"))
+                .contains(entry("Connection", "keep-alive"));
     }
 }
