bc071c75b0606037dbcf15dc092dc060b4ede88d
jst0951 <jst0951@gmail.com>
2023-07-18 11:43:05 +0900

    feat: Session관련된 Service를 제공하는 SessionService 생성

--- src/main/java/service/SessionService.java
+++ src/main/java/service/SessionService.java
@@ -1,2 +1,31 @@
-package service;public class SessionService {
+package service;
+
+import db.Database;
+import model.Session;
+
+import java.util.Collection;
+import java.util.UUID;
+
+public class SessionService {
+
+    public static Session getSession(String userId) {
+        // 해당 userId에 해당하는 Session 존재하는지 확인
+        Session session = Database.findSessionByUserId(userId);
+        // 존재하지 않으면 새로 Session 생성하여 제공
+        if(session == null) {
+            session = Database.addSession(userId);
+        }
+
+        return session;
+    }
+
+    public static Session updateSession(String userId) {
+        Session session = new Session(userId);
+        Database.updateSession(userId, session);
+        return session;
+    }
+
+    public static Collection<Session> getAllSession() {
+        return Database.findAllSession();
+    }
 }
