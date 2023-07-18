35da3c7825d97630cfc5d11f784c49abe147222f
Yunsik-Choi <choiys0311@gmail.com>
2023-07-18 20:48:36 +0900

    ⚡feat : 쿼리 파라미터 객체 구현

---
+++ src/main/java/webserver/http/request/QueryParameter.java
@@ -0,0 +1,35 @@
+package webserver.http.request;
+
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class QueryParameter {
+    private static final String DELIMITER = "?";
+    private static final String ENTRY_SEPARATOR = "&";
+    private static final String ENTRY_DELIMITER = "=";
+
+    private final Map<String, String> map;
+
+    public QueryParameter(final Map<String, String> map) {
+        this.map = map;
+    }
+
+    public static QueryParameter from(final String text) {
+        String queryParameterText = text.substring(text.indexOf(DELIMITER) + 1);
+        Map<String, String> map = Arrays.stream(queryParameterText.split(ENTRY_SEPARATOR))
+                .map(entry -> entry.split(ENTRY_DELIMITER))
+                .collect(Collectors.toMap(strings -> decode(strings[0]), strings -> decode(strings[1])));
+        return new QueryParameter(map);
+    }
+
+    private static String decode(final String string) {
+        return URLDecoder.decode(string, StandardCharsets.UTF_8);
+    }
+
+    public String get(final String key) {
+        return map.get(key);
+    }
+}
