9a3e3e9a946a835fc4943bf6571305e6611491f3
jst0951 <jst0951@gmail.com>
2023-07-18 11:25:40 +0900

    feat: Session이 User가 아닌 userId 기반으로 동작하도록 변경, Database 메서드 구현

--- src/main/java/db/Database.java
+++ src/main/java/db/Database.java
@@ -3,12 +3,14 @@ package db;
 import com.google.common.collect.Maps;
 
 import model.User;
+import model.Session;
 
 import java.util.Collection;
 import java.util.Map;
 
 public class Database {
     private static final Map<String, User> users = Maps.newHashMap();
+    private static final Map<String, Session> sessions = Maps.newHashMap();
 
     // User
     public static void addUser(User user) {
@@ -19,5 +21,19 @@ public class Database {
     }
     public static Collection<User> findAllUser() {
         return users.values();
+    }
+
+    // Session
+    public static void addSession(String userId) {
+        sessions.put(userId, new Session(userId));
+    }
+    public static void updateSession(String userId, Session session) {
+        sessions.put(userId, session);
+    }
+    public static Session findSessionByUserId(String userId) {
+        return sessions.get(userId);
+    }
+    public static Collection<Session> findAllSession() {
+        return sessions.values();
     }
 }
--- src/main/java/model/Session.java
+++ src/main/java/model/Session.java
@@ -3,24 +3,22 @@ package model;
 import java.util.UUID;
 
 public class Session {
-    private final User user;
+    private final String userId;
     private String sessionId;
 
-    public Session(User user) {
-        this.user = user;
+    public Session(String userId) {
+        this.userId = userId;
         this.sessionId = UUID.randomUUID().toString();
     }
 
-    public User getUser() {
-        return user;
+    public String getUserId() {
+        return userId;
     }
-
     public String getSessionId() {
         return sessionId;
     }
-
-    public String regenerateSessionId() {
-        sessionId = UUID.randomUUID().toString();
-        return sessionId;
+    public String setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+        return this.sessionId;
     }
 }
