e1411a581926b37f593d44f4721fd46eed831475
kwyoohae <gocksdl7979@gmail.com>
2023-07-18 17:24:07 +0900

    refactor: softassertion으로 테스트 변경

--- src/test/java/webserver/response/HttpWasResponseTest.java
+++ src/test/java/webserver/response/HttpWasResponseTest.java
@@ -2,7 +2,6 @@ package webserver.response;
 
 import java.io.ByteArrayOutputStream;
 
-import org.assertj.core.api.Assertions;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,11 +12,10 @@ class HttpWasResponseTest {
 	@DisplayName("404 Response를 제대로 생성하는지")
 	void create404Header() {
 		// given
-		String response = "HTTP/1.1 404 NOT FOUND\r\n"
-			+ "Content-Type: text/plain;charset=utf-8\r\n"
-			+ "Content-Length: 13\r\n"
-			+ "\r\n"
-			+ "404 Not Found";
+		String requestLine = "HTTP/1.1 404 NOT FOUND\r\n";
+		String contentType = "Content-Type: text/plain;charset=utf-8\r\n";
+		String contentLength = "Content-Length: 13\r\n";
+		String body = "404 Not Found";
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 		final HttpWasResponse httpWasResponse = new HttpWasResponse(outputStream);
 
@@ -26,25 +24,31 @@ class HttpWasResponseTest {
 		httpWasResponse.doResponse();
 
 		// then
-		Assertions.assertThat(outputStream.toString()).hasToString(response);
+		SoftAssertions softAssertions = new SoftAssertions();
+		softAssertions.assertThat(outputStream.toString()).contains(requestLine);
+		softAssertions.assertThat(outputStream.toString()).contains(contentType);
+		softAssertions.assertThat(outputStream.toString()).contains(contentLength);
+		softAssertions.assertThat(outputStream.toString()).contains(body);
 	}
 
 	@Test
 	@DisplayName("302 Header를 제대로 생성하는지")
 	void create302Header() {
 		// given
-		String location = "http:localhost:8080/index.html";
-		String response = "HTTP/1.1 302 FOUND\r\n"
-			+ "Location: " + location + "\r\n";
+		String locationPath = "http:localhost:8080/index.html";
+		String requestLine = "HTTP/1.1 302 FOUND\r\n";
+		String location = "Location: " + locationPath + "\r\n";
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 		final HttpWasResponse httpWasResponse = new HttpWasResponse(outputStream);
 
 		// when
-		httpWasResponse.response302Header(location);
+		httpWasResponse.response302Header(locationPath);
 		httpWasResponse.doResponse();
 
 		// then
-		Assertions.assertThat(outputStream.toString()).hasToString(response);
+		SoftAssertions softAssertions = new SoftAssertions();
+		softAssertions.assertThat(outputStream.toString()).contains(requestLine);
+		softAssertions.assertThat(outputStream.toString()).contains(location);
 	}
 
 	@Test
