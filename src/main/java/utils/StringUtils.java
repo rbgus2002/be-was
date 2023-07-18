3e435fc48ed824f34a269ff7703bec2b013a60bc
ddingmin <ddingminyt@gmail.com>
2023-07-18 12:59:04 +0900

    fix: "NEW_LINE "line.separator" -> "\r\n" 으로 변경."
    - 클라이언트와 서버의 운영체제 차이를 생각해 line break를 표준인 \r\n으로 변경.

--- src/main/java/utils/StringUtils.java
+++ src/main/java/utils/StringUtils.java
@@ -1,7 +1,7 @@
 package utils;
 
 public class StringUtils {
-    public static final String NEW_LINE = System.getProperty("line.separator");
+    public static final String NEW_LINE = "\r\n";
     public static final String FILE_SEPARATOR = System.getProperty("file.separator");
     public static final String DOT = "\\.";
     public static final String QUESTION_MARK = "\\?";
