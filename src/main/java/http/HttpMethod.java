4816dc4bbf48f690574c17e24c44ac9ced3945d0
Jaehong Kim <kjhonggg95@gmail.com>
2023-07-18 12:52:31 +0900

    feat: HttpMethod enum 생성
    문자열로 파싱되어 사용되고 있는 http method 를 enum 타입으로 관리하도록 HttpMethod enum 을 생성함.

---
+++ src/main/java/http/HttpMethod.java
@@ -0,0 +1,13 @@
+package http;
+
+import java.util.Arrays;
+
+public enum HttpMethod {
+    GET, HEAD, POST, PUT, DELETE, CONNECT, OPTIONS, PATCH;
+
+    public static HttpMethod of(String method) throws IllegalStateException {
+        return Arrays.stream(HttpMethod.values())
+                .filter(httpMethod -> httpMethod.name().equalsIgnoreCase(method))
+                .findAny().orElseThrow(() -> new IllegalStateException("해당하는 HTTP 메서드를 찾을 수 없습니다."));
+    }
+}
