9b09b0100426cdc28e3e9fa6592e8aa4b5451306
jjy0709 <jjy0709@kaist.ac.kr>
2023-07-18 13:08:07 +0900

    [기능추가] GetParameter를 처리할 수 있는 handler 함수 추가
    - Get에 parameter가 있는 user/create을 handle 해주는 함수를 추가하였다.
    - uri의 Path에 따라 handle하는 방법을 나누기 위해 Map을 추가하였다.

--- src/main/java/webserver/HttpRequestHandler.java
+++ src/main/java/webserver/HttpRequestHandler.java
@@ -1,22 +1,59 @@
 package webserver;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 public class HttpRequestHandler {
     private final static Logger logger = LoggerFactory.getLogger(HttpRequestHandler.class);
+    private static final Map<String, Function> requestHandlers = new HashMap<>(){{
+        put("/user/create", request -> {
+            try {
+                return handleUserCreateRequest(request);
+            } catch (UnsupportedEncodingException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }};
 
-    public static byte[] handleGetStaticRequest(HttpRequest request) throws IOException {
-        String fileName = request.uri().split("/")[1];
-        logger.info("Requested Filename: {}", fileName);
+    public static byte[] handleRequest(HttpRequest request) throws IOException {
+        logger.info("HERE..... request uri: {}", request.uri());
+        if(requestHandlers.containsKey(request.uri())) {
+            return (byte[]) requestHandlers.get(request.uri()).apply(request);
+        }
+        return handleGetStaticRequest(request);
+    }
 
-        String path = System.getProperty("user.dir") + "/src/main/resources/templates" + fileName;
-        logger.info("Path: {}", path);
+    private static byte[] handleGetStaticRequest(HttpRequest request) throws IOException {
+        String fileName = request.uri();
+//        logger.info("Requested Filename: {}", fileName);
+
+        String path = System.getProperty("user.dir") + "/src/main/resources/templates/" + fileName;
+//        logger.info("Path: {}", path);
 
         return Files.readAllBytes(Paths.get(path));
+    }
+
+    private static byte[] handleUserCreateRequest(Object request) throws UnsupportedEncodingException {
+        String paramString = ((HttpRequest) request).uri().split("\\?")[1];
+        String[] parameters = paramString.split("&");
+        Map<String, String> paramPair = new HashMap();
+        for(String parameter: parameters) {
+            int splitIndex = parameter.indexOf("=");
+            paramPair.put(parameter.substring(0,splitIndex),
+                    URLDecoder.decode(parameter.substring(splitIndex+1), "UTF-8"));
+        }
+        User user = new User(paramPair.get("userId"), paramPair.get("password"), paramPair.get("name"), paramPair.get("email"));
+        logger.info("User info: userId: {}, password: {}, name: {}, email: {}", user.getUserId(), user.getPassword(), user.getName(), user.getEmail());
+        return user.toString().getBytes();
     }
 }
