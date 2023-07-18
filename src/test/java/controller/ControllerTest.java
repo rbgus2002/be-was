87f837dc5e19180fadb46842cae700f89d128d8b
kwyoohae <gocksdl7979@gmail.com>
2023-07-18 17:44:28 +0900

    test: 제대로 회원가입이 되는지 테스트 작성

---
+++ src/test/java/controller/ControllerTest.java
@@ -0,0 +1,48 @@
+package controller;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import db.Database;
+import model.User;
+import webserver.request.HttpWasRequest;
+import webserver.response.HttpWasResponse;
+
+class ControllerTest {
+
+	@BeforeEach
+	void beforeEach() {
+		Database.deleteAll();
+	}
+
+	@Test
+	@DisplayName("제대로 회원가입이 되야한다")
+	void saveUser() throws IOException {
+		// given
+		Controller controller = new Controller();
+
+		String input = "GET /user/create?userId=chan&password=123&name=chan&email=a%40naver.com HTTP/1.1\r\n";
+		InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+		final HttpWasRequest httpWasRequest = new HttpWasRequest(inputStream);
+		final HttpWasResponse httpWasResponse = new HttpWasResponse(new ByteArrayOutputStream());
+
+		//when
+		controller.saveUser(httpWasRequest, httpWasResponse);
+
+		//then
+		final User findUser = Database.findUserById("chan");
+		SoftAssertions softAssertions = new SoftAssertions();
+		softAssertions.assertThat(findUser.getUserId()).isEqualTo("chan");
+		softAssertions.assertThat(findUser.getEmail()).isEqualTo("a@naver.com");
+		softAssertions.assertThat(findUser.getName()).isEqualTo("chan");
+		softAssertions.assertThat(findUser.getPassword()).isEqualTo("123");
+	}
+
+}
