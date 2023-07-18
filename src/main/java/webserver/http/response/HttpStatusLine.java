54395471e4df65dff2b0da8cace62209f67c3ad3
Hypersand <on8214@gmail.com>
2023-07-18 23:21:37 +0900

    feat: HttpResponse의 Status Line 클래스 추가

--- src/main/java/webserver/http/response/HttpStatusLine.java
+++ src/main/java/webserver/http/response/HttpStatusLine.java
@@ -1,4 +1,25 @@
 package webserver.http.response;
 
 public class HttpStatusLine {
+    private final String version;
+    private final int statusCode;
+    private final String statusText;
+
+    public HttpStatusLine(String version, int statusCode, String statusText) {
+        this.version = version;
+        this.statusCode = statusCode;
+        this.statusText = statusText;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public String getStatusText() {
+        return statusText;
+    }
 }
