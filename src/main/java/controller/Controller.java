352847a9b97f6f0329bd445b03152cf8c6941a2b
kwyoohae <gocksdl7979@gmail.com>
2023-07-18 11:15:20 +0900

    fix: Service가 아닌 FrontController 도입을 위한 Service 로직 삭제

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,22 +1,24 @@
 package controller;
 
 import controller.annotation.RequestMapping;
-import service.UserService;
+import db.Database;
+import model.User;
 import webserver.HttpWasRequest;
 import webserver.HttpWasResponse;
 import webserver.utils.HttpMethod;
 
 public class Controller {
 
-	private final UserService userService;
-
-	public Controller(final UserService userService) {
-		this.userService = userService;
-	}
 
 	@RequestMapping(method = HttpMethod.GET, path = "/user/create")
 	public void saveUser(HttpWasRequest request, HttpWasResponse response) {
-		userService.saveUser(request);
+		final String userId = request.getParameter("userId");
+		final String password = request.getParameter("password");
+		final String name = request.getParameter("name");
+		final String email = request.getParameter("email");
+		final User user = new User(userId, password, name, email);
+		Database.addUser(user);
+
 		response.response302Header("http://localhost:8080/index.html");
 	}
 }
