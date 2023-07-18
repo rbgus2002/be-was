2c9a09a73c202c112a86a1bbcca85dce8b0be11c
dohyeon-han <limited.hyeon@gmail.com>
2023-07-18 13:56:52 +0900

    remove: 사용하지 않는 convertBufferedReaderToList 삭제

--- src/main/java/util/FileUtils.java
+++ src/main/java/util/FileUtils.java
@@ -16,13 +16,4 @@ public class FileUtils {
         String[] split = path.split("\\.");
         return split[split.length - 1];
     }
-
-    public static List<String> convertBufferedReaderToList(BufferedReader reader) throws IOException {
-        String line;
-        List<String> strings = new ArrayList<>();
-        while ((line = reader.readLine()) != null && !line.isEmpty()) {
-            strings.add(line);
-        }
-        return strings;
-    }
 }
--- src/test/java/util/FileUtilsTest.java
+++ src/test/java/util/FileUtilsTest.java
@@ -11,22 +11,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class FileUtilsTest {
 
     @Test
-    @DisplayName("bufferedReader를 list로 변환한다.")
-    void convertBufferedReaderToList() throws IOException {
-        //given
-        String newLine = System.getProperty("line.separator");
-        InputStream inputStream = new ByteArrayInputStream(("a" + newLine + "bb" + newLine + "ccc").getBytes());
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-
-        //when
-        List<String> strings = FileUtils.convertBufferedReaderToList(bufferedReader);
-
-        //then
-        List<String> expected = List.of("a", "bb", "ccc");
-        assertThat(strings).usingRecursiveComparison().isEqualTo(expected);
-    }
-
-    @Test
     @DisplayName("html 확장자를 추출한다.")
     void extractHtmlString() {
         //given
