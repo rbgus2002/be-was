3abeb8e8b557aa012883f7072e8d55056c2b05f9
kimahhh <kimahyunn132@gmail.com>
2023-07-18 13:13:58 +0900

    Fix: 현재 값 출력 안되는 오류 수정
    {}로 출력하는 방식에서 %d, %s로 출력하는 방식으로 수정

--- src/test/java/webserver/RequestHandlerTest.java
+++ src/test/java/webserver/RequestHandlerTest.java
@@ -3,10 +3,15 @@ package webserver;
 import controller.Controller;
 import db.Database;
 import http.HttpRequest;
+import http.HttpResponse;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import static java.lang.System.out;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,9 +30,9 @@ class RequestHandlerTest {
         // Then
         SoftAssertions softAssertions = new SoftAssertions();
         softAssertions.assertThat(Database.findAll().size())
-                .as("Database의 크기가 1이 아닙니다.\n현재 값: {}", Database.findAll().size()).isEqualTo(1);
+                .as("Database의 크기가 1이 아닙니다.\n현재 값: %d", Database.findAll().size()).isEqualTo(1);
         softAssertions.assertThat(Database.findUserById("kimahhh").getName())
-                .as("Id로 이름을 찾을 수 없습니다.\n 현재 값: {}", Database.findUserById("kimahhh").getName()).isEqualTo("김아현");
+                .as("Id로 이름을 찾을 수 없습니다.\n 현재 값: %s", Database.findUserById("kimahhh").getName()).isEqualTo("김아현");
         softAssertions.assertAll();
     }
 
