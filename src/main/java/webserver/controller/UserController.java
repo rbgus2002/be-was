8180a3df9dc7091ae444d560a5d85a3c0c99800c
Hypersand <on8214@gmail.com>
2023-07-18 16:16:35 +0900

    feat: User 관련 처리를 담당하는 UserController 생성

---
+++ src/main/java/webserver/controller/UserController.java
@@ -0,0 +1,16 @@
+package webserver.controller;
+
+import webserver.http.response.HttpResponse;
+import webserver.http.request.HttpRequest;
+
+public class UserController implements Controller {
+    @Override
+    public void doGet(HttpRequest httpRequest, HttpResponse httpResponse) {
+
+    }
+
+    @Override
+    public void doPost(HttpRequest httpRequest, HttpResponse httpResponse) {
+
+    }
+}
