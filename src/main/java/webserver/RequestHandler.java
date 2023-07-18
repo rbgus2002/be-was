aec5a2cbb8f02a413a80425d3da713bcdd0daa91
csct3434 <csct3434@gmail.com>
2023-07-18 23:28:51 +0900

    feat(RequestHandler): HTTP Request 메시지 로깅 기능 추가

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -7,10 +7,7 @@ import webserver.http.HttpRequest;
 import webserver.http.HttpResponse;
 import webserver.utils.FileUtils;
 
-import java.io.BufferedOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 
 public class RequestHandler implements Runnable {
@@ -26,7 +23,9 @@ public class RequestHandler implements Runnable {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+        try (BufferedInputStream in = new BufferedInputStream(connection.getInputStream());
+             OutputStream out = connection.getOutputStream()) {
+            logRequestMessageAndResetStream(in);
             HttpRequest httpRequest = new HttpRequest(in);
             HttpResponse httpResponse = new HttpResponse();
 
@@ -36,6 +35,23 @@ public class RequestHandler implements Runnable {
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
+    }
+
+    private void logRequestMessageAndResetStream(BufferedInputStream in) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+        String line;
+
+        in.mark(0);
+
+        while (bufferedReader.ready()) {
+            line = bufferedReader.readLine();
+            stringBuilder.append(line).append('\n');
+        }
+
+        in.reset();
+
+        logger.debug("{}", stringBuilder.toString());
     }
 
     private void sendResponse(HttpResponse httpResponse, OutputStream out) throws IOException {
