ed623c2f3f7b40970555de5dfe92cf00a0d53b83
dohyeon-han <limited.hyeon@gmail.com>
2023-07-18 23:26:51 +0900

    refactor: HttpRequest의 유틸리티 메소드를 HttpUtils로 이동

--- src/main/java/http/HttpRequest.java
+++ src/main/java/http/HttpRequest.java
@@ -1,17 +1,12 @@
 package http;
 
 import util.HttpUtils;
-import util.StringUtils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URLDecoder;
 import java.net.http.HttpClient;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
 
 public class HttpRequest {
@@ -26,41 +21,12 @@ public class HttpRequest {
     public HttpRequest(BufferedReader reader) throws URISyntaxException, IOException {
         String requestLine = reader.readLine();
         String[] requestParts = requestLine.split(" ");
-        this.headers = parseHeader(reader);
+        this.headers = HttpUtils.parseHeader(reader);
         this.method = HttpUtils.Method.of(requestParts[0]);
-        this.uri = constructUri(requestParts[1], headers);
+        this.uri = HttpUtils.constructUri(requestParts[1], headers);
         this.version = HttpUtils.getHttpVersion(requestParts[2]).orElse(null);
-        this.mime = decideMime(this.uri.getPath());
-        this.body = parseBody(reader);
-    }
-
-    private Map<String, String> parseHeader(BufferedReader reader) throws IOException {
-        Map<String, String> map = new HashMap<>();
-        String line;
-        while ((line = reader.readLine()) != null && !line.isEmpty()) {
-            int colonIndex = line.indexOf(':');
-            if (colonIndex != -1) {
-                String headerName = line.substring(0, colonIndex).trim();
-                String headerValue = line.substring(colonIndex + 1).trim();
-                map.put(headerName, headerValue);
-            }
-        }
-        return map;
-    }
-
-    private String parseBody(BufferedReader reader) throws IOException {
-        int contentLength = Integer.parseInt(headers.getOrDefault("Content-Length", "0"));
-        if (this.method.equals(HttpUtils.Method.GET) || contentLength == 0) {
-            return null;
-        }
-        char[] buffer = new char[contentLength];
-        reader.read(buffer);
-        return URLDecoder.decode(new String(buffer), StandardCharsets.UTF_8);
-    }
-
-    private URI constructUri(String file, Map<String, String> headers) throws URISyntaxException {
-        String host = headers.get("Host");
-        return new URI("http://" + host + URLDecoder.decode(file, StandardCharsets.UTF_8));
+        this.mime = HttpUtils.decideMime(this.uri.getPath());
+        this.body = HttpUtils.parseBody(reader, this.headers, this.method);
     }
 
     public HttpUtils.Method method() {
@@ -89,14 +55,5 @@ public class HttpRequest {
 
     public String getBody() {
         return this.body;
-    }
-
-    private Mime decideMime(String path) {
-        String extension = StringUtils.getExtension(path);
-
-        return Arrays.stream(Mime.values())
-                .filter(mime -> mime.getExtension().equals(extension))
-                .findFirst()
-                .orElse(Mime.DEFAULT);
     }
 }
--- src/main/java/util/HttpUtils.java
+++ src/main/java/util/HttpUtils.java
@@ -1,7 +1,17 @@
 package util;
 
+import http.Mime;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLDecoder;
 import java.net.http.HttpClient;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 public class HttpUtils {
@@ -24,4 +34,42 @@ public class HttpUtils {
         return Optional.empty();
     }
 
+
+    public static Map<String, String> parseHeader(BufferedReader reader) throws IOException {
+        Map<String, String> map = new HashMap<>();
+        String line;
+        while ((line = reader.readLine()) != null && !line.isEmpty()) {
+            int colonIndex = line.indexOf(':');
+            if (colonIndex != -1) {
+                String headerName = line.substring(0, colonIndex).trim();
+                String headerValue = line.substring(colonIndex + 1).trim();
+                map.put(headerName, headerValue);
+            }
+        }
+        return map;
+    }
+
+    public static String parseBody(BufferedReader reader, Map<String, String> headers, Method method) throws IOException {
+        int contentLength = Integer.parseInt(headers.getOrDefault("Content-Length", "0"));
+        if (method.equals(HttpUtils.Method.GET) || contentLength == 0) {
+            return null;
+        }
+        char[] buffer = new char[contentLength];
+        reader.read(buffer);
+        return URLDecoder.decode(new String(buffer), StandardCharsets.UTF_8);
+    }
+
+    public static URI constructUri(String file, Map<String, String> headers) throws URISyntaxException {
+        String host = headers.get("Host");
+        return new URI("http://" + host + URLDecoder.decode(file, StandardCharsets.UTF_8));
+    }
+
+    public static Mime decideMime(String path) {
+        String extension = StringUtils.getExtension(path);
+
+        return Arrays.stream(Mime.values())
+                .filter(mime -> mime.getExtension().equals(extension))
+                .findFirst()
+                .orElse(Mime.DEFAULT);
+    }
 }
