e663fe62c758cbdac25a6af4ca7a92ee8d26946a
dohyeon-han <limited.hyeon@gmail.com>
2023-07-18 13:50:14 +0900

    test: HttpRequest가 BufferedReader를 인자로 받도록 변경

--- src/test/java/http/HttpRequestTest.java
+++ src/test/java/http/HttpRequestTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import util.HttpUtils;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
@@ -24,9 +26,10 @@ class HttpRequestTest {
                 "Host: localhost:8080",
                 "Connection: keep-alive",
                 "Accept: */*");
+        BufferedReader bufferedReader = stringListToBufferedReader(strings);
 
         //when
-        HttpRequest httpRequest = new HttpRequest(strings);
+        HttpRequest httpRequest = new HttpRequest(bufferedReader);
 
         //then
         URI expectedUri = new URI("http://localhost:8080/user/create?userId=javajigi&password=password&name=박재성&email=javajigi@slipp.net");
@@ -40,15 +43,16 @@ class HttpRequestTest {
 
     @Test
     @DisplayName("PNG HttpRequest 객체를 생성한다.")
-    void createPngHttpRequest() throws URISyntaxException {
+    void createPngHttpRequest() throws URISyntaxException, IOException {
         //given
         List<String> strings = List.of("GET /user/create.png HTTP/1.1",
                 "Host: localhost:8080",
                 "Connection: keep-alive",
                 "Accept: */*");
+        BufferedReader bufferedReader = stringListToBufferedReader(strings);
 
         //when
-        HttpRequest httpRequest = new HttpRequest(strings);
+        HttpRequest httpRequest = new HttpRequest(bufferedReader);
 
         //then
         URI expectedUri = new URI("http://localhost:8080/user/create.png");
@@ -69,10 +73,11 @@ class HttpRequestTest {
                 "Host: localhost:8080",
                 "Connection: keep-alive",
                 "Accept: */*");
-        HttpRequest httpRequest = new HttpRequest(strings);
+        BufferedReader bufferedReader = stringListToBufferedReader(strings);
+        HttpRequest httpRequest = new HttpRequest(bufferedReader);
 
         //when
-        Map<String, String> parameters = httpRequest.parameters();
+        Map<String, String> parameters = httpRequest.parameters(httpRequest.uri().getQuery());
 
         //then
         SoftAssertions.assertSoftly(softAssertions -> {
@@ -92,12 +97,22 @@ class HttpRequestTest {
                 "Host: localhost:8080",
                 "Connection: keep-alive",
                 "Accept: */*");
-        HttpRequest httpRequest = new HttpRequest(strings);
+        BufferedReader bufferedReader = stringListToBufferedReader(strings);
+        HttpRequest httpRequest = new HttpRequest(bufferedReader);
 
         //when
-        Map<String, String> parameters = httpRequest.parameters();
+        Map<String, String> parameters = httpRequest.parameters(httpRequest.uri().getQuery());
 
         //then
         assertThat(parameters).isEmpty();
+    }
+
+    private BufferedReader stringListToBufferedReader(List<String> strings) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String str : strings) {
+            stringBuilder.append(str).append(System.lineSeparator());
+        }
+        String stringData = stringBuilder.toString();
+        return new BufferedReader(new StringReader(stringData));
     }
 }
