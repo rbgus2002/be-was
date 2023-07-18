d58eaaef4aa3ba73d34b2bc55438f23de0d4c46b
ijehyunpark <adam991217@naver.com>
2023-07-18 14:00:27 +0900

    Feat: ClassListener 개선
    ClassListener가 이제 디렉토리의 경우에도 순회하며 하위 디렉토리 탐색이 가능해졌습니다.

--- src/main/java/utils/ClassListener.java
+++ src/main/java/utils/ClassListener.java
@@ -1,9 +1,5 @@
 package utils;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import webserver.RequestHandler;
-
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,38 +7,49 @@ import java.util.Objects;
 
 public abstract class ClassListener {
 
-    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
-
+    /**
+     * 패키지내부에 작성된 모든 클래스의 이름을 가져온다.
+     *
+     * @param packageName 클래스를 검색할 package 이름을 나타냅니다. <br />
+     *                    주로 "package.name.subname..." 형식으로 작성하는 것을 권장하며 '.' 대신 '/'를 작성하여도 무방합니다.
+     */
     public static List<Class<?>> scanClass(String packageName) {
         String path = packageName.replace('.', '/');
+
         List<Class<?>> classes = new ArrayList<>();
 
-        try {
-            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-            File packageDirectory = new File(Objects.requireNonNull(classLoader.getResource(path)).getFile());
-
-            if (packageDirectory.exists() && packageDirectory.isDirectory()) {
-                File[] files = packageDirectory.listFiles();
-
-                assert files != null;
-                for (File file : files) {
-                    if (file.isFile() && file.getName().endsWith(".class")) {
-                        String className = packageName + '.' + file.getName().substring(0, file.getName().length() - 6);
-
-                        try {
-                            Class<?> clazz = Class.forName(className);
-                            classes.add(clazz);
-                        } catch (ClassNotFoundException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            }
-        } catch (NullPointerException e) {
-            logger.error("존재하지 않는 패키지 입니다. : {}", packageName);
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        if (classLoader.getResource(path) != null) {
+            scanClassesInDirectory(path, classes, classLoader);
         }
 
         return classes;
+    }
+
+    private static void scanClassesInDirectory(String path, List<Class<?>> classes, ClassLoader classLoader) {
+        File file = new File(Objects.requireNonNull(classLoader.getResource(path)).getFile());
+
+        // 디렉토리일 경우 해당 디렉토리를 순회한다.
+        if (file.exists() && file.isDirectory()) {
+            File[] directory = file.listFiles();
+            if (directory != null) {
+                for (File fileInDirectory : directory) {
+                    scanClassesInDirectory(("".equals(path) ? path : path + "/") + fileInDirectory.getName(), classes, classLoader);
+                }
+            }
+            return;
+        }
+
+        // 파일 일 경우 클래스 경로에 추가한다.
+        if (file.isFile() && file.getName().endsWith(".class")) {
+            String className = path.substring(0, path.length() - 6).replace('/', '.');
+            try {
+                Class<?> clazz = Class.forName(className);
+                classes.add(clazz);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
