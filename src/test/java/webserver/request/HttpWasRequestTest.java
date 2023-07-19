e705740a5b14e8b6d2f4bf157f6ee71dabd7fa1c
kwyoohae <gocksdl7979@gmail.com>
2023-07-19 13:36:34 +0900

    test: x-www-form-urlencoded인 경우 parameter로 파싱이 되는지 확인하는 테스트 작성

--- src/test/java/webserver/request/HttpWasRequestTest.java
+++ src/test/java/webserver/request/HttpWasRequestTest.java
@@ -47,4 +47,29 @@ class HttpWasRequestTest {
 		softAssertions.assertThat(httpWasRequest.getAttribute("Host")).as("host 테스트").isEqualTo("localhost:8080");
 		softAssertions.assertThat(httpWasRequest.getAttribute("Accept")).as("Accept 테스트").isEqualTo("*/*");
 	}
+
+	@Test
+	@DisplayName("x-www-form-urlencoded는 body의 값을 제대로 파싱할 수 있어야한다")
+	void parseXWwwFormInBody() throws IOException {
+		// given
+		String request = "POST /user/create HTTP/1.1 \r\n"
+			+ "Host: localhost:8080\r\n"
+			+ "Connection: keep-alive\r\n"
+			+ "Accept: */*\r\n"
+			+ "\r\n"
+			+ "userId=haechan&password=password&name=%EC%9C%A0%ED%95%B4%EC%B0%AC&email=a%40naver.com ";
+		final ByteArrayInputStream inputStream = new ByteArrayInputStream(request.getBytes());
+
+		// when
+		final HttpWasRequest httpWasRequest = new HttpWasRequest(inputStream);
+
+		// then
+		SoftAssertions softAssertions = new SoftAssertions();
+		softAssertions.assertThat(httpWasRequest.getParameter("userId")).as("userId 테스트").isEqualTo("haechan");
+		softAssertions.assertThat(httpWasRequest.getParameter("password")).as("password 테스트").isEqualTo("password");
+		softAssertions.assertThat(httpWasRequest.getParameter("name")).as("name 테스트").isEqualTo("유해찬");
+		softAssertions.assertThat(httpWasRequest.getParameter("email")).as("email 테스트").isEqualTo("a@naver.com");
+		softAssertions.assertThat(httpWasRequest.getAttribute("Host")).as("host 테스트").isEqualTo("localhost:8080");
+		softAssertions.assertThat(httpWasRequest.getAttribute("Accept")).as("Accept 테스트").isEqualTo("*/*");
+	}
 }
