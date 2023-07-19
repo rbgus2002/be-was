7531e883ae7799ca61d6d447374f88509acf61e1
kimahhh <kimahyunn132@gmail.com>
2023-07-19 18:33:45 +0900

    Test: 회원가입 시 정보를 모두 입력하지 않는 경우 예외 처리
    + softAssertions 적용

--- src/test/java/controller/ControllerTest.java
+++ src/test/java/controller/ControllerTest.java
@@ -4,9 +4,7 @@ import db.Database;
 import http.HttpRequest;
 import http.HttpResponse;
 import org.assertj.core.api.SoftAssertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static db.Database.clear;
 import static http.HttpMethod.GET;
@@ -18,23 +16,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class ControllerTest {
 
     Controller controller;
+    SoftAssertions softAssertions;
 
     @BeforeEach
     void setup() {
         controller = new Controller();
+        softAssertions = new SoftAssertions();
     }
 
-    @Test
-    @DisplayName("적절한 확장자가 아닌 잘못된 요청에 NOT_FOUND 404 상태를 반환해야 한다")
-    void wrongRequestNoExtension() {
-        // Given
-        HttpRequest httpRequest = new HttpRequest.RequestBuilder(GET, "/index.htm1", "HTTP/1.1").build();
-
-        // When
-        HttpResponse httpResponse = controller.loadFileByRequest(httpRequest).build();
-
-        // Then
-        assertThat(httpResponse.getHttpStatus()).isEqualTo(NOT_FOUND);
+    @AfterEach
+    void after() {
+        softAssertions.assertAll();
     }
 
     @Test
@@ -85,20 +77,40 @@ class ControllerTest {
     void signUp() {
         // Given
         clear();
-        String uri = "/user/create?userId=kimahhh&password=1234&name=김아현&email=kimahyunn132@gmail.com";
-        HttpRequest httpRequest = new HttpRequest.RequestBuilder("GET", uri, "HTTP/1.1").build();
+        String uri = "/user/create";
+        String body = "userId=kimahhh&password=1234&name=김아현&email=kimahyunn132@gmail.com";
+        HttpRequest httpRequest = new HttpRequest.RequestBuilder("POST", uri, "HTTP/1.1")
+                .setBody(body)
+                .build();
         Controller controller = new Controller();
 
         // When
         controller.loadFileByRequest(httpRequest);
 
         // Then
-        SoftAssertions softAssertions = new SoftAssertions();
         softAssertions.assertThat(Database.findAll().size())
                 .as("Database의 크기가 1이 아닙니다.\n현재 값: %d", Database.findAll().size()).isEqualTo(1);
         softAssertions.assertThat(Database.findUserById("kimahhh").getName())
                 .as("Id로 이름을 찾을 수 없습니다.\n 현재 값: %s", Database.findUserById("kimahhh").getName()).isEqualTo("김아현");
-        softAssertions.assertAll();
+    }
+    @Test
+    @DisplayName("유저가 회원가입 시 정보를 모두 입력하지 않는 경우, NOT_FOUND 404 상태를 반환해야 한다")
+    void notEnoughInformation() {
+        // Given
+        clear();
+        String uri = "/user/create";
+        String body = "userId=kimahhh&password=&name=김아현&email=kimahyunn132@gmail.com";
+        HttpRequest httpRequest = new HttpRequest.RequestBuilder("POST", uri, "HTTP/1.1")
+                .setBody(body)
+                .build();
+        Controller controller = new Controller();
+
+        // When
+        HttpResponse httpResponse = controller.loadFileByRequest(httpRequest).build();
+
+        // Then
+        softAssertions.assertThat(httpResponse.getHttpStatus())
+                .as("Http Status 상태가 404 Not Found가 아닙니다.\n현재 값: %s", httpResponse.getHttpStatus()).isEqualTo(NOT_FOUND);
     }
 
 }
