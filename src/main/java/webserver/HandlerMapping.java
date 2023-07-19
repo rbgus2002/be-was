3242f0c71a8a0b1eff28a3e3c7d14d1ba06872c0
Jaehong Kim <kjhonggg95@gmail.com>
2023-07-19 21:52:16 +0900

    feat: Http request 를 받아 해당 요청을 처리하는 메서드를 반환하는 HandlerMapping 구현
    지금은 Method 를 반환하고 있지만 추후에 HandlerAdapter 역할을 할 객체로 리팩토링 예정.

---
+++ src/main/java/webserver/HandlerMapping.java
@@ -0,0 +1,42 @@
+package webserver;
+
+import annotation.RequestMapping;
+import controller.UserController;
+import http.HttpMethod;
+import http.HttpRequest;
+import http.HttpResponse;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HandlerMapping {
+
+    public static final Map<HandlerKey, Method> mappings = new HashMap<>();
+
+    static {
+        Method[] methods = UserController.class.getDeclaredMethods();
+        for(Method method : methods) {
+            if(method.isAnnotationPresent(RequestMapping.class)) {
+                RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+                String path = annotation.path();
+                HttpMethod httpMethod = annotation.method();
+                HandlerKey handlerKey = new HandlerKey(path, httpMethod);
+                mappings.put(handlerKey, method);
+            }
+        }
+    }
+
+    public static Method getHandler(HttpRequest request) {
+        String path = request.getPath();
+        HttpMethod httpMethod = HttpMethod.resolve(request.getMethod());
+        HandlerKey handlerKey = new HandlerKey(path, httpMethod);
+
+        if(mappings.containsKey(handlerKey)) {
+            return mappings.get(handlerKey);
+        }
+
+        // TODO: 핸들러 없을 경우 예외처리
+        throw new IllegalArgumentException();
+    }
+}
