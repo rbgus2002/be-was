d66991ae4d008f917a6918cdc600cfb8487daaed
Hypersand <on8214@gmail.com>
2023-07-19 10:27:01 +0900

    feat: Parser 클래스의 쿼리 스트링 파싱 스태틱 메서드 구현

--- src/main/java/utils/Parser.java
+++ src/main/java/utils/Parser.java
@@ -1,11 +1,15 @@
 package utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Parser {
+    private static final Logger log = LoggerFactory.getLogger(Parser.class);
 
     public static Map<String, String> parseRequestLine(String requestLine) {
         String[] tokens = requestLine.split(" ");
@@ -20,11 +24,24 @@ public class Parser {
         String line;
         Map<String, String> requestHeaders = new HashMap<>();
 
-        while((line = br.readLine()) != null && !line.isEmpty()) {
+        while ((line = br.readLine()) != null && !line.isEmpty()) {
             String[] header = line.split(":");
             requestHeaders.put(header[0], header[1]);
         }
 
         return requestHeaders;
+    }
+
+    public static Map<String, String> parseQueryParameters(String requestUri) {
+        log.debug("requestUri = {}", requestUri);
+        Map<String, String> queryParams = new HashMap<>();
+        String queryString = requestUri.substring(requestUri.indexOf("?") + 1);
+        String[] params = queryString.split("&");
+
+        for (String param : params) {
+            int splitIndex = param.indexOf("=");
+            queryParams.put(param.substring(0, splitIndex), param.substring(splitIndex + 1));
+        }
+        return queryParams;
     }
 }
