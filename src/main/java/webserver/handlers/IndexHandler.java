c5d60861a9abaaf9bbc7e47b40d2600fe21ee234
윤상현 <yshyeon102@gmail.com>
2023-07-19 13:19:56 +0900

    refactor: IndexHandler가 갖고 있던 패키지 경로 정보를 IndexHandler가 갖도록 함.

--- src/main/java/webserver/handlers/IndexHandler.java
+++ src/main/java/webserver/handlers/IndexHandler.java
@@ -5,10 +5,10 @@ import webserver.http.message.HttpResponse;
 import webserver.utils.FileUtils;
 
 public class IndexHandler implements Handler {
-    private static final String INDEX_FILE_PATH = "src/main/resources/templates/index.html";
     @Override
     public HttpResponse handle(HttpRequest request) {
-        byte[] file = FileUtils.readFile(INDEX_FILE_PATH);
+        String path = request.getURL().getPath();
+        byte[] file = FileUtils.readFileFromTemplate(path);
         return HttpResponse.okWithFile(file);
     }
 }
