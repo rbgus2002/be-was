cd6c9a74f0d420563a7f1cbe28dc63a93a191219
ddingmin <ddingminyt@gmail.com>
2023-07-19 09:04:10 +0900

    feat: "HttpURL 클래스 구현."
    - String 형태가 아닌 클래스로 구현.
    - parameter, path, extension 을 필드로 가진다.

--- src/main/java/webserver/request/HttpRequestParser.java
+++ src/main/java/webserver/request/HttpRequestParser.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.URLDecoder;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.IntStream;
@@ -40,7 +41,7 @@ public class HttpRequestParser {
         }
 
         String method = statusLine[0];
-        String url = statusLine[1];
+        HttpURL url = HttpRequestParser.parseUrl(URLDecoder.decode(statusLine[1]));
         String version = statusLine[2];
 
         Map<String, String> headers = new HashMap<>();
@@ -50,5 +51,45 @@ public class HttpRequestParser {
         });
 
         return new HttpRequest(method, url, version, headers, body);
+    }
+
+    public static HttpURL parseUrl(String url) {
+        int queryIndex = url.indexOf("?");
+
+        String path = extractPath(url, queryIndex);
+        String extension = extractExtension(path);
+        Map<String, String> parameters = extractParameters(url, queryIndex);
+
+        return new HttpURL(url, extension, path, parameters);
+    }
+
+    private static Map<String, String> extractParameters(String url, int queryIndex) {
+        Map<String, String> parameters = null;
+
+        if (queryIndex != -1) {
+            String queryString = url.substring(queryIndex + 1);
+            parameters = new HashMap<>();
+            String[] tokens = queryString.split("&");
+            for (String parameter : tokens) {
+                int splitIndex = parameter.indexOf("=");
+                parameters.put(parameter.substring(0, splitIndex), parameter.substring(splitIndex + 1));
+            }
+        }
+        return parameters;
+    }
+
+    private static String extractPath(String url, int queryIndex) {
+        if (queryIndex != -1) {
+            return url.substring(0, queryIndex);
+        }
+        return url;
+    }
+
+    private static String extractExtension(String path) {
+        int extensionIndex = path.indexOf(".");
+        if (extensionIndex != -1) {
+            return path.substring(extensionIndex);
+        }
+        return null;
     }
 }
