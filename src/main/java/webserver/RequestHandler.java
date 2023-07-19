c3b80d7094f30b5464649ae834422706b4dd89d0
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-19 22:07:16 +0900

    refactor: 선언한 클래스들을 이용하여 불필요한 부분 제거

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -5,8 +5,10 @@ import java.net.Socket;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import webserver.request.HttpRequest;
+import webserver.response.HttpResponse;
+import webserver.response.HttpStatus;
 
-import static java.nio.charset.StandardCharsets.*;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -22,39 +24,14 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            BufferedReader br = new BufferedReader(new InputStreamReader(in, UTF_8));
-            String line = br.readLine();
-            logger.debug("request line: {}", line);
 
-            while(!line.equals("")) {
-                line = br.readLine();
-                logger.debug("header: {}", line);
-            }
+            HttpRequest httpRequest = HttpRequest.of(in);
 
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
+            HttpResponse httpResponse = new HttpResponse(HttpStatus.OK, ContentType.HTML, body.length, body);
+            httpResponse.sendResponse(dos);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
