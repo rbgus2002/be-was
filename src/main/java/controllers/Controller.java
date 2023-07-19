09b99317d6c8882ea4c409a979ac812443974a38
socra167 <socra167@gmail.com>
2023-07-19 11:40:52 +0900

    feat: create 엔드포인트 컨트롤러 생성

--- src/main/java/controllers/Controller.java
+++ src/main/java/controllers/Controller.java
@@ -1,11 +1,18 @@
 package controllers;
 
 import annotations.GetMapping;
+import annotations.PostMapping;
+import annotations.RequestParam;
 
 public class Controller {
 
 	@GetMapping(path = "/")
 	public String getIndex() {
 		return "index.html";
+	}
+
+	@GetMapping(path = "/create")
+	public String createUser(@RequestParam String userId, @RequestParam String password, @RequestParam String name, @RequestParam String email) {
+		return "";
 	}
 }
