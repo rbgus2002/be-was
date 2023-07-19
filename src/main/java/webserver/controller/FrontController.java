91404c1ad79ed23d1c1dc96f738038cc8d32411e
Hypersand <on8214@gmail.com>
2023-07-19 11:29:34 +0900

    feat: FrontController를 통해 일차적으로 url에 맞는 핸들러와 매핑시켜준다

--- src/main/java/webserver/controller/FrontController.java
+++ src/main/java/webserver/controller/FrontController.java
@@ -1,17 +1,31 @@
 package webserver.controller;
 
-import webserver.http.response.HttpResponse;
 import webserver.http.request.HttpRequest;
+import webserver.http.response.HttpResponse;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
 
 public class FrontController {
-
     private final HttpRequest httpRequest;
-    private final HttpResponse httpResponse;
+    private final DataOutputStream dos;
 
-    public FrontController(HttpRequest httpRequest, HttpResponse httpResponse) {
+    public FrontController(HttpRequest httpRequest, DataOutputStream dos) throws IOException {
         this.httpRequest = httpRequest;
-        this.httpResponse = httpResponse;
+        this.dos = dos;
     }
 
+    public void doDispatch() throws IOException {
+        Controller controller = HandlerMapper.findHandler(httpRequest.getRequestUri());
+        if (controller == null) {
+            HttpResponse httpResponse = HttpResponse.createStatic(httpRequest.getRequestUri());
+            httpResponse.responseStatic(dos);
+            return;
+        }
+
+        HttpResponse httpResponse = HttpResponse.createRedirect();
+        controller.doGet(httpRequest, httpResponse);
+        httpResponse.responseDynamic(dos);
+    }
 
 }
