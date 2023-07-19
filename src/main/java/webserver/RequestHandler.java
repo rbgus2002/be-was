13680116b470c808c015816823e69930aa1dfcbd
ddingmin <ddingminyt@gmail.com>
2023-07-19 10:54:45 +0900

    refactor: "RequestHandler.java 기능 분리 완료."

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,13 +2,20 @@ package webserver;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import webserver.request.RequestMessage;
+import webserver.request.HttpRequest;
+import webserver.response.HttpResponse;
+import webserver.response.HttpStatus;
 
-import java.io.*;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 
-import static utils.StringUtils.NEWLINE;
+import static utils.StringUtils.NEW_LINE;
+import static webserver.request.HttpRequestParser.parseRequest;
 
 public class RequestHandler implements Runnable {
     public final String RESOURCE_PATH = "src/main/resources/templates";
@@ -22,51 +29,34 @@ public class RequestHandler implements Runnable {
     public void run() {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
-
+//
         try (InputStream in = connection.getInputStream();
-             OutputStream out = connection.getOutputStream();
-             InputStreamReader reader = new InputStreamReader(in);
-             BufferedReader bufferedReader = new BufferedReader(reader);
+             OutputStream out = connection.getOutputStream()
         ) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            StringBuilder sb = new StringBuilder();
-            String line = bufferedReader.readLine();
+            HttpRequest httpRequest = parseRequest(in);
+            printLog(httpRequest);
 
-            while (!line.equals("")) {
-                logger.debug("request message : {}", line);
-                sb.append(line).append(NEWLINE);
-                line = bufferedReader.readLine();
-            }
+            // 수행 로직 구현해서 분리
+            HttpResponse httpResponse = new HttpResponse(HttpStatus.OK, Files.readAllBytes(Paths.get(RESOURCE_PATH + httpRequest.getUrl().getPath())));
 
-            RequestMessage requestMessage = new RequestMessage(sb.toString());
-
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = Files.readAllBytes(new File(RESOURCE_PATH + requestMessage.getPath()).toPath());
-
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            response(out, httpResponse);
         } catch (IOException | IllegalArgumentException e) {
-            logger.error(e.getMessage());
+            logger.error(e.getLocalizedMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
+    private void printLog(HttpRequest httpRequest) {
+        logger.debug(httpRequest.getMethod());
+        logger.debug(httpRequest.getVersion());
+        logger.debug(httpRequest.getUrl().toString());
+        logger.debug(httpRequest.getBody());
     }
 
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
+    private void response(OutputStream out, HttpResponse httpResponse) throws IOException {
+        DataOutputStream dos = new DataOutputStream(out);
+        dos.writeBytes(httpResponse.getResponseHeader());
+        dos.writeBytes(NEW_LINE);
+        dos.write(httpResponse.getResponseBody());
+        dos.flush();
     }
 }
