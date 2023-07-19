1f88dcc8ab2c6a1cc59de70052323a23ccd729b7
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-19 22:05:08 +0900

    refactor: HttpRequest안으로 inputStream 내재화
    - 기존 생성자 private로 수정
    - InputStream을 parameter로 받아 인스턴스 생성하는 of 메소드 추가

--- src/main/java/webserver/request/HttpRequest.java
+++ src/main/java/webserver/request/HttpRequest.java
@@ -1,6 +1,14 @@
 package webserver.request;
 
+import utils.StringUtils;
 import webserver.Header;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class HttpRequest {
 
@@ -9,13 +17,27 @@ public class HttpRequest {
     private final String path;
     private final Header header;
 
-    public HttpRequest(String request, String header) {
+    private HttpRequest(String requestLine, String header) {
 
-        String[] tokens = request.split(" ");
+        String[] tokens = requestLine.split(" ");
 
         this.httpMethod = HttpMethod.valueOf(tokens[0]);
         this.path = tokens[1];
         this.version = tokens[2];
         this.header = Header.of(header);
+    }
+
+    public static HttpRequest of (InputStream in) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in, UTF_8));
+        String line = br.readLine();
+        String requestLine = line;
+
+        StringBuilder header = new StringBuilder();
+        while(!line.equals("")) {
+            line = br.readLine();
+            header.append(line).append(StringUtils.NEWLINE);
+        }
+
+        return new HttpRequest(requestLine, header.toString());
     }
 }
