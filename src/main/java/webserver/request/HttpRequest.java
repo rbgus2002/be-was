384e373d47f05676f080f75e649a603648112f5b
ddingmin <ddingminyt@gmail.com>
2023-07-19 09:05:20 +0900

    rename: "MetaData 대신 headers 으로 변경."

--- src/main/java/webserver/request/HttpRequest.java
+++ src/main/java/webserver/request/HttpRequest.java
@@ -4,12 +4,12 @@ import java.util.Map;
 
 public class HttpRequest {
     private final String method;
-    private final String url;
+    private final HttpURL url;
     private final String version;
     private final String body;
     private final Map<String, String> headers;
 
-    public HttpRequest(String method, String url, String version, Map<String, String> headers, String body) {
+    public HttpRequest(String method, HttpURL url, String version, Map<String, String> headers, String body) {
         this.method = method;
         this.url = url;
         this.version = version;
@@ -21,7 +21,7 @@ public class HttpRequest {
         return method;
     }
 
-    public String getUrl() {
+    public HttpURL getUrl() {
         return url;
     }
 
@@ -29,7 +29,7 @@ public class HttpRequest {
         return version;
     }
 
-    public String getMetaData(String key) {
+    public String getHeader(String key) {
         return headers.get(key);
     }
 
