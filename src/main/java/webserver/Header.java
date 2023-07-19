04f2928e7f0976f2f5a0847616beadf2eae70308
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-19 19:46:44 +0900

    feat: Header 정보 저장할 클래스 구현

---
+++ src/main/java/webserver/Header.java
@@ -0,0 +1,24 @@
+package webserver;
+
+import utils.StringUtils;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Header {
+
+    private Header(HashMap<String, String> headers) {
+    }
+
+    public static Header of(String string) {
+        String[] tokens = string.split(StringUtils.NEWLINE);
+
+        HashMap<String, String> headers = new HashMap<>();
+
+        Arrays.stream(tokens)
+                .map(token -> token.split(":"))
+                .forEach(header -> headers.put(header[0].trim(), header[1].trim()));
+
+        return new Header(headers);
+    }
+}
