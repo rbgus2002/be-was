f8ca8da3e5b14fc5ae26b4664cd50e755e82fae8
Hypersand <on8214@gmail.com>
2023-07-19 10:26:20 +0900

    feat: 쿼리스트링을 파싱하고 유저 객체를 저장하는 UserController 구현

--- src/main/java/webserver/controller/UserController.java
+++ src/main/java/webserver/controller/UserController.java
@@ -1,11 +1,27 @@
 package webserver.controller;
 
-import webserver.http.response.HttpResponse;
+import db.Database;
+import model.User;
+import utils.Parser;
 import webserver.http.request.HttpRequest;
+import webserver.http.response.HttpResponse;
+
+import java.util.Map;
 
 public class UserController implements Controller {
     @Override
     public void doGet(HttpRequest httpRequest, HttpResponse httpResponse) {
+        Map<String, String> queryParams = Parser.parseQueryParameters(httpRequest.getRequestUri());
+        String userId = queryParams.get("userId");
+        String password = queryParams.get("password");
+        String name = queryParams.get("name");
+        String email = queryParams.get("email");
 
+        if (userId == null || password == null || name == null || email == null) {
+            throw new IllegalArgumentException("입력을 제대로 해주세요!");
+        }
+
+        User user = new User(userId, password, name, email);
+        Database.addUser(user);
     }
 }
