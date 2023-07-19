15cf5dae38ae357912d96285bb4eea146f0ff4ff
Hypersand <on8214@gmail.com>
2023-07-19 13:09:45 +0900

    refactor: 사용하지 않는 doPost 메서드 제거

--- src/main/java/webserver/controller/Controller.java
+++ src/main/java/webserver/controller/Controller.java
@@ -3,7 +3,9 @@ package webserver.controller;
 import webserver.http.response.HttpResponse;
 import webserver.http.request.HttpRequest;
 
+import java.io.UnsupportedEncodingException;
+
 public interface Controller {
 
-    void doGet(HttpRequest httpRequest, HttpResponse httpResponse);
+    void doGet(HttpRequest httpRequest, HttpResponse httpResponse) throws UnsupportedEncodingException;
 }
