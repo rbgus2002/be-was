7ce62ce7291e26f535c10e76950dbc470c386e28
SeoSiun <siun0816@hanyang.ac.kr>
2023-07-20 12:04:29 +0900

    feat: 각 요청에 맞는 핸들러 맵핑을 위해 HandlerMapper 구현
    Controller class의 method 중 @Handler annoation이 있으며,
    request의 method, path와 일치하는 method, path를 갖는 메소드를 찾아 리턴
    해당하는 핸들러가 없는 경우 getStaticResource를 리턴

---
+++ src/main/java/webserver/controller/HandlerMapper.java
@@ -0,0 +1,40 @@
+package webserver.controller;
+
+import webserver.annotaion.Handler;
+import webserver.http.request.HttpRequest;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class HandlerMapper {
+    private static Method[] methods;
+    private static Method getStaticResource;
+
+    static {
+        try {
+            methods = Controller.class.getMethods();
+            getStaticResource = Controller.class.getMethod("getStaticResource", HttpRequest.class);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Method getHandler(HttpRequest request) {
+        List<Method> filteredMethods = Arrays.stream(methods)
+                .filter(method -> isMatch(method, request)).collect(Collectors.toList());
+        if (filteredMethods.isEmpty()) {
+            return getStaticResource;
+        }
+        return filteredMethods.get(0);
+    }
+
+    private static boolean isMatch(Method method, HttpRequest request) {
+        if (method.isAnnotationPresent(Handler.class)) {
+            Handler annotation = method.getAnnotation(Handler.class);
+            return request.isMatchHandler(annotation.method(), annotation.path());
+        }
+        return false;
+    }
+}
