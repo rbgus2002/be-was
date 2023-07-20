1cdcbed623f564c269d8ac4eded317ec062c3d22
socra167 <socra167@gmail.com>
2023-07-20 11:30:06 +0900

    feat: addUser()시 동일한 userId가 존재하는 경우 예외가 발생하도록 함

--- src/main/java/db/Database.java
+++ src/main/java/db/Database.java
@@ -8,10 +8,18 @@ import java.util.Collection;
 import java.util.Map;
 
 public class Database {
+    public static final String USERID_ALREADY_EXISTS_MESSAGE = "이미 userId가 존재합니다.";
     private static Map<String, User> users = Maps.newHashMap();
 
     public static void addUser(User user) {
+        CheckDuplicateUserId(user.getUserId());
         users.put(user.getUserId(), user);
+    }
+
+    private static void CheckDuplicateUserId(final String userId) {
+        if (findUserById(userId) != null) {
+            throw new IllegalArgumentException(USERID_ALREADY_EXISTS_MESSAGE);
+        }
     }
 
     public static User findUserById(String userId) {
