db01b32887fa9a15d8cc710f7d5a53dd8dd856a3
Kwon Min A <min2010011@gmail.com>
2023-07-21 01:27:24 +0900

    feat(FrontController): 정적 파일 응답 기능 추가

--- src/main/java/webserver/http/controller/FrontController.java
+++ src/main/java/webserver/http/controller/FrontController.java
@@ -3,36 +3,54 @@ package webserver.http.controller;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
-import controller.Controller;
 import webserver.http.message.HttpRequest;
 import webserver.http.message.HttpResponse;
 import webserver.http.message.HttpStatus;
 
 public class FrontController {
 
+	private final List<String> resourceDirectoryPathList;
 	ControllerMapper controllerMapper = new ControllerMapper();
 	ViewResolver viewResolver = new ViewResolver();
 
+	public FrontController() {
+		resourceDirectoryPathList = new ArrayList<>();
+		resourceDirectoryPathList.add("src/main/resources/static");
+		resourceDirectoryPathList.add("src/main/resources/templates");
+	}
+
 	public void service(HttpRequest request, HttpResponse response) {
-		byte[] body = new byte[0];
-		// 1. url을 받고 이에 따른 컨트롤러를 리턴
-		Controller controller = controllerMapper.findController(request.getUrlPath());
-		// 2. 컨트롤러를 실행
+		// 파일 찾기
+		String path = request.getUrlPath();
+		Path filePath = findFilePath(path);
 		try {
-			if (controller == null) {
-				throw new IOException();
+			if (filePath == null) {
+				throw new IllegalArgumentException("Wrong path.");
 			}
-			String viewName = controller.process(request, response);
-			// 3. 컨트롤러에서 뷰 네임을 리턴하면, 이를 물리 이름으로 변환하는 메소드
-			String viewPath = viewResolver.resolveViewName(viewName);
-			// 4. 파일을 읽고 dos로 출력(응답 전송)
-			body = Files.readAllBytes(new File(viewPath).toPath());
+			byte[] body = Files.readAllBytes(filePath);
+			// 응답 메세지 작성하기
 			response.writeStatus(HttpStatus.OK);
-		} catch (IOException exception) {
+			response.writeBody(body, Files.probeContentType(filePath));
+		} catch (IllegalArgumentException | IOException exception) {
+			byte[] body = exception.getMessage().getBytes();
 			response.writeStatus(HttpStatus.NOT_FOUND);
+			response.writeBody(body, "text/plain");
 		}
-		response.writeBody(body);
+	}
+
+	private Path findFilePath(String path) {
+		File file;
+		for (String resourceDirectoryPath : resourceDirectoryPathList) {
+			file = new File(resourceDirectoryPath + path);
+			if (file.exists() && file.isFile()) {
+				return file.toPath();
+			}
+		}
+		return null;
 	}
 
 }
