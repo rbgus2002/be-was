1680f5097a16d1999c44e53b87fe059b2d3309b2
SyuA0529 <silbi3408@gmail.com>
2023-07-20 22:09:58 +0900

    feat: 회원가입시 중복 회원가입 확인 로직 추가

--- src/main/java/user/service/UserService.java
+++ src/main/java/user/service/UserService.java
@@ -4,10 +4,24 @@ import db.Database;
 import model.User;
 import webserver.myframework.requesthandler.annotation.Service;
 
+
 @Service
 public class UserService {
     public void signUp(String userId, String password, String name, String email) {
+        User findUser = Database.findUserById(userId);
+        if(findUser != null) {
+            throw new IllegalArgumentException();
+        }
+
         User user = new User(userId, password, name, email);
         Database.addUser(user);
+    }
+
+    public User signIn(String userId, String password) {
+        User user = Database.findUserById(userId);
+        if(user == null || !user.getPassword().equals(password)) {
+            return null;
+        }
+        return user;
     }
 }
