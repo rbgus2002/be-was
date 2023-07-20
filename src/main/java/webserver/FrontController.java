db9ef7752625121c39e2d39035edb30dcb5e5475
ajsthfldu <ajsthfldu@outlook.com>
2023-07-20 12:27:59 +0900

    fix: response status line 수정

--- src/main/java/webserver/FrontController.java
+++ src/main/java/webserver/FrontController.java
@@ -55,7 +55,7 @@ public class FrontController {
     }
 
     private void render(DataOutputStream dos, HttpResponse response) throws IOException {
-        dos.writeBytes(response.getVersion() + " " + response.getMethod() + response.getStatusMessage() + "\r\n");
+        dos.writeBytes(response.getVersion() + " " + response.getMethod() + " " + response.getStatusMessage() + "\r\n");
         Map<String, String> headers = response.getHeaders();
         for (Map.Entry<String, String> header : headers.entrySet()) {
             dos.writeBytes(header.getKey() + ": " + header.getValue() + "\r\n");
