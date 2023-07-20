61a1511794e8887061041cdfcce8882d7ec54cf4
김종원 Jongwon <tank3a@gmail.com>
2023-07-20 22:38:05 +0900

    FEATURE: HandlerMapping 클래스 생성
    Mapping되는 uri 메서드를 가지는 controller을 찾아 반환하는 클래스 생성

--- src/main/java/webserver/HandlerMapping.java
+++ src/main/java/webserver/HandlerMapping.java
@@ -1,13 +1,85 @@
 package webserver;
 
+import annotations.Controller;
+import annotations.RequestMapping;
 import model.HttpRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public class HandlerMapping {
 
-    //TODO: getHandler(HttpRequest) 각 컨트롤러의 메서드를 검사해 일치하는 컨트롤러 찾기
+    private static final String ROOT_PATH = "src/main";
+    private static final Logger logger = LoggerFactory.getLogger(HandlerMapping.class);
+
     protected Object getHandler(HttpRequest request) {
+
+        Set<Class<?>> controllers = reflection(ROOT_PATH);
+
         String uri = request.getRequestURI();
 
-        return null;
+        //TODO: 예외처리 부분 404 페이지 반환하도록 수정
+        if(controllers.isEmpty()) {
+            return null;
+        }
+
+        //TODO: 예외처리 부분 404 페이지 반환하도록 수정
+        return controllers.stream().filter(controller ->
+            Arrays.stream(controller.getDeclaredMethods()).anyMatch(method -> {
+                if(method.isAnnotationPresent(RequestMapping.class)) {
+                    return method.getAnnotation(RequestMapping.class).value().equals(uri);
+                }
+                return false;
+            })
+        ).findFirst().orElseThrow(() -> new RuntimeException("매핑되는 컨트롤러가 존재하지 않습니다."));
+    }
+
+    private Set<Class<?>> reflection(String path) {
+        Set<Class<?>> controllers = new HashSet<>();
+
+        File rootDir = new File(path);
+        String[] files = null;
+        if(rootDir.exists() && rootDir.isDirectory()) {
+            files = rootDir.list();
+        }
+
+        if(files == null) {
+            return Collections.emptySet();
+        }
+
+        for(String file : files) {
+            String subPath = path + File.separator + file;
+
+            File subFile = new File(subPath);
+
+            if(subFile.isDirectory()) {
+                controllers.addAll(reflection(subPath));
+                continue;
+            }
+
+            if(subPath.endsWith(".class")) {
+                String className = subPath.substring(0, subPath.length() - ".class".length());
+
+                try {
+                    Class<?> clazz = Class.forName(path.replace('/', '.') + "." + className);
+                    if(isController(clazz)) {
+                        controllers.add(clazz);
+                    }
+                } catch (ClassNotFoundException e) {
+                    logger.error(e.getMessage());
+                }
+            }
+        }
+
+        return controllers;
+    }
+
+    private boolean isController(Class<?> clazz) {
+        return clazz.isAnnotationPresent(Controller.class);
     }
 }
