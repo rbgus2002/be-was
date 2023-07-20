84b87b0ddc679aabe6d4b5039af6a25e2cb48ae5
kwyoohae <gocksdl7979@gmail.com>
2023-07-20 10:34:14 +0900

    feat: User가 제대로 값을 받았는지 검증하는 로직 작성

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -7,10 +7,22 @@ public class User {
     private String email;
 
     public User(String userId, String password, String name, String email) {
+        verifyUser();
         this.userId = userId;
         this.password = password;
         this.name = name;
         this.email = email;
+    }
+
+    public void verifyUser() {
+       if (userId == null || userId.isBlank())
+           throw new IllegalArgumentException("User의 ID는 null이 될 수 없습니다");
+       if (password == null || password.isBlank())
+           throw new IllegalArgumentException("Password는 값이 비어있을 수 없습니다.");
+       if (name == null || name.isBlank())
+           throw new IllegalArgumentException("name은 값이 비어 있을 수 없습니다.");
+       if (email == null || email.isBlank())
+           throw new IllegalArgumentException("email은 값이 비어 있을 수 없습니다.");
     }
 
     public String getUserId() {
