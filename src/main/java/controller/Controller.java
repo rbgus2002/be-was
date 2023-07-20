bdb3efc5d373f778f753715d8edbcdc6f44ff440
plusjob70 <plusjob70@naver.com>
2023-07-21 03:31:40 +0900

    feat: 컨트롤러 변경
    - 인터페이스 상속 -> 어노테이션 매핑

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,10 +1,44 @@
 package controller;
 
-import modelview.ModelView;
+import annotation.RequestMapping;
+import common.enums.RequestMethod;
 import common.http.HttpRequest;
+import model.User;
+import modelview.ModelView;
+import service.UserService;
 
-public interface Controller {
-    ModelView process(HttpRequest request);
+import java.util.Map;
 
-    void validate(HttpRequest request);
+public class Controller {
+
+    @RequestMapping(method = RequestMethod.GET, path = "/")
+    public ModelView root(HttpRequest request) {
+        return new ModelView("Hello world");
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/index.html")
+    public ModelView index(HttpRequest request) {
+        return new ModelView("/index.html");
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/user/form.html")
+    public ModelView signUpForm(HttpRequest request) {
+        return new ModelView("/user/form.html");
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/user/create")
+    public ModelView createUser(HttpRequest request) {
+        Map<String, String> params = request.getParams();
+        User user = UserService.createUser(
+                params.get("userId"),
+                params.get("password"),
+                params.get("name"),
+                params.get("email")
+        );
+
+        ModelView mv = new ModelView("OK");
+        mv.setAttribute("user", user);
+        return mv;
+    }
+
 }
---
+++ src/main/java/webserver/ControllerMapper.java
@@ -0,0 +1,50 @@
+package webserver;
+
+import annotation.RequestMapping;
+import common.enums.RequestMethod;
+import exception.NotFoundControllerException;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+import static webserver.ServerConfig.CONTROLLER_CLASS;
+
+public class ControllerMapper {
+    private static final ControllerMapper INSTANCE = new ControllerMapper();
+    private final Map<RequestMethod, Map<String, Method>> map;
+
+    private ControllerMapper() {
+        map = new HashMap<>();
+        map.put(RequestMethod.GET, new HashMap<>());
+        map.put(RequestMethod.POST, new HashMap<>());
+        map.put(RequestMethod.PUT, new HashMap<>());
+        map.put(RequestMethod.DELETE, new HashMap<>());
+    }
+
+    public static ControllerMapper getInstance() {
+        return INSTANCE;
+    }
+
+    public void initialize() {
+        Method[] methods = CONTROLLER_CLASS.getMethods();
+
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(RequestMapping.class)) {
+                RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+
+                RequestMethod requestMethod = annotation.method();
+                String path = annotation.path();
+
+                map.get(requestMethod).put(path, method);
+            }
+        }
+    }
+
+    public Method getControllerMethod(RequestMethod requestMethod, String path) {
+        if (map.containsKey(requestMethod) && map.get(requestMethod).containsKey(path)) {
+            return map.get(requestMethod).get(path);
+        }
+        throw new NotFoundControllerException("처리할 컨트롤러 없음");
+    }
+}
