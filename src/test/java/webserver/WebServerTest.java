c38086c43fa7da3fe7934341d440ae1e16529b5d
ajsthfldu <ajsthfldu@outlook.com>
2023-07-21 18:02:55 +0900

    test: 회원가입 테스트 작성

--- src/test/java/webserver/WebServerTest.java
+++ src/test/java/webserver/WebServerTest.java
@@ -18,5 +18,12 @@ class WebServerTest {
 
     @Test
     void join() {
+        String requestBody = "userId=abc&password=abc&name=abc&email=abc%40abc";
+        given()
+                .body(requestBody)
+                .when()
+                .post("http://localhost:8080/user/create")
+                .then()
+                .statusCode(302);
     }
 }
