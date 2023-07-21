f0c14ad661515be863ef694ed27b6f7992cde2e1
jst0951 <jst0951@gmail.com>
2023-07-21 13:32:14 +0900

    feat: 동적 렌더링 controller로 이동(redirect 처리 추가 등 위해)

--- src/main/java/controller/FileController.java
+++ src/main/java/controller/FileController.java
@@ -5,13 +5,25 @@ import service.FileService;
 import webserver.model.Request;
 import webserver.model.Response;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 import static http.HttpUtil.*;
+import static service.FileService.readStaticFile;
+import static service.SessionService.getUserIdBySid;
 
 public class FileController {
+    private static final String STATIC_FILEPATH = "./src/main/resources/static";
+    private static final String TEMPLATE_FILEPATH = "./src/main/resources/templates";
+    private static final String NAVBAR_RIGHT = "<li><a href=\"./user/list.html\"><i class=\"glyphicon glyphicon-user\"></i></a></li>";
+    private static final String USERNAME_FORMAT = "<li>%s</li>";
+    private static final String BUTTON_LOGOUT = "<li><a href=\"#\" role=\"button\">로그아웃</a></li>";
+    private static final String BUTTON_LOGIN = "<li><a href=\"user/login.html\" role=\"button\">로그인</a></li>";
+    private static final String BUTTON_SIGNUP = "<li><a href=\"user/form.html\" role=\"button\">회원가입</a></li>";
+    private static final String BUTTON_MODIFY_USERDATA = "<li><a href=\"#\" role=\"button\">개인정보수정</a></li>";
+
     public static Response genereateResponse(Request request) throws IOException {
         String targetUri = request.getTargetUri();
 
@@ -19,16 +31,37 @@ public class FileController {
         String[] tokens = targetUri.split("\\.");
         String extension = tokens[tokens.length-1];
         HttpUtil.MIME mime = HttpUtil.MIME.getMimeByExtension(extension);
-        if (mime != null) {
-            byte[] body = FileService.loadFile(request);
+        if(mime == null) {
+            return null;
+        }
+        String targetPath;
+        byte[] body = null;
 
-            Map<String, String> headerMap = new HashMap<>();
-            headerMap.put(HEADER_CONTENT_TYPE, mime.getMime() + HEADER_CHARSET);
-            headerMap.put(HEADER_CONTENT_LENGTH, String.valueOf(body.length));
-
-            return new Response(STATUS.OK, headerMap, body);
+        targetPath = STATIC_FILEPATH + targetUri;
+        if(new File(targetPath).exists()) {
+            body = readStaticFile(targetPath);
+        }
+        targetPath = TEMPLATE_FILEPATH + targetUri;
+        if(new File(targetPath).exists()) {
+            String sid = request.getSid();
+            String httpDocument = new String(readStaticFile(TEMPLATE_FILEPATH + targetUri));
+            if(sid == null) {
+                httpDocument = httpDocument.replace(BUTTON_LOGOUT, "");
+                httpDocument = httpDocument.replace(BUTTON_MODIFY_USERDATA, "");
+            }
+            else {
+                httpDocument = httpDocument.replace(NAVBAR_RIGHT, NAVBAR_RIGHT +
+                        String.format(USERNAME_FORMAT, getUserIdBySid(sid)));
+                httpDocument = httpDocument.replace(BUTTON_LOGIN, "");
+                httpDocument = httpDocument.replace(BUTTON_SIGNUP, "");
+            }
+            body = httpDocument.getBytes();
         }
 
-        return null;
+        Map<String, String> headerMap = new HashMap<>();
+        headerMap.put(HEADER_CONTENT_TYPE, mime.getMime() + HEADER_CHARSET);
+        headerMap.put(HEADER_CONTENT_LENGTH, String.valueOf(body.length));
+
+        return new Response(STATUS.OK, headerMap, body);
     }
 }
--- src/main/java/service/FileService.java
+++ src/main/java/service/FileService.java
@@ -9,48 +9,8 @@ import static http.HttpUtil.*;
 import static service.SessionService.getUserIdBySid;
 
 public class FileService {
-    private static final String STATIC_FILEPATH = "./src/main/resources/static";
-    private static final String TEMPLATE_FILEPATH = "./src/main/resources/templates";
-    private static final String NAVBAR_RIGHT = "<li><a href=\"./user/list.html\"><i class=\"glyphicon glyphicon-user\"></i></a></li>";
-    private static final String USERNAME_FORMAT = "<li>%s</li>";
-    private static final String BUTTON_LOGOUT = "<li><a href=\"#\" role=\"button\">로그아웃</a></li>";
-    private static final String BUTTON_LOGIN = "<li><a href=\"user/login.html\" role=\"button\">로그인</a></li>";
-    private static final String BUTTON_SIGNUP = "<li><a href=\"user/form.html\" role=\"button\">회원가입</a></li>";
-    private static final String BUTTON_MODIFY_USERDATA = "<li><a href=\"#\" role=\"button\">개인정보수정</a></li>";
 
-    public static byte[] loadFile(Request request) throws IOException {
-        String targetUri = request.getTargetUri();
-        String targetPath;
-        byte[] body = null;
-
-        targetPath = STATIC_FILEPATH + targetUri;
-        if(new File(targetPath).exists()) {
-            body = readStaticFile(targetPath);
-        }
-        targetPath = TEMPLATE_FILEPATH + targetUri;
-        if(new File(targetPath).exists()) {
-            String sid = request.getSid();
-            body = renderTemplateFile(targetUri, sid);
-        }
-
-        return body;
-    }
-
-    private static byte[] readStaticFile(String route) throws IOException {
+    public static byte[] readStaticFile(String route) throws IOException {
         return Files.readAllBytes(new File(route).toPath());
-    }
-    private static byte[] renderTemplateFile(String targetUri, String sid) throws IOException {
-        String httpDocument = new String(readStaticFile(TEMPLATE_FILEPATH + targetUri));
-        if(sid == null) {
-            httpDocument = httpDocument.replace(BUTTON_LOGOUT, "");
-            httpDocument = httpDocument.replace(BUTTON_MODIFY_USERDATA, "");
-        }
-        else {
-            httpDocument = httpDocument.replace(NAVBAR_RIGHT, NAVBAR_RIGHT +
-                    String.format(USERNAME_FORMAT, getUserIdBySid(sid)));
-            httpDocument = httpDocument.replace(BUTTON_LOGIN, "");
-            httpDocument = httpDocument.replace(BUTTON_SIGNUP, "");
-        }
-        return httpDocument.getBytes();
     }
 }
