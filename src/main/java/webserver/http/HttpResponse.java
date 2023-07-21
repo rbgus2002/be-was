7f168d1cfb6c8a0e96c96fd90e221353a0ee37ed
Jeongyeon Kim <wjddus9132@gmail.com>
2023-07-21 13:54:49 +0900

    fix: 누락된 메서드 추가

--- src/main/java/webserver/http/HttpResponse.java
+++ src/main/java/webserver/http/HttpResponse.java
@@ -1,11 +1,38 @@
 package webserver.http;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 public class HttpResponse {
+
+    private static final Logger logger = LoggerFactory.getLogger(HttpResponse.class);
     private final String status;
     private final byte[] body;
 
     public HttpResponse(String status, byte[] body) {
         this.status = status;
         this.body = body;
+    }
+
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+    private void responseBody(DataOutputStream dos, byte[] body) {
+        try {
+            dos.write(body, 0, body.length);
+            dos.flush();
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
     }
 }
