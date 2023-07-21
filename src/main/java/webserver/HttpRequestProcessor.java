ed2043f5f16c57d377040453c0d001ef4ae33f42
윤상현 <yshyeon102@gmail.com>
2023-07-21 23:11:21 +0900

    fix: 크롬 브라우저에서 아이콘 이미지?를 제대로 읽어오지 못하는 문제 해결
    크롬 브라우저에서 아이콘? 이미지?를 제대로 읽지 못하는 문제 발생 -> Byte[] 타입의 file을 String 타입으로 바꾸고, 이후에 다시 Byte[]타입으로 바꾸면 Byte 배열의 값이 일부 바뀜. -> 약간의 데이터 손상이 일어남.
    따라서 HttpResponse를 String으로 바꾸지 않고 OutputStream에 바로 쓰도록, HttpResponseStringifer를 제거하고 OutputStream에 파일을 바로 보내줄 수 있도록 바꿈.

--- src/main/java/webserver/HttpRequestProcessor.java
+++ src/main/java/webserver/HttpRequestProcessor.java
@@ -3,20 +3,12 @@ package webserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import webserver.http.HttpRequestParser;
-import webserver.http.HttpResponseStringifier;
+import webserver.http.HttpResponseSender;
+import webserver.http.message.*;
 import webserver.utils.ByteReader;
-import webserver.http.message.HttpRequest;
-import webserver.http.message.HttpResponse;
-import webserver.http.message.HttpStatus;
-import webserver.http.message.HttpVersion;
 
 import java.io.*;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * 개별 스레드의 최상위 클래스로
@@ -25,19 +17,23 @@ import java.util.Map;
 public class HttpRequestProcessor {
     private final Logger logger = LoggerFactory.getLogger(HttpRequestProcessor.class);
     private final HttpRequestParser requestParser = new HttpRequestParser();
-    private final HttpResponseStringifier responseStringifier = new HttpResponseStringifier();
+    private final HttpResponseSender responseSender = new HttpResponseSender();
     private final HttpRequestHandler handler = new HttpRequestHandler();
 
     public void process(Socket connection) {
         try (InputStream inputStream = connection.getInputStream(); OutputStream outputStream = connection.getOutputStream()) {
+
             String requestMessage = getRequestMessage(inputStream);
-            if (isEmptyMessage(requestMessage)) return;
-            logger.debug("requestMessage : \n{}", requestMessage);
+            if (isEmptyMessage(requestMessage)) {
+                return;
+            }
             HttpRequest httpRequest = requestParser.parseHttpMessage(requestMessage);
+            logger.debug("requestMessage : \n{}", requestMessage);
 
             HttpResponse httpResponse = handler.handle(httpRequest);
-            String responseMessage = responseStringifier.stringify(httpResponse);
-            sendResponse(outputStream, responseMessage);
+
+            responseSender.sendResponse(outputStream, httpResponse);
+
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
@@ -45,17 +41,6 @@ public class HttpRequestProcessor {
 
     private static boolean isEmptyMessage(String requestMessage) {
         return requestMessage.equals("");
-    }
-
-    private void sendResponse(OutputStream outputStream, String responseMessage) {
-        try {
-            DataOutputStream dos = new DataOutputStream(outputStream);
-            byte[] bytes = responseMessage.getBytes(StandardCharsets.UTF_8);
-            dos.write(bytes);
-            dos.flush();
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
     }
 
     private String getRequestMessage(InputStream inputStream) throws IOException {
--- src/main/java/webserver/utils/ByteReader.java
+++ src/main/java/webserver/utils/ByteReader.java
@@ -6,9 +6,9 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
-import static webserver.http.HttpResponseStringifier.CRLF;
-
 public class ByteReader {
+    private static final String CRLF = "\r\n";
+
     private ByteReader() {
     }
 
@@ -16,7 +16,7 @@ public class ByteReader {
         StringBuilder messageBuilder = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
         String line;
-        while (!(line = br.readLine()).equals(""))  {
+        while (!(line = br.readLine()).equals("")) {
             messageBuilder.append(line).append(CRLF);
         }
         messageBuilder.append(CRLF);
