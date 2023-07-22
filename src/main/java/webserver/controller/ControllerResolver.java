3869d6e1cff2cd5a37731e4b8c0d2e0fc8b77167
csct3434 <csct3434@gmail.com>
2023-07-23 02:33:23 +0900

    feat(ControllerResolver): 요청을 처리할 컨트롤러를 결정하는 Controller Resolver 구현

---
+++ src/main/java/webserver/controller/ControllerResolver.java
@@ -0,0 +1,55 @@
+package webserver.controller;
+
+import webserver.utils.HttpMethod;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ControllerResolver {
+    private static ControllerResolver instance;
+
+    private final Map<ControllerSignature, Controller> controllers = new HashMap<>();
+    private final Controller fileController = new FileController();
+
+    private ControllerResolver() {
+        initControllers();
+    }
+
+    public static ControllerResolver getInstance() {
+        if (instance == null) {
+            synchronized (ControllerResolver.class) {
+                instance = new ControllerResolver();
+            }
+        }
+        return instance;
+    }
+
+    private void initControllers() {
+        controllers.put(new ControllerSignature("/user/create", HttpMethod.POST), new UserSaveController());
+    }
+
+    public Controller resolve(String path, String method) {
+        return controllers.keySet().stream()
+                .filter(signature -> signature.verifySignature(path, method))
+                .findFirst()
+                .map(controllers::get)
+                .orElse(fileController);
+    }
+
+    private static class ControllerSignature {
+        private final String path;
+        private final String method;
+
+        public ControllerSignature(String path, String method) {
+            this.path = path;
+            this.method = method;
+        }
+
+        public boolean verifySignature(String path, String method) {
+            if (path == null || path.isEmpty() || method == null || method.isEmpty()) {
+                return false;
+            }
+            return path.equals(this.path) && method.equals(this.method);
+        }
+    }
+}
