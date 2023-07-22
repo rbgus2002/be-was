1b2ef44e5b32411f74cbce30c4155f44b6dda986
김종원 Jongwon <tank3a@gmail.com>
2023-07-22 09:50:08 +0900

    FEATURE: ModelAndView, HandlerAdapter 생성

--- src/main/java/view/ModelAndView.java
+++ src/main/java/view/ModelAndView.java
@@ -1,4 +1,31 @@
 package view;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ModelAndView {
+
+    private String view;
+
+    private Map<String, Object> modelMap;
+
+    public ModelAndView() {
+        modelMap = new HashMap<>();
+    }
+
+    public String getView() {
+        return view;
+    }
+
+    public Map<String, Object> getModelMap() {
+        return modelMap;
+    }
+
+    public void setViewName(String view) {
+        this.view = view;
+    }
+
+    public void addObject(String name, Object value) {
+        modelMap.put(name, value);
+    }
 }
--- src/main/java/webserver/HandlerAdapter.java
+++ src/main/java/webserver/HandlerAdapter.java
@@ -1,11 +1,83 @@
 package webserver;
 
+import annotations.RequestMapping;
+import model.HttpRequest;
+import model.HttpResponse;
+import model.HttpStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import view.ModelAndView;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+import java.util.Arrays;
 
 public class HandlerAdapter {
 
-    //TODO: handle() = 실제 controller 실행, 모든 핸들러가 동일하게 동작하도록
-    protected ModelAndView handle(Object handler) {
-        return null;
+    private static final Logger logger = LoggerFactory.getLogger(HandlerAdapter.class);
+
+    //TODO: 예외처리를 404 페이지로 MV 반환하도록 수정
+    protected ModelAndView handle(HttpRequest request, HttpResponse response, Object handler) {
+
+        Class<?> controller = handler.getClass();
+
+        Method requestHandler = Arrays.stream(controller.getDeclaredMethods())
+                .filter(method -> matchesURI(request, method))
+                .findFirst()
+                .orElse(null);
+
+        if(requestHandler == null) {
+            response.setStatus(HttpStatus.OK);
+            return staticView(request);
+        }
+
+        Object result = null;
+        ModelAndView mv = null;
+        try {
+            result = requestHandler.invoke(controller.getDeclaredConstructor().newInstance(), request, response);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+
+        Type type = requestHandler.getGenericReturnType();
+
+        if(type.equals(String.class)) {
+            mv = new ModelAndView();
+            mv.setViewName((String) result);
+            return mv;
+        }
+
+        if(type.equals(void.class)) {
+            mv = new ModelAndView();
+            mv.setViewName(request.getRequestURI());
+            return mv;
+        }
+
+        if(type.equals(Object.class)) {
+            //객체로 반환시 처리
+        }
+
+        return mv;
+    }
+
+    private boolean matchesURI(HttpRequest request, Method method) {
+        if(method.isAnnotationPresent(RequestMapping.class)) {
+            RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+
+            if(annotation.method().compareTo(request.getMethod()) != 0) {
+                return false;
+            }
+
+            return annotation.value().equals(request.getRequestURI());
+        }
+        return false;
+    }
+
+    protected ModelAndView staticView(HttpRequest request) {
+        ModelAndView mv = new ModelAndView();
+
+        mv.setViewName(request.getRequestURI());
+
+        return mv;
     }
 }
