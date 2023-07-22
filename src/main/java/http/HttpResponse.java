1a278d88f0a9b291089b4e19611a2d82ebd9eaf8
dohyeon-han <limited.hyeon@gmail.com>
2023-07-22 13:23:38 +0900

    feat: HttpResponse에 List<Cookie> 필드 및 관련 메소드 추가

--- src/main/java/http/HttpResponse.java
+++ src/main/java/http/HttpResponse.java
@@ -1,10 +1,14 @@
 package http;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HttpResponse {
 
     private final String path;
     private final HttpStatus httpStatus;
     private final Mime contentType;
+    private final List<Cookie> cookies;
 
     public static HttpResponse ok(String path, Mime mime) {
         return new HttpResponse(path, mime, HttpStatus.OK);
@@ -22,6 +26,7 @@ public class HttpResponse {
         this.path = path;
         this.contentType = mime;
         this.httpStatus = httpStatus;
+        this.cookies = new ArrayList<>();
     }
 
     public String getPath() {
@@ -34,5 +39,14 @@ public class HttpResponse {
 
     public Mime getContentType() {
         return contentType;
+    }
+
+    public void setCookie(String name, String value) {
+        Cookie cookie = new Cookie(name, value);
+        cookies.add(cookie);
+    }
+
+    public List<Cookie> getCookies() {
+        return new ArrayList<>(this.cookies);
     }
 }
