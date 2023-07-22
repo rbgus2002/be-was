f9e3f90cbd9d55a7d10b05fbff386394ce840005
Jaehong Kim <kjhonggg95@gmail.com>
2023-07-22 14:40:29 +0900

    feat: Controller 에 POST 로 회원가입하는 메서드 추가

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -9,6 +9,7 @@ import http.MIME;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.Parser;
 
 import java.io.IOException;
 import java.util.Map;
@@ -17,13 +18,22 @@ public class UserController {
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @RequestMapping(path = "/user/create", method = HttpMethod.GET)
-    public HttpResponse createUser(HttpRequest request) throws IOException {
-
+    public HttpResponse createUserByGET(HttpRequest request) throws IOException {
         Map<String, String> params = request.getParams();
+
         User user = new User(params.get("userId"),params.get("password"),params.get("name"),params.get("email"));
         Database.addUser(user);
 
-        logger.debug("user create");
+        return HttpResponse.redirect("/index.html", MIME.html);
+    }
+
+    @RequestMapping(path = "/user/create", method = HttpMethod.POST)
+    public HttpResponse createUserByPOST(HttpRequest request) throws IOException {
+        Map<String, String> params = Parser.parseParamsFromBody(request.getBody());
+
+        User user = new User(params.get("userId"),params.get("password"),params.get("name"),params.get("email"));
+        Database.addUser(user);
+
         return HttpResponse.redirect("/index.html", MIME.html);
     }
 }
