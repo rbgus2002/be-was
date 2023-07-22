f46e49bd3858d1e629d5eab7067b155aa5816f37
ddingmin <ddingminyt@gmail.com>
2023-07-22 16:41:44 +0900

    feat: "기본 경로로 이동시 index.html로 이동 기능 구현"

--- src/main/java/webserver/request/HttpRequestParser.java
+++ src/main/java/webserver/request/HttpRequestParser.java
@@ -1,10 +1,16 @@
 package webserver.request;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.Dispatcher;
+import webserver.exception.BadRequestException;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.IntStream;
@@ -12,45 +18,49 @@ import java.util.stream.IntStream;
 import static utils.StringUtils.NEW_LINE;
 
 public class HttpRequestParser {
+    private static final Logger logger = LoggerFactory.getLogger(Dispatcher.class);
 
-    public static HttpRequest parseRequest(InputStream inputStream) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+    public static HttpRequestMessage parseRequest(InputStream inputStream) throws BadRequestException {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            StringBuilder stringBuilder = new StringBuilder();
+            char[] buffer = new char[1024]; // 읽을 버퍼 사이즈는 성능에 따라 정하기
+            int bytesRead;
+            while (bufferedReader.ready()) {
+                bytesRead = bufferedReader.read(buffer);
+                stringBuilder.append(buffer, 0, bytesRead);
+            }
 
-        StringBuilder stringBuilder = new StringBuilder();
-        char[] buffer = new char[1024]; // 읽을 버퍼 사이즈는 성능에 따라 정하기
-        int bytesRead;
-        while (bufferedReader.ready()) {
-            bytesRead = bufferedReader.read(buffer);
-            stringBuilder.append(buffer, 0, bytesRead);
+            String readAll = stringBuilder.toString();
+            logger.debug("[request message] \n{}", readAll);
+
+            int crlfIndex = readAll.indexOf(NEW_LINE + NEW_LINE);
+            String header = readAll.substring(0, crlfIndex);
+            String body = "";
+            if (crlfIndex > 0) {
+                /* \r\n 바이트 이후 body 가 시작되므로 */
+                body = readAll.substring(crlfIndex + 4);
+            }
+
+            String[] lines = header.split(NEW_LINE);
+            String[] statusLine = lines[0].split(" ");
+            if (statusLine.length != 3) {
+                throw new IllegalArgumentException("잘못된 Status line 입니다.");
+            }
+
+            String method = statusLine[0];
+            HttpURL url = HttpRequestParser.parseUrl(URLDecoder.decode(statusLine[1], StandardCharsets.UTF_8));
+            String version = statusLine[2];
+
+            Map<String, String> headers = new HashMap<>();
+            IntStream.range(1, lines.length).forEach(i -> {
+                String[] tokens = lines[i].split(": ?");
+                headers.put(tokens[0], tokens[1]);
+            });
+            return new HttpRequestMessage(method, url, version, headers, body);
+        } catch (IOException | IndexOutOfBoundsException e) {
+            throw new BadRequestException("잘못된 요청 메시지 입니다.");
         }
-
-        String readAll = stringBuilder.toString();
-
-        int crlfIndex = readAll.indexOf(NEW_LINE + NEW_LINE);
-        String header = readAll.substring(0, crlfIndex);
-        String body = "";
-        if (crlfIndex > 0) {
-            /* \r\n 바이트 이후 body 가 시작되므로 */
-            body = readAll.substring(crlfIndex + 4);
-        }
-
-        String[] lines = header.split(NEW_LINE);
-        String[] statusLine = lines[0].split(" ");
-        if (statusLine.length != 3) {
-            throw new IllegalArgumentException("잘못된 Status line 입니다.");
-        }
-
-        String method = statusLine[0];
-        HttpURL url = HttpRequestParser.parseUrl(URLDecoder.decode(statusLine[1]));
-        String version = statusLine[2];
-
-        Map<String, String> headers = new HashMap<>();
-        IntStream.range(1, lines.length).forEach(i -> {
-            String[] tokens = lines[i].split(": ?");
-            headers.put(tokens[0], tokens[1]);
-        });
-
-        return new HttpRequest(method, url, version, headers, body);
     }
 
     public static HttpURL parseUrl(String url) {
@@ -79,6 +89,10 @@ public class HttpRequestParser {
     }
 
     private static String extractPath(String url, int queryIndex) {
+        // todo 기본 경로에 접속할 경우 파서가 재설정 해주는 것이 맞는것인지..?
+        if (url.equals("/")) {
+            return "/index.html";
+        }
         if (queryIndex != -1) {
             return url.substring(0, queryIndex);
         }
