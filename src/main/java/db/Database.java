35a2da0926e2c940407d4ebbffbdef9a1b523c84
csct3434 <csct3434@gmail.com>
2023-07-23 00:35:00 +0900

    refactor(Database): 회원 정보를 관리하는 HashMap을 ConcurrentHashMap으로 변경하여 멀티 쓰레드 환경에서의 Thread-Safety를 향상

--- src/main/java/db/Database.java
+++ src/main/java/db/Database.java
@@ -1,14 +1,13 @@
 package db;
 
-import com.google.common.collect.Maps;
-
 import model.User;
 
 import java.util.Collection;
-import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 public class Database {
-    private static Map<String, User> users = Maps.newHashMap();
+    private static ConcurrentHashMap<String, User> users = new ConcurrentHashMap<>();
 
     public static void addUser(User user) {
         users.put(user.getUserId(), user);
