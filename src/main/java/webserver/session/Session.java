fb9d980295acfae3e4f2f0766fe183db815311cc
csct3434 <csct3434@gmail.com>
2023-07-23 01:05:05 +0900

    feat(Session): UUID가 세션 아이디인 세션 클래스 구현

---
+++ src/main/java/webserver/session/Session.java
@@ -0,0 +1,24 @@
+package webserver.session;
+
+import java.util.UUID;
+
+public class Session {
+    private final String sessionId;
+    private final String userId;
+
+    public Session(String userId) {
+        this.sessionId = UUID.randomUUID().toString();
+        this.userId = userId;
+    }
+
+    public boolean verifySessionId(String sessionId) {
+        if(sessionId == null) {
+            return false;
+        }
+        return sessionId.equals(this.sessionId);
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+}
