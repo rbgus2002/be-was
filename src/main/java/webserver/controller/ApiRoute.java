a2096a621b4b6927c9c735c29c550da870572db5
ddingmin <ddingminyt@gmail.com>
2023-07-22 16:37:38 +0900

    feat: "사용자 컨트롤러들의 매핑을 관리하는 객체 생성"

---
+++ src/main/java/webserver/controller/ApiRoute.java
@@ -0,0 +1,28 @@
+package webserver.controller;
+
+import webserver.HttpMethod;
+
+import java.util.Objects;
+
+public class ApiRoute {
+    private String path;
+    private HttpMethod method;
+
+    public ApiRoute(String path, HttpMethod method) {
+        this.path = path;
+        this.method = method;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ApiRoute pathAndMethod = (ApiRoute) o;
+        return Objects.equals(path, pathAndMethod.path) && method == pathAndMethod.method;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(path, method);
+    }
+}
