58e985875157bd8799ce4485325a1bc7dbc7bddc
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-22 10:17:55 +0900

    refactor: Webserver 클래스에서 메소드 분리, ThreadPool 사용
    - getPort 메소드 구현
    - ExecutorService 이용해서 threadPool 생성해놓고 사용

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -2,6 +2,8 @@ package webserver;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,14 +11,12 @@ import org.slf4j.LoggerFactory;
 public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
+    private static final int THREAD_COUNT = 100;
 
     public static void main(String[] args) throws Exception {
-        int port = 0;
-        if (args == null || args.length == 0) {
-            port = DEFAULT_PORT;
-        } else {
-            port = Integer.parseInt(args[0]);
-        }
+        int port = getPort(args);
+
+        ExecutorService executor = Executors.newFixedThreadPool(THREAD_COUNT);
 
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
@@ -25,9 +25,17 @@ public class WebServer {
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection));
-                thread.start();
+                executor.execute(new RequestHandler(connection));
             }
         }
+    }
+
+    private static int getPort(String[] args) {
+        int port = DEFAULT_PORT;
+        if (args != null && args.length != 0) {
+            port = Integer.parseInt(args[0]);
+        }
+
+        return port;
     }
 }
