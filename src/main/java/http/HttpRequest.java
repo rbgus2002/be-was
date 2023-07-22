9fc45e60736381a1013391e3e191cc4424634dc7
socra167 <socra167@gmail.com>
2023-07-22 19:51:25 +0900

    refactor: Request헤더가 Map으로 파싱되도록 변경

--- src/main/java/http/HttpRequest.java
+++ src/main/java/http/HttpRequest.java
@@ -6,66 +6,53 @@ import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import http.header.Header;
 import http.statusline.HttpMethod;
 import http.statusline.HttpVersion;
 
 public class HttpRequest {
 	private Logger logger = LoggerFactory.getLogger(HttpRequest.class);
+	private BufferedReader reader;
 	private HttpMethod method;
 	private String path;
 	private String endPoint;
 	private HttpVersion version;
 	private String body;
+	private Header header = new Header();
 	private HttpParameter httpParameter = new HttpParameter();
+
+	public HttpRequest(BufferedReader reader) throws IOException {
+		this.reader = reader;
+		parseRequest();
+	}
 
 	public boolean isGet() {
 		return method.equals(HttpMethod.GET);
 	}
 
-	public HttpRequest(BufferedReader reader) throws IOException {
-		parseRequest(reader);
+	private void parseRequest() throws IOException {
+		parseStatusLine();
+		parseHeader();
+		parseBody();
 	}
 
-	private void parseRequest(BufferedReader reader) throws IOException {
-		parseStatusLine(reader);
-		parseHost(reader);
-		parseConnection(reader);
-		parseAccept(reader);
-		parseUserAgent(reader);
-		parseAcceptEndcoding(reader);
-		parseAcceptLanguage(reader);
-		parseBody(reader);
+	private void parseHeader() throws IOException {
+		String line = "";
+		String[] tokens;
+		while (!line.isBlank()) {
+			line = reader.readLine();
+			if (line.contains(": ")) {
+				tokens = line.split(": ");
+				header.addHeader(tokens[0], tokens[1]);
+			}
+		}
 	}
 
-	private void parseBody(BufferedReader reader) throws IOException {
+	private void parseBody() throws IOException {
 		body = reader.readLine();
 	}
 
-	private void parseAcceptLanguage(BufferedReader reader) throws IOException {
-		reader.readLine();
-	}
-
-	private void parseAcceptEndcoding(BufferedReader reader) throws IOException {
-		reader.readLine();
-	}
-
-	private void parseUserAgent(BufferedReader reader) throws IOException {
-		reader.readLine();
-	}
-
-	private void parseAccept(BufferedReader reader) throws IOException {
-		reader.readLine();
-	}
-
-	private void parseConnection(BufferedReader reader) throws IOException {
-		reader.readLine();
-	}
-
-	private void parseHost(BufferedReader reader) throws IOException {
-		reader.readLine();
-	}
-
-	private void parseStatusLine(BufferedReader reader) throws IOException {
+	private void parseStatusLine() throws IOException {
 		String statusLine = reader.readLine();
 		String[] arguments = statusLine.split(" ");
 		method = HttpMethod.typeOf(arguments[0]);
