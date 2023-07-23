3867ce3bbfc8bf16c3b5fe11510a2f36f1d1c906
윤상현 <yshyeon102@gmail.com>
2023-07-23 17:18:59 +0900

    feat(WebServer): 리팩터링 및 포트번호가 사용중인 경우 예외 처리
    **WebServer**
    
    - 역할
        - Http 요청을 받고 관리중인 스레드에 요청 처리 위임.
    - 책임
        - 스레드를 관리하는 스레드 풀 초기화 및 관리
        - 요청을 처리할 수 있는 프로세서 초기화 및 관리
        - 서버 소켓 열고, 커넥션이 들어오면 RequestProcessor한테 작업 위임.
    - 발생할 수 있는 예외
        - 포트 번호로 서버를 열 수 없는 경우 (이미 사용중인 경우)
            - error 로그 남기고 프로그램 종료

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -3,30 +3,28 @@ package webserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class WebServer {
-    public static final int DEFAULT_PORT = 8080;
+    public static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     public static final int NUM_OF_THREAD = 10;
 
-    private final Logger logger = LoggerFactory.getLogger(WebServer.class);
+    private final ExecutorService executorService;
+    private final HttpRequestProcessor httpRequestProcessor;
+
     private final int port;
 
-    public WebServer() {
-        port = DEFAULT_PORT;
-    }
-
     public WebServer(int port) {
+        executorService = Executors.newFixedThreadPool(NUM_OF_THREAD);
+        httpRequestProcessor = new HttpRequestProcessor();
         this.port = port;
     }
 
     public void run() throws Exception {
-        ExecutorService executorService = Executors.newFixedThreadPool(NUM_OF_THREAD);
-        HttpRequestProcessor httpRequestProcessor = new HttpRequestProcessor();
-
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
 
@@ -37,7 +35,10 @@ public class WebServer {
                     httpRequestProcessor.process(httpConnection);
                 });
             }
+        } catch (BindException e) {
+            logger.error("{} 포트는 사용중인 포트입니다.", port);
+        } finally {
+            executorService.shutdown();
         }
-        executorService.shutdown();
     }
 }
