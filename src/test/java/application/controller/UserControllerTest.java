08ba8521552c0c3bbba9066a884c4444bfc768d6
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-23 19:29:45 +0900

    test: UserController를 위한 test 추가

---
+++ src/test/java/application/controller/UserControllerTest.java
@@ -0,0 +1,45 @@
+package application.controller;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import webserver.Constants.HttpStatus;
+import webserver.Constants.HttpVersion;
+import webserver.request.HttpRequest;
+import webserver.request.RequestQuery;
+import webserver.response.HttpResponse;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+class UserControllerTest {
+
+    private UserController userController;
+
+    @BeforeEach
+    void setup() {
+        userController = new UserController();
+    }
+
+    @Test
+    @DisplayName("유저가 정상적으로 생성되어야 한다.")
+    void createUser() {
+        HttpRequest mockRequest = Mockito.mock(HttpRequest.class);
+        RequestQuery mockQuery = Mockito.mock(RequestQuery.class);
+
+        when(mockRequest.getVersion()).thenReturn(HttpVersion.HTTP_1_1);
+        when(mockRequest.getRequestQuery()).thenReturn(Optional.of(mockQuery));
+        when(mockQuery.getValue("userId")).thenReturn(Optional.of("1"));
+        when(mockQuery.getValue("password")).thenReturn(Optional.of("password"));
+        when(mockQuery.getValue("name")).thenReturn(Optional.of("name"));
+        when(mockQuery.getValue("email")).thenReturn(Optional.of("email@example.com"));
+
+        HttpResponse response = userController.createUser(mockRequest);
+
+        assertEquals(HttpResponse.ofWithStatusOnly(HttpVersion.HTTP_1_1, HttpStatus.CREATED), response);
+    }
+}
