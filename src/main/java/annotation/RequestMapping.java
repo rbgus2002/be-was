0ae7ff140a357f33750c1386866ef6255d69d2d5
Hypersand <on8214@gmail.com>
2023-07-23 21:09:57 +0900

    feat: RequestMapping 어노테이션 구현

---
+++ src/main/java/annotation/RequestMapping.java
@@ -0,0 +1,16 @@
+package annotation;
+
+import webserver.http.request.RequestMethod;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RequestMapping {
+    RequestMethod method() default RequestMethod.GET;
+    String path() default "";
+
+}
