2b5e00799777cb87f6fa4f7f2759616add9e6386
윤상현 <yshyeon102@gmail.com>
2023-07-23 17:17:30 +0900

    refactor(Main): Main 리팩터링 및 예외처리 추가
    Main 클래스
    - 역할
        - 포트 번호를 입력 받고 서버 프로그램 실행
    - 책임
        - 인자로 숫자(포트 번호)가 입력 되었는지 확인
        - 서버 프로그램 객체 생성 및 실행
    - 발생할 수 있는 예외
        - 인자로 입력번호가 숫자가 아닌 경우 예외 발생
            - Main에서 예외 발생 후 종료.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,25 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import webserver.WebServer;
 
 public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    private static final int DEFAULT_PORT = 8080;
+
     public static void main(String[] args) throws Exception {
+        new WebServer(getPortNum(args))
+                .run();
+    }
+
+    private static int getPortNum(String[] args) {
         if (args == null || args.length == 0) {
-            new WebServer().run();
-        } else {
-            int port = Integer.parseInt(args[0]);
-            new WebServer(port).run();
+            return DEFAULT_PORT;
+        }
+        try {
+            return Integer.parseInt(args[0]);
+        } catch (IllegalArgumentException e) {
+            logger.error("포트 번호는 숫자로만 이루어져야 합니다.");
+            throw e;
         }
     }
 }
