dbe32bdd86cee45626ef6789ed0cfc7fcf174049
Jaehong Kim <kjhonggg95@gmail.com>
2023-07-24 18:05:52 +0900

    refactor: 컨트롤러 리턴타입 String 으로 변경
    각 api 마다 HttpResponse 를 새로 생성해서 반환하고 있는데 이러면 쿠키와 세션을 다루기에 불편하다고 느낌. 따라서 HttpResponse 객체를 미리 만들고 컨트롤러에서는 로직 수행 후 리다이렉트 할 경로를 반환하는 것으로 리팩토링함.

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -18,38 +18,41 @@ public class UserController {
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @RequestMapping(path = "/user/create", method = HttpMethod.GET)
-    public HttpResponse createUserByGET(HttpRequest request) throws IOException {
+    public String createUserByGET(HttpRequest request, HttpResponse response) throws IOException {
         Map<String, String> params = request.getParams();
 
         User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
         Database.addUser(user);
 
-        return HttpResponse.redirect("/index.html", MIME.html);
+        return "/index.html";
     }
 
     @RequestMapping(path = "/user/create", method = HttpMethod.POST)
-    public HttpResponse createUserByPOST(HttpRequest request) throws IOException {
+    public String createUserByPOST(HttpRequest request, HttpResponse response) throws IOException {
         Map<String, String> params = Parser.parseParamsFromBody(request.getBody());
 
         User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
         Database.addUser(user);
 
-        return HttpResponse.redirect("/index.html", MIME.html);
+        return "redirect:/index.html";
     }
 
     @RequestMapping(path = "/user/login", method = HttpMethod.POST)
-    public HttpResponse login(HttpRequest request) throws IOException {
+    public String login(HttpRequest request, HttpResponse response) throws IOException {
         Map<String, String> params = Parser.parseParamsFromBody(request.getBody());
         String userId = params.get("userId");
         String password = params.get("password");
 
         // login 실패시 /user/index_failed.html 로 이동
+        // TODO: 회원 인증 로직 메서드로 추출
         User user = Database.findUserById(userId);
         if (user == null || !user.getPassword().equals(password)) {
-            return HttpResponse.redirect("/user/login_failed.html", MIME.html);
+            return "redirect:/user/login_failed.html";
         }
 
         // login 성공시 /index.html 로 이동
-        return HttpResponse.redirect("/index.html", MIME.html);
+        // 로그인이 성공할 경우 HTTP 헤더의 쿠키 값을 SID=세션 ID 로 응답한다.
+
+        return "redirect:/index.html";
     }
 }
