decd0544654e2ca9843c2fb618a65a3e21a25e80
ddingmin <ddingminyt@gmail.com>
2023-07-24 17:44:05 +0900

    feat: "리플렉션 세팅을 서버 시작 시점에 수행 하도록 변경."

--- src/main/java/webserver/controller/ApplicationMethod.java
+++ src/main/java/webserver/controller/ApplicationMethod.java
@@ -5,26 +5,25 @@ import webserver.annotation.Controller;
 import webserver.annotation.RequestMapping;
 
 import java.lang.reflect.Method;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class ApplicationMethod {
     public static Map<ApiRoute, Class<?>> apiRouteToClassMap;
     public static Map<ApiRoute, Method> apiRouteToMethodMap;
 
-    static {
+    public static void initialize() {
         Set<Class<?>> classes = new Reflections("application").getTypesAnnotatedWith(Controller.class);
         Map<ApiRoute, Class<?>> classMap = new HashMap<>();
         Map<ApiRoute, Method> methodMap = new HashMap<>();
 
         for (Class<?> clazz : classes) {
-            for (Method method : clazz.getDeclaredMethods()) {
-                RequestMapping targetAnnotation = method.getDeclaredAnnotation(RequestMapping.class);
-                classMap.put(new ApiRoute(targetAnnotation.path(), targetAnnotation.method()), clazz);
-                methodMap.put(new ApiRoute(targetAnnotation.path(), targetAnnotation.method()), method);
-            }
+            Arrays.stream(clazz.getDeclaredMethods())
+                    .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                    .forEach(method -> {
+                        RequestMapping targetAnnotation = method.getDeclaredAnnotation(RequestMapping.class);
+                        classMap.put(new ApiRoute(targetAnnotation.path(), targetAnnotation.method()), clazz);
+                        methodMap.put(new ApiRoute(targetAnnotation.path(), targetAnnotation.method()), method);
+                    });
         }
         apiRouteToClassMap = Collections.unmodifiableMap(classMap);
         apiRouteToMethodMap = Collections.unmodifiableMap(methodMap);
