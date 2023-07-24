458edf75e5ec747d5f11d355ab6071a1e3d922d7
csct3434 <csct3434@gmail.com>
2023-07-24 16:42:54 +0900

    feat(Cookie): 쿠키 정보를 관리하는 클래스 작성

--- src/main/java/webserver/http/Cookie.java
+++ src/main/java/webserver/http/Cookie.java
@@ -1,20 +1,30 @@
 package webserver.http;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Cookie {
-    private final Map<String, String> directives;
+    private final List<String> directives;
 
     public Cookie() {
-        this.directives = new HashMap<>();
+        this.directives = new ArrayList<>();
     }
 
-    public void add(String key, String value) {
-        directives.put(key, value);
+    public void add(String directive) {
+        directives.add(directive);
     }
 
-    public String get(String key) {
-        return directives.get(key);
-    }   
+    public String get(String name) {
+        for (String directive : directives) {
+            String[] tokens = directive.split("=");
+            if (tokens.length == 2 && tokens[0].equals(name)) {
+                return tokens[1];
+            }
+        }
+        return "";
+    }
+
+    public String getMessage() {
+        return String.join("; ", directives);
+    }
 }
