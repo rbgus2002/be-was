da8f7048868bdc14b6e23978df497cdee2edfee0
socra167 <socra167@gmail.com>
2023-07-24 10:50:46 +0900

    feat: Session의 Map을 양방향으로 만들어 User로 sessionId를 찾을 수 있게 함

--- src/main/java/session/Session.java
+++ src/main/java/session/Session.java
@@ -8,24 +8,34 @@ import com.google.common.collect.Maps;
 import model.User;
 
 public class Session {
-	private Map<String, User> sessionStore = Maps.newConcurrentMap();
+	private Map<String, User> sessionToUserStore = Maps.newConcurrentMap();
+	private Map<User, String> userToSessionStore = Maps.newConcurrentMap();
 
 	public String createSession(User user) {
 		String sessionId = UUID.randomUUID().toString();
-		sessionStore.put(sessionId, user);
+		sessionToUserStore.put(sessionId, user);
+		userToSessionStore.put(user, sessionId);
 		return sessionId;
 	}
 
-	public User getUser(String sessionId) {
-		if (sessionStore.containsKey(sessionId)) {
-			return sessionStore.get(sessionId);
+	public User getUser(String sessionId) throws IllegalArgumentException {
+		if (sessionToUserStore.containsKey(sessionId)) {
+			return sessionToUserStore.get(sessionId);
 		}
 		throw new IllegalArgumentException("등록되지 않은 세션 ID가 입력되었습니다.");
 	}
 
-	public void removeSession(String sessionId) {
-		if (sessionStore.containsKey(sessionId)) {
-			sessionStore.remove(sessionId);
+	public String getSessionId(User user) throws IllegalArgumentException {
+		if (userToSessionStore.containsKey(user)) {
+			return userToSessionStore.get(user);
+		}
+		throw new IllegalArgumentException("세션이 생성되지 않은 사용자입니다.");
+	}
+
+	public void removeSession(String sessionId) throws IllegalArgumentException {
+		if (sessionToUserStore.containsKey(sessionId)) {
+			userToSessionStore.remove(sessionToUserStore.get(sessionId));
+			sessionToUserStore.remove(sessionId);
 			return;
 		}
 		throw new IllegalArgumentException("세션 ID가 등록되지 않아 세션을 제거할 수 없습니다.");
--- src/test/java/session/SessionTest.java
+++ src/test/java/session/SessionTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.*;
 
 import java.util.UUID;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
