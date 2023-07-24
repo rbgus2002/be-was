f5db5fee808c4c030aa5e5eeab83bb0bcd03a83d
Jeongyeon Kim <wjddus9132@gmail.com>
2023-07-24 23:21:31 +0900

    test: Controller 클래스 GET으로 회원가입 테스트 수정

--- src/test/java/controller/ControllerTest.java
+++ src/test/java/controller/ControllerTest.java
@@ -6,22 +6,27 @@ import webserver.http.HttpRequest;
 import webserver.http.HttpResponse;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@DisplayName("User Controller Test")
+@DisplayName("Controller Test")
 class ControllerTest {
+
+    Controller controller = new Controller();
 
     @Test
     @DisplayName("GET으로 회원가입을 요청하면 입력 정보를 바탕으로 유저가 생성되어야 한다.")
     void createUser() throws IOException {
         // given
-        String firstLine = "GET /user/create?userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net HTTP/1.1";
-        Map<String, String> parameters = HttpRequest.parseQueryString(firstLine);
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("userId", "test123");
+        parameters.put("password", "password");
+        parameters.put("name", "userA");
+        parameters.put("email", "test@gmail.com");
 
         // when
-        Controller controller = new Controller();
         HttpResponse httpResponse = controller.createUser(parameters);
 
         //then
