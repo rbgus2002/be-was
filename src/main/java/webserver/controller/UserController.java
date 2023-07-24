cb9b3abd32c63ffc94b1e2d4b34364fb4e945f35
Hypersand <on8214@gmail.com>
2023-07-24 13:19:04 +0900

    feat: UserController 어노테이션 방식으로 변경

--- src/main/java/webserver/controller/UserController.java
+++ src/main/java/webserver/controller/UserController.java
@@ -1,27 +1,33 @@
 package webserver.controller;
 
+import annotation.Controller;
+import annotation.RequestMapping;
 import db.Database;
 import model.User;
 import utils.Parser;
 import webserver.http.request.HttpRequest;
-import webserver.http.response.HttpResponse;
 
 import java.util.Map;
 
-public class UserController implements Controller {
-    @Override
-    public void doGet(HttpRequest httpRequest, HttpResponse httpResponse) {
-        Map<String, String> queryParams = Parser.parseQueryParameters(httpRequest.getRequestUri());
+import static webserver.http.request.RequestMethod.POST;
+
+@Controller
+public class UserController {
+    @RequestMapping(method = POST, value = "/user/create")
+    public void saveUser(HttpRequest httpRequest) {
+        Map<String, String> queryParams = Parser.parseQueryParameters(httpRequest.getHttpRequestBody());
         String userId = queryParams.get("userId");
         String password = queryParams.get("password");
         String name = queryParams.get("name");
         String email = queryParams.get("email");
 
+        //TODO: 확장성을 고려했을 때 코드가 지저분해질 수 있다.
         if (userId == null || password == null || name == null || email == null) {
             throw new IllegalArgumentException("입력을 제대로 해주세요!");
         }
 
         User user = new User(userId, password, name, email);
         Database.addUser(user);
+        System.out.println("유저 = " + user.getUserId() + " " + user.getEmail());
     }
 }
