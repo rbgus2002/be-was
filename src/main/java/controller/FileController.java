5f9a54fe46adbb24fd55fba231eb3c8c39f4321f
jst0951 <jst0951@gmail.com>
2023-07-24 11:36:49 +0900

    hotfix: feat: regex 기반으로 동작하도록 수정, 호환성 개선

--- src/main/java/controller/FileController.java
+++ src/main/java/controller/FileController.java
@@ -11,6 +11,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static http.HttpUtil.*;
 import static service.FileService.readStaticFile;
@@ -19,12 +21,12 @@ import static service.SessionService.getUserIdBySid;
 public class FileController {
     private static final String STATIC_FILEPATH = "./src/main/resources/static";
     private static final String TEMPLATE_FILEPATH = "./src/main/resources/templates";
-    private static final String NAVBAR_RIGHT = "/user/list.html\"><i class=\"glyphicon glyphicon-user\"></i></a></li>";
+    private static final String NAVBAR_RIGHT = "<li>.*?/user/list\\.html.*?</li>";
     private static final String USERNAME_FORMAT = "<li>%s</li>";
-    private static final String BUTTON_LOGOUT = "<li><a href=\"#\" role=\"button\">로그아웃</a></li>";
-    private static final String BUTTON_LOGIN = "user/login.html\" role=\"button\">로그인</a></li>";
-    private static final String BUTTON_SIGNUP = "user/form.html\" role=\"button\">회원가입</a></li>";
-    private static final String BUTTON_MODIFY_USERDATA = "<li><a href=\"#\" role=\"button\">개인정보수정</a></li>";
+    private static final String BUTTON_LOGOUT = "<li>.*?로그아웃.*?</li>";
+    private static final String BUTTON_LOGIN = "<li>.*?로그인.*?</li>";
+    private static final String BUTTON_SIGNUP = "<li>.*?회원가입.*?</li>";
+    private static final String BUTTON_MODIFY_USERDATA = "<li>.*?개인정보수정.*?</li>";
     private static final String USER_LIST_TBODY = "<tbody>";
     private static final String USER_LIST_ROW_FORM = "<tr>\n<th scope=\"row\">%d</th> <td>%s</td> <td>%s</td> <td>%s</td><td><a href=\"#\" class=\"btn btn-success\" role=\"button\">수정</a></td>\n</tr>";
 
@@ -52,14 +54,13 @@ public class FileController {
 
             // Navbar
             if(sid == null) {
-                httpDocument = httpDocument.replace(BUTTON_LOGOUT, "");
-                httpDocument = httpDocument.replace(BUTTON_MODIFY_USERDATA, "");
+                httpDocument = httpDocument.replaceAll(BUTTON_LOGOUT, "");
+                httpDocument = httpDocument.replaceAll(BUTTON_MODIFY_USERDATA, "");
             }
             else {
-                httpDocument = httpDocument.replace(NAVBAR_RIGHT, NAVBAR_RIGHT +
-                        String.format(USERNAME_FORMAT, getUserIdBySid(sid)));
-                httpDocument = httpDocument.replace(BUTTON_LOGIN, "");
-                httpDocument = httpDocument.replace(BUTTON_SIGNUP, "");
+                httpDocument = appendTail(httpDocument, NAVBAR_RIGHT, String.format(USERNAME_FORMAT, getUserIdBySid(sid)));
+                httpDocument = httpDocument.replaceAll(BUTTON_LOGIN, "");
+                httpDocument = httpDocument.replaceAll(BUTTON_SIGNUP, "");
             }
 
             // userList
@@ -72,7 +73,7 @@ public class FileController {
                         String tr = String.format(USER_LIST_ROW_FORM, i, user.getUserId(), user.getName(), user.getEmail());
                         sb.append(tr);
                     }
-                    httpDocument = httpDocument.replace(USER_LIST_TBODY, USER_LIST_TBODY + sb);
+                    httpDocument = httpDocument.replaceAll(USER_LIST_TBODY, USER_LIST_TBODY + sb);
                 }
             }
             body = httpDocument.getBytes();
@@ -83,5 +84,17 @@ public class FileController {
         headerMap.put(HEADER_CONTENT_LENGTH, String.valueOf(body.length));
 
         return new Response(STATUS.OK, headerMap, body);
+    }
+
+    private static String appendTail(String source, String regex, String tail) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(source);
+
+        while(matcher.find()) {
+            String foundString = matcher.group();
+            source = source.replaceAll(regex, foundString + tail);
+        }
+
+        return source;
     }
 }
