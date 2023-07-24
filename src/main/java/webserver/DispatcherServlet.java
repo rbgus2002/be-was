761377357b76081991c38547176826244d9cd551
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-24 20:21:55 +0900

    fix: DispathcerServlet 클래스 수정
    - path를 이용해 Controller를 먼저 찾으면, 정적 리소스파일에 접근하지 못하는 오류를 해결하기 위해 정적 리소스 파일을 먼저 찾아보고 없으면 Controller에서 찾는 형식으로 변경

--- src/main/java/webserver/DispatcherServlet.java
+++ src/main/java/webserver/DispatcherServlet.java
@@ -1,30 +1,40 @@
 package webserver;
 
+import application.controller.WebController;
+import exception.InvalidPathException;
+import exception.InvalidQueryParameterException;
 import webserver.view.view.View;
 import webserver.view.viewResolver.StaticViewResolver;
 import webserver.request.HttpRequest;
-import webserver.response.HttpResponse;
-import webserver.Constants.HttpStatus;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class DispatcherServlet {
 
-    private final HttpRequest httpRequest;
+    private final HttpRequest request;
 
-    public DispatcherServlet(HttpRequest httpRequest) {
-        this.httpRequest = httpRequest;
+    public DispatcherServlet(HttpRequest request) {
+        this.request = request;
     }
 
-    public void dispatch(final DataOutputStream dos) throws IOException {
-
+    public void dispatch(final DataOutputStream dos) throws IOException, InvalidPathException, InvalidQueryParameterException, InvocationTargetException, IllegalAccessException {
         StaticViewResolver staticViewResolver = new StaticViewResolver();
-        View view = staticViewResolver.resolve(httpRequest.getFullPath());
 
-        view.render(httpRequest.getVersion(), null, dos);
+        View view = staticViewResolver.resolve(request.getFullPath());
+        if(view != null) {
+            view.render(request.getVersion(), null, dos);
+            return;
+        }
 
-        HttpResponse httpResponse = HttpResponse.ofWithStatusOnly(httpRequest.getVersion(), HttpStatus.NOT_FOUND);
-        httpResponse.sendResponse(dos);
+        ControllerMapper controllerMapper = new ControllerMapper();
+        WebController controller = controllerMapper.getController(request);
+
+        Method method = controllerMapper.getMethod(controller, request);
+        ModelAndView modelAndView = (ModelAndView) method.invoke(controller, request);
+        view = staticViewResolver.resolve(modelAndView.getViewName());
+        view.render(request.getVersion(), modelAndView.getModel(), dos);
     }
 }
