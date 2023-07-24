94785ce827293f9ebd5dd2559798d4ba848d0ab0
Hypersand <on8214@gmail.com>
2023-07-24 12:58:27 +0900

    feat: RequestMapping 어노테이션의 path 필드 value로 네이밍 변경

--- src/main/java/annotation/RequestMapping.java
+++ src/main/java/annotation/RequestMapping.java
@@ -11,6 +11,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RequestMapping {
     RequestMethod method() default RequestMethod.GET;
-    String path() default "";
-
+    String value() default "";
 }
