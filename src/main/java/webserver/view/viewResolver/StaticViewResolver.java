70de044bb5462fd5243d46fc4f335b52393ceb10
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-24 18:07:04 +0900

    fix: StaticViewResolver의 상수 이름 변경

--- src/main/java/webserver/view/viewResolver/StaticViewResolver.java
+++ src/main/java/webserver/view/viewResolver/StaticViewResolver.java
@@ -10,14 +10,12 @@ import java.nio.file.Paths;
 
 public class StaticViewResolver implements ViewResolver {
 
-    private static final String resourcePath = "src/main/resources/templates/";
+    private static final String RESOURCES_TEMPLATES = "src/main/resources/templates/";
 
     @Override
-    public View resolve(final String viewName) {
-        Path filePath = Paths.get(resourcePath, viewName);
-
-        if (!Files.exists(filePath)) throw new InvalidPathException();
-
-        return new StaticView(filePath.toString());
+    public View resolve(final String viewName) throws InvalidPathException {
+        Path templatesFilePath = Paths.get(RESOURCES_TEMPLATES, viewName);
+        if (!Files.exists(templatesFilePath)) throw new InvalidPathException();
+        return new StaticView(templatesFilePath.toString());
     }
 }
