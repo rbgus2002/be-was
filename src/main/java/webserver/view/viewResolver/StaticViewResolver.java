36663c2618aad9d02f274cd3a0fe1dcf12f968e7
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-24 16:16:39 +0900

    fix: ViewResolver에서 잘못 사용하던 Optional 수정
    - 단순히 null인 경우만이 아니라 null일 때 특정 행동을 할 때 Optioanl 사용하는 것이었다.

--- src/main/java/webserver/view/viewResolver/StaticViewResolver.java
+++ src/main/java/webserver/view/viewResolver/StaticViewResolver.java
@@ -1,25 +1,23 @@
 package webserver.view.viewResolver;
 
+import exception.InvalidPathException;
 import webserver.view.view.StaticView;
 import webserver.view.view.View;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Optional;
 
 public class StaticViewResolver implements ViewResolver {
 
     private static final String resourcePath = "src/main/resources/templates/";
 
     @Override
-    public Optional<View> resolve(final String viewName) {
+    public View resolve(final String viewName) {
         Path filePath = Paths.get(resourcePath, viewName);
 
-        if (!Files.exists(filePath)) {
-            return Optional.empty();
-        }
+        if (!Files.exists(filePath)) throw new InvalidPathException();
 
-        return Optional.of(new StaticView(filePath.toString()));
+        return new StaticView(filePath.toString());
     }
 }
--- src/main/java/webserver/view/viewResolver/ViewResolver.java
+++ src/main/java/webserver/view/viewResolver/ViewResolver.java
@@ -2,8 +2,6 @@ package webserver.view.viewResolver;
 
 import webserver.view.view.View;
 
-import java.util.Optional;
-
 public interface ViewResolver {
-    Optional<View> resolve(final String viewName) throws Exception;
+    View resolve(final String viewName) throws Exception;
 }
