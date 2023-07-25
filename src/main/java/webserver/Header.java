04b6cbe30b4eb9f1ccf897b122ec8f8b2f199a9c
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-25 21:20:25 +0900

    fix: Header 클래스가 response에서도 사용되므로 위치 이전, 이름 수정

---
+++ src/main/java/webserver/Header.java
@@ -0,0 +1,27 @@
+package webserver;
+
+import support.utils.StringUtils;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Header {
+
+    private final HashMap<String, String> headers;
+
+    private Header(HashMap<String, String> headers) {
+        this.headers = headers;
+    }
+
+    public static Header of(final String string) {
+        String[] tokens = string.split(StringUtils.NEWLINE);
+
+        HashMap<String, String> headers = new HashMap<>();
+
+        Arrays.stream(tokens)
+                .map(token -> token.split(":"))
+                .forEach(header -> headers.put(header[0].trim(), header[1].trim()));
+
+        return new Header(headers);
+    }
+}
