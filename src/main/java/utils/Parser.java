497ab14cfba3f1440f5675b85323b69021d585a5
Hypersand <on8214@gmail.com>
2023-07-25 16:48:44 +0900

    feat: Parser의 requestHeaders map HashMap으로 변경

--- src/main/java/utils/Parser.java
+++ src/main/java/utils/Parser.java
@@ -6,13 +6,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class Parser {
     public static Map<String, String> parseRequestLine(String requestLine) {
         String[] tokens = requestLine.split(" ");
-        Map<String, String> requestLineMap = new ConcurrentHashMap<>();
+        Map<String, String> requestLineMap = new HashMap<>();
         requestLineMap.put("method", tokens[0]);
         requestLineMap.put("uri", tokens[1]);
         requestLineMap.put("version", tokens[2]);
@@ -21,7 +21,7 @@ public class Parser {
 
     public static Map<String, String> parseHeaders(BufferedReader br) throws IOException {
         String line;
-        Map<String, String> requestHeaders = new ConcurrentHashMap<>();
+        Map<String, String> requestHeaders = new HashMap<>();
 
         while ((line = br.readLine()) != null && !line.isEmpty()) {
             String[] header = line.split(":");
@@ -32,7 +32,7 @@ public class Parser {
     }
 
     public static Map<String, String> parseQueryParameters(String queryString) {
-        Map<String, String> queryParams = new ConcurrentHashMap<>();
+        Map<String, String> queryParams = new HashMap<>();
         if (queryString.contains("?")) {
             queryString = queryString.substring(queryString.indexOf("?") + 1);
         }
