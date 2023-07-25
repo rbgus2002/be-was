df8ef2dc64f75d204b6009509e10cee726db94bd
Jeongyeon Kim <wjddus9132@gmail.com>
2023-07-26 00:24:24 +0900

    test: HttpRequestHeaders 클래스 headers 문자열을 Map으로 파싱 테스트

---
+++ src/test/java/webserver/http/request/HttpRequestHeadersTest.java
@@ -0,0 +1,28 @@
+package webserver.http.request;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("HttpRequest headers test")
+class HttpRequestHeadersTest {
+
+    @Test
+    @DisplayName("headers 문자열을 Map으로 파싱해야 한다.")
+    void parseRequestBody() {
+        String headersString = "Host: localhost:8080\r\n" +
+                "Connection: keep-alive\n\n" +
+                "Content-Length: 59\n\n" +
+                "Content-Type: application/x-www-form-urlencoded\n\n" +
+                "Accept: */*\n\n";
+
+        HttpRequestHeaders httpRequestHeaders = new HttpRequestHeaders(headersString);
+
+        assertEquals("localhost:8080", httpRequestHeaders.getHeader().get("Host"));
+        assertEquals("keep-alive", httpRequestHeaders.getHeader().get("Connection"));
+        assertEquals("59", httpRequestHeaders.getHeader().get("Content-Length"));
+        assertEquals("application/x-www-form-urlencoded", httpRequestHeaders.getHeader().get("Content-Type"));
+        assertEquals("*/*", httpRequestHeaders.getHeader().get("Accept"));
+    }
+}
