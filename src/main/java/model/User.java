d83a3e9cc44f058c27612d999a2921f68086e98c
SeoSiun <siun0816@hanyang.ac.kr>
2023-07-25 17:52:08 +0900

    feat: parameter Map을 받아 유저 객체를 생성해 리턴하는 User.from 메소드 구현

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Map;
+
 public class User {
     private String userId;
     private String password;
@@ -11,6 +13,21 @@ public class User {
         this.password = password;
         this.name = name;
         this.email = email;
+    }
+
+    public static User from(Map<String, String> params) {
+        String userId = params.get("userId");
+        String password = params.get("password");
+        String name = params.get("name");
+        String email = params.get("email");
+        if (hasMissingValue(userId, password, name, email)) {
+            throw new IllegalArgumentException("모든 필드값이 존재해야 합니다!");
+        }
+        return new User(userId, password, name, email);
+    }
+
+    private static boolean hasMissingValue(String userId, String password, String name, String email) {
+        return userId == null || password == null || name == null || email == null;
     }
 
     public String getUserId() {
