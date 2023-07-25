920e152a7894d978b032818799375d12875eca59
Jaehong Kim <kjhonggg95@gmail.com>
2023-07-25 15:37:23 +0900

    rename: enum 필드 대문자로 변경

--- src/main/java/http/MIME.java
+++ src/main/java/http/MIME.java
@@ -2,13 +2,13 @@ package http;
 
 public enum MIME {
 
-    html("html", "text/html"),
-    css("css", "text/css"),
-    js("js", "text/javascript"),
-    ico("ico", "image/vnd.microsoft.icon"),
-    png("png", "image/png"),
-    jpg("jpg", "image/jpeg"),
-    woff("woff", "font/woff");
+    HTML("html", "text/html"),
+    CSS("css", "text/css"),
+    JS("js", "text/javascript"),
+    ICO("ico", "image/vnd.microsoft.icon"),
+    PNG("png", "image/png"),
+    JPG("jpg", "image/jpeg"),
+    WOFF("woff", "font/woff");
 
     private final String extension;
     private final String contentType;
