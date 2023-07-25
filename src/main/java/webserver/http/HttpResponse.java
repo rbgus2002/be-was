8b0adcd2c79773173a88e16fa85c07c758ae4a7f
socra167 <socra167@gmail.com>
2023-07-25 10:45:36 +0900

    refactor: Content Length, Content Type, Location, Set-Cookie 를 HeaderConst의 상수로 사용

--- src/main/java/webserver/http/HttpResponse.java
+++ src/main/java/webserver/http/HttpResponse.java
@@ -1,5 +1,7 @@
 package webserver.http;
 
+import static webserver.http.header.HeaderConst.*;
+
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -8,7 +10,6 @@ import java.nio.file.Path;
 
 import webserver.http.header.Header;
 import webserver.http.header.MimeType;
-import webserver.http.statusline.HttpVersion;
 import webserver.http.statusline.StatusCode;
 import webserver.http.statusline.ResponseLine;
 import webserver.http.header.Cookie;
@@ -18,7 +19,6 @@ public class HttpResponse {
 	private ResponseLine responseLine = new ResponseLine();
 	private Header header = new Header();
 	private byte[] body = new byte[0];
-
 	public void response(OutputStream out) throws IOException {
 		DataOutputStream dos = new DataOutputStream(out);
 		responseStatusLine(dos);
@@ -30,19 +30,19 @@ public class HttpResponse {
 		body = Files.readAllBytes(path);
 		String fileName = path.getFileName().toString();
 		String extension = fileName.substring(fileName.lastIndexOf("."));
-		header.addHeader("Content-Type", MimeType.typeOf(extension).extension + ";charset=utf-8");
-		header.addHeader("Content-Length", String.valueOf(body.length));
+		header.addHeader(CONTENT_TYPE, MimeType.typeOf(extension).extension + ";charset=utf-8");
+		header.addHeader(CONTENT_LENGTH, String.valueOf(body.length));
 	}
 
 	public void setRedirect(final String redirectPath, StatusCode statusCode) {
 		responseLine.setStatusCode(statusCode);
-		header.addHeader("Location", redirectPath);
+		header.addHeader(LOCATION, redirectPath);
 	}
 
 	public void addCookie(String cookieName, String sessionId) {
 		Cookie cookie = Cookie.newCookie();
 		cookie.add(cookieName, sessionId);
-		header.addHeader("Set-Cookie", cookie.toHeaderValue());
+		header.addHeader(SET_COOKIE, cookie.toHeaderValue());
 	}
 
 	private void responseStatusLine(DataOutputStream dos) throws IOException {
--- src/main/java/webserver/http/header/HeaderConst.java
+++ src/main/java/webserver/http/header/HeaderConst.java
@@ -4,5 +4,8 @@ public interface HeaderConst {
 	public static final String HEADER_DELIMITER = ": ";
 	public static final String CRLF = "\r\n";
 	public static final String COOKIE = "Cookie";
+	public static final String CONTENT_TYPE = "Content-Type";
 	public static final String CONTENT_LENGTH = "Content-Length";
+	public static final String LOCATION = "Location";
+	public static final String SET_COOKIE = "Set-Cookie";
 }
