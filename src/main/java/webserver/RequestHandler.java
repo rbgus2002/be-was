e4be7c3cdcf4d4ead6baa1e563e8dfb25c514d9f
Jaehong Kim <kjhonggg95@gmail.com>
2023-07-25 15:48:38 +0900

    refactor: Socket connection 에 final 키워드 추가

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -18,7 +18,7 @@ import util.Parser;
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
-    private Socket connection;
+    private final Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
