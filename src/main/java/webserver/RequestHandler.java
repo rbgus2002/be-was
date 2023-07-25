580cd46c40f94fb30996d9e713f28a7012667858
Jaehong Kim <kjhonggg95@gmail.com>
2023-07-25 15:47:34 +0900

    refactor: HttpResponse 를 기본생성자로 생성.
    필드들에 대한 정보는 메서드에 따라 설정하도록 수정.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,13 +3,17 @@ package webserver;
 import java.io.*;
 import java.lang.reflect.Method;
 import java.net.Socket;
+import java.util.UUID;
 
+import db.SessionDatabase;
+import handler.HandlerMapping;
 import controller.UserController;
 import http.HttpRequest;
 import http.HttpResponse;
-import http.MIME;
+import http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.Parser;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -26,19 +30,19 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             HttpRequest httpRequest = new HttpRequest(in);
+            HttpResponse httpResponse = new HttpResponse();
             logger.debug("{}", httpRequest);
 
-            String path = httpRequest.getPath();
-            MIME mime = httpRequest.getMime();
-
-            HttpResponse httpResponse;
-
+            // TODO: 여기서부터 httpResponse.response 이전까지의 로직들을 다른 객체가 처리하도록 하면 좋을거같다. Front Controller? Dispatcher?
             Method method = HandlerMapping.getHandler(httpRequest);
 
             if (method != null) {
-                httpResponse = (HttpResponse) method.invoke(new UserController(), httpRequest);
+                String redirectPath = (String) method.invoke(new UserController(), httpRequest, httpResponse);
+                String responsePath = Parser.parsePathFromRedirect(redirectPath);
+                httpResponse.redirect(responsePath);
             } else {
-                httpResponse = HttpResponse.ok(path, mime);
+                String requestPath = httpRequest.getPath();
+                httpResponse.ok(requestPath);
             }
 
             httpResponse.response(new DataOutputStream(out));
