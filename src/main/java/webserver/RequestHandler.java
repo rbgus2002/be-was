9d8f24a7f9a7dedd8dd84047133454a6cd03852e
Kwon Min A <min2010011@gmail.com>
2023-07-25 11:25:06 +0900

    refactor: 응답 메세지 전송 기능 RequestHandler -> HttpResponse 이동

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,12 +1,9 @@
 package webserver;
 
-import static webserver.http.utils.HttpConstant.*;
-
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
-import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,11 +11,9 @@ import org.slf4j.LoggerFactory;
 import webserver.http.controller.FrontController;
 import webserver.http.message.HttpRequest;
 import webserver.http.message.HttpResponse;
-import webserver.http.utils.StringUtils;
 
 public class RequestHandler implements Runnable {
 	private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
-	private static final String RESOURCES_PATH = "src/main/resources/templates";
 
 	private final Socket connection;
 
@@ -33,48 +28,20 @@ public class RequestHandler implements Runnable {
 		try (InputStream inputStream = connection.getInputStream();
 			 DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream())) {
 			// Request 로그 출력
-			preProcess(inputStream);
 			// Request & Response 객체 생성
 			HttpRequest request = HttpRequest.from(inputStream);
-			HttpResponse response = HttpResponse.from(request);
+			HttpResponse response = HttpResponse.from(request, outputStream);
 			// 요청 처리
 			FrontController frontController = new FrontController();
 			frontController.service(request, response);
 			// 응답 전송
-			sendResponse(outputStream, response);
+			response.sendResponse(outputStream);
 		} catch (IOException e) {
 			logger.error(e.getMessage());
 		}
 	}
 
-	private void sendResponse(DataOutputStream outputStream, HttpResponse response) {
-		writeResponseHeader(outputStream, response);
-		writeResponseBody(outputStream, response);
-	}
-
-	private void writeResponseHeader(DataOutputStream outputStream, HttpResponse response) {
-		try {
-			outputStream.writeBytes(StringUtils.joinStatusLine(response.getStatusLineTokens()));
-			for (Map.Entry<String, String> headerField : response.getHeaderFieldsEntry()) {
-				outputStream.writeBytes(StringUtils.joinHeaderFields(headerField.getKey(), headerField.getValue()));
-			}
-			outputStream.writeBytes(CRLF);
-		} catch (IOException e) {
-			logger.error(e.getMessage());
-		}
-	}
-
-	private void writeResponseBody(DataOutputStream outputStream, HttpResponse response) {
-		try {
-			byte[] body = response.getBody();
-			outputStream.write(body, 0, body.length);
-			outputStream.flush();
-		} catch (IOException e) {
-			logger.error(e.getMessage());
-		}
-	}
-
-	private void preProcess(InputStream inputStream) throws IOException {
-		// TODO: 전체 로그 출력
-	}
+	// private void preProcess(InputStream inputStream) throws IOException {
+	// 	// TODO: 전체 로그 출력
+	// }
 }
--- src/main/java/webserver/http/message/HttpResponse.java
+++ src/main/java/webserver/http/message/HttpResponse.java
@@ -1,12 +1,23 @@
 package webserver.http.message;
 
+import static webserver.http.utils.HttpConstant.*;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import webserver.http.utils.StringUtils;
 
 public class HttpResponse {
 
+	private static final Logger logger = LoggerFactory.getLogger(HttpResponse.class);
+
+	private DataOutputStream outputStream;
 	private String httpVersion;
 	private HttpStatus status;
 	private HttpHeaderFields headerFields;
@@ -16,41 +27,56 @@ public class HttpResponse {
 		headerFields = new HttpHeaderFields();
 	}
 
-	public static HttpResponse from(HttpRequest request) {
+	public static HttpResponse from(HttpRequest request, DataOutputStream outputStream) {
 		HttpResponse httpResponse = new HttpResponse();
 		httpResponse.httpVersion = request.getHttpVersion();
-		// TODO: 요청의 general header는 응답에도 바로 저장하기
+		httpResponse.outputStream = outputStream;
+		// TODO : 요청의 general header는 응답에도 바로 저장하기
 		return httpResponse;
 	}
 
-	public Set<Map.Entry<String, String>> getHeaderFieldsEntry() {
-		return headerFields.getEntrySet();
-	}
-
-	public List<String> getStatusLineTokens() {
-		List<String> tokens = new ArrayList<>();
-		tokens.add(httpVersion);
-		tokens.add(status.getCode());
-		tokens.add(status.getMessage());
-		return tokens;
-	}
-
-	public byte[] getBody() {
-		return body;
-	}
-
-	public void writeStatus(HttpStatus status) {
+	public void setStatus(HttpStatus status) {
 		this.status = status;
 	}
 
-	public void writeHeaderField(String key, String value) {
+	public void setHeader(String key, String value) {
 		headerFields.addHeaderField(key, value);
 	}
 
-	public void writeBody(byte[] body, String contentType) {
+	public void setBody(byte[] body, String contentType) {
 		this.body = body;
-		writeHeaderField("Content-Type", contentType);
-		writeHeaderField("Content-Length", String.valueOf(body.length));
+		setHeader("Content-Type", contentType);
+		setHeader("Content-Length", String.valueOf(body.length));
+	}
+
+	public void sendResponse(DataOutputStream outputStream) {
+		writeResponseHeader(outputStream);
+		writeResponseBody(outputStream);
+	}
+
+	private void writeResponseHeader(DataOutputStream outputStream) {
+		try {
+			List<String> tokens = new ArrayList<>();
+			tokens.add(httpVersion);
+			tokens.add(status.getCode());
+			tokens.add(status.getMessage());
+			outputStream.writeBytes(StringUtils.joinStatusLine(tokens));
+			for (Map.Entry<String, String> headerField : headerFields.getEntrySet()) {
+				outputStream.writeBytes(StringUtils.joinHeaderFields(headerField.getKey(), headerField.getValue()));
+			}
+			outputStream.writeBytes(CRLF);
+		} catch (IOException e) {
+			logger.error(e.getMessage());
+		}
+	}
+
+	private void writeResponseBody(DataOutputStream outputStream) {
+		try {
+			outputStream.write(body, 0, body.length);
+			outputStream.flush();
+		} catch (IOException e) {
+			logger.error(e.getMessage());
+		}
 	}
 
 }
