854799f59b6ee530b56355307745b57a665dc633
Yunsik-Choi <choiys0311@gmail.com>
2023-07-25 11:37:02 +0900

    🛠️ fix : 문자열 자르기 수정

--- src/main/java/application/common/StringUtils.java
+++ src/main/java/application/common/StringUtils.java
@@ -5,12 +5,12 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class StringUtils {
-    private static final String REGEX = "\\?";
-    private static final String ENTRY_DELIMITER = "=";
+    private static final String ENTRY_DELIMITER = "&";
+    private static final String KEY_VALUE_DELIMITER = "=";
 
     public static Map<String, String> extractBy(final String body) {
-        return Arrays.stream(body.split(REGEX))
-                .map(entry -> entry.split(ENTRY_DELIMITER))
+        return Arrays.stream(body.split(ENTRY_DELIMITER))
+                .map(entry -> entry.split(KEY_VALUE_DELIMITER))
                 .filter(strings -> strings.length == 2)
                 .collect(Collectors.toMap(strings -> strings[0], strings -> strings[1]));
     }
