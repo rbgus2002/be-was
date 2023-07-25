c2d66d5d67d7b4b9c5bff813c6ca136a0845d92c
Kwon Min A <min2010011@gmail.com>
2023-07-25 11:30:01 +0900

    refactor: 정적 파일 요청과 컨트롤러 요청 처리 분리

--- src/main/java/webserver/http/controller/ControllerResolver.java
+++ src/main/java/webserver/http/controller/ControllerResolver.java
@@ -1,25 +1,50 @@
 package webserver.http.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.nio.file.Files;
 
 import webserver.container.ControllerMapping;
 import webserver.container.UrlMapping;
-import webserver.http.message.HttpMethod;
+import webserver.http.message.HttpRequest;
+import webserver.http.message.HttpResponse;
+import webserver.http.message.HttpStatus;
+import webserver.http.utils.FileMapper;
 
 public class ControllerResolver {
 
 	private final UrlMapping urlMapping = UrlMapping.getInstance();
 	private final ControllerMapping controllerMapping = ControllerMapping.getInstance();
+	private final FileMapper fileMapper = new FileMapper();
 
-	public String resolve(String path, HttpMethod httpMethod) {
+	public void resolve(HttpRequest request, HttpResponse response) {
+		Method method = urlMapping.find(request.getUrlPath(), request.getHttpMethod());
+		if (method == null) {
+			setBadRequest(response);
+			return;
+		}
+
+		Object controller = controllerMapping.find(method.getDeclaringClass());
+		if (controller == null) {
+			setBadRequest(response);
+			return;
+		}
+
+		String fileName = null;
 		try {
-			Method method = urlMapping.find(path, httpMethod);
-			Object controller = controllerMapping.find(method.getDeclaringClass());
-			return (String)method.invoke(controller);
-		} catch (NullPointerException | IllegalArgumentException | IllegalAccessException |
-				 InvocationTargetException e) {
-			return null;
+			fileName = (String)method.invoke(controller, request, response);
+			File file = fileMapper.findFile(fileName);
+			response.setBody(Files.readAllBytes(file.toPath()), Files.probeContentType(file.toPath()));
+		} catch (IllegalAccessException | InvocationTargetException | IOException e) {
+			throw new RuntimeException(e);
 		}
 	}
+
+	private void setBadRequest(HttpResponse response) {
+		response.setStatus(HttpStatus.BAD_REQUEST);
+		response.setBody("test".getBytes(), "text/plain");
+	}
+
 }
--- src/main/java/webserver/http/controller/FrontController.java
+++ src/main/java/webserver/http/controller/FrontController.java
@@ -1,36 +1,27 @@
 package webserver.http.controller;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
+import webserver.container.ControllerScanner;
 import webserver.http.message.HttpRequest;
 import webserver.http.message.HttpResponse;
-import webserver.http.message.HttpStatus;
+import webserver.http.utils.FileMapper;
 
 public class FrontController {
 
 	FileMapper fileMapper = new FileMapper();
+	StaticFileResolver staticFileResolver = new StaticFileResolver();
+	ControllerResolver controllerResolver = new ControllerResolver();
 
 	public void service(HttpRequest request, HttpResponse response) {
-		// 파일 찾기
 		String path = request.getUrlPath();
+		ControllerScanner.initialize();
 		File file = fileMapper.findFile(path);
-		try {
-			if (file == null) {
-				throw new IllegalArgumentException("Wrong path.");
-			}
-			Path filePath = file.toPath();
-			byte[] body = Files.readAllBytes(filePath);
-			// 응답 메세지 작성하기
-			response.writeStatus(HttpStatus.OK);
-			response.writeBody(body, Files.probeContentType(filePath));
-		} catch (IllegalArgumentException | IOException exception) {
-			byte[] body = exception.getMessage().getBytes();
-			response.writeStatus(HttpStatus.NOT_FOUND);
-			response.writeBody(body, "text/plain");
+		if (file != null) {
+			staticFileResolver.resolve(response, file);
+			return;
 		}
+		controllerResolver.resolve(request, response);
 	}
 
 }
