db696842c484e66d5f010d6a556ff8b112bd9836
Kwon Min A <min2010011@gmail.com>
2023-07-25 13:24:44 +0900

    feat: @RequestParam 기반 파라미터 주입 기능 추가

--- src/main/java/webapp/controller/HomeController.java
+++ src/main/java/webapp/controller/HomeController.java
@@ -4,8 +4,8 @@ import webapp.db.Database;
 import webapp.model.User;
 import webserver.annotation.Controller;
 import webserver.annotation.RequestMapping;
+import webserver.annotation.RequestParam;
 import webserver.http.message.HttpMethod;
-import webserver.http.message.HttpRequest;
 import webserver.http.message.HttpResponse;
 import webserver.http.message.HttpStatus;
 
@@ -13,16 +13,17 @@ import webserver.http.message.HttpStatus;
 public class HomeController {
 
 	@RequestMapping(method = HttpMethod.GET, path = "/index")
-	public String home() {
+	public String home(HttpResponse response) {
+		response.setStatus(HttpStatus.OK);
 		return "index.html";
 	}
 
 	@RequestMapping(method = HttpMethod.GET, path = "/user/create")
-	public String join(HttpRequest request, HttpResponse response) {
-		String userId = request.getParam("userId");
-		String password = request.getParam("password");
-		String name = request.getParam("name");
-		String email = request.getParam("email");
+	public String signUp(@RequestParam(name = "userId") String userId,
+		@RequestParam(name = "password") String password,
+		@RequestParam(name = "name") String name,
+		@RequestParam(name = "email") String email,
+		HttpResponse response) {
 		User user = new User(userId, password, name, email);
 		Database.addUser(user);
 		response.setStatus(HttpStatus.CREATED);
--- src/main/java/webserver/http/controller/ControllerResolver.java
+++ src/main/java/webserver/http/controller/ControllerResolver.java
@@ -4,8 +4,13 @@ import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import webserver.annotation.RequestParam;
 import webserver.http.message.HttpRequest;
 import webserver.http.message.HttpResponse;
 import webserver.http.message.HttpStatus;
@@ -34,7 +39,7 @@ public class ControllerResolver {
 
 		String fileName = null;
 		try {
-			fileName = (String)method.invoke(controller, request, response);
+			fileName = invoke(method, controller, request, response);
 			File file = fileMapper.findFile(fileName);
 			response.setBody(Files.readAllBytes(file.toPath()), Files.probeContentType(file.toPath()));
 		} catch (IllegalAccessException | InvocationTargetException | IOException e) {
@@ -45,6 +50,23 @@ public class ControllerResolver {
 	private void setBadRequest(HttpResponse response) {
 		response.setStatus(HttpStatus.BAD_REQUEST);
 		response.setBody("test".getBytes(), "text/plain");
+	}
+
+	private String invoke(Method method, Object controller, HttpRequest request, HttpResponse response) throws
+		InvocationTargetException,
+		IllegalAccessException {
+		Parameter[] parameters = method.getParameters();
+		List<Object> args = Arrays.stream(parameters)
+			.filter(parameter -> parameter.isAnnotationPresent(RequestParam.class))
+			.map(parameter -> parameter.getAnnotation(RequestParam.class))
+			.map(requestParam -> request.getParam(requestParam.name()))
+			.collect(Collectors.toList());
+
+		if (args.contains(null)) {
+			throw new IllegalArgumentException();
+		}
+		args.add(response);
+		return (String)method.invoke(controller, args.toArray());
 	}
 
 }
