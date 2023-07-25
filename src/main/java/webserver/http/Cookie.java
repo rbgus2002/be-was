35d08ed23d81817a8518c6019295438a3f7b7f7b
Hypersand <on8214@gmail.com>
2023-07-25 16:35:34 +0900

    feat: Cookie 클래스 getter 및 팩토리생성자메서드 추가

--- src/main/java/webserver/http/Cookie.java
+++ src/main/java/webserver/http/Cookie.java
@@ -3,16 +3,31 @@ package webserver.http;
 import java.time.LocalDateTime;
 
 public class Cookie {
+    public static final String SESSIONID = "SID";
     private final String name;
     private final String value;
     private LocalDateTime expires;
 
-    public Cookie(String name, String value) {
+    private Cookie(String name, String value) {
         this.name = name;
         this.value = value;
     }
 
+    public String getName() {
+        return name;
+    }
+    public String getValue() {
+        return value;
+    }
+    public LocalDateTime getExpires() {
+        return expires;
+    }
+
     public void setExpires() {
         expires = LocalDateTime.now().plusMinutes(10);
+    }
+
+    public static Cookie create(String value) {
+        return new Cookie(SESSIONID, value);
     }
 }
