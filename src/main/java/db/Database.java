6b0974fa18c8b1df94602de1dc078052b3f553d0
ajsthfldu <ajsthfldu@outlook.com>
2023-07-25 17:06:55 +0900

    refactor: db 맵을 concurrentHashMap으로 변경

--- src/main/java/db/Database.java
+++ src/main/java/db/Database.java
@@ -1,14 +1,16 @@
 package db;
 
-import com.google.common.collect.Maps;
-
 import model.User;
 
 import java.util.Collection;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Database {
-    private static Map<String, User> users = Maps.newHashMap();
+
+    private Database() {}
+
+    private static final Map<String, User> users = new ConcurrentHashMap<>();
 
     public static void addUser(User user) {
         users.put(user.getUserId(), user);
