42e6a66d0e70919a5412f559a166bb1ca8d0590b
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-25 10:40:17 +0900

    fix: 예외의 parameter의 이름 을 resourceName 에서 resourcePath으로 수정

--- src/main/java/exception/path/InvalidResourcePathException.java
+++ src/main/java/exception/path/InvalidResourcePathException.java
@@ -1,7 +1,7 @@
 package exception.path;
 
 public class InvalidResourcePathException extends InvalidPathException {
-    public InvalidResourcePathException(String resourceName) {
-        super(resourceName + "에 리소스 파일이 존재하지 않습니다.");
+    public InvalidResourcePathException(String resourcePath) {
+        super(resourcePath + "에 리소스 파일이 존재하지 않습니다.");
     }
 }
