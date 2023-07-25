f6071e0d17f48a2a0e00953870e6015c2dfda5d5
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-25 21:02:30 +0900

    fix: 예외가 발생하면 CustomException을 이용해 한번에 catch
    - 예상되는 문제 createUser처럼 database에 변경이 일어난 후에 response를 보내는 와중에 예외가 발생하면 데이터의 일관성이 깨진다git statusgit status 이거를 어떻게 해결할 수 있을까?

--- src/main/java/webserver/DispatcherServlet.java
+++ src/main/java/webserver/DispatcherServlet.java
@@ -1,20 +1,26 @@
 package webserver;
 
 import application.controller.WebController;
-import exception.notFound.NotFoundException;
-import exception.badRequest.MissingParameterException;
+import exception.CustomException;
+import exception.internalServerError.MethodAccessException;
+import exception.internalServerError.MethodInvocationException;
 import exception.notFound.InvalidResourcePathException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.response.HttpResponse;
 import webserver.view.view.View;
 import webserver.view.viewResolver.StaticViewResolver;
 import webserver.request.HttpRequest;
 
 import java.io.DataOutputStream;
-import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Map;
 import java.util.Optional;
 
 public class DispatcherServlet {
+
+    private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
 
     private final HttpRequest request;
 
@@ -22,22 +28,41 @@ public class DispatcherServlet {
         this.request = request;
     }
 
-    public void dispatch(final DataOutputStream dos) throws IOException, NotFoundException, MissingParameterException, InvocationTargetException, IllegalAccessException {
-        StaticViewResolver staticViewResolver = new StaticViewResolver();
+    public void dispatch(final DataOutputStream dos) {
 
-        Optional<View> viewOpt = staticViewResolver.resolve(request.getFullPath());
-        if(viewOpt.isPresent()) {
-            viewOpt.get().render(request.getVersion(), request.getContentType(), null, dos);
-            return;
+        try {
+            StaticViewResolver staticViewResolver = new StaticViewResolver();
+            Map<String, Object> model = null;
+
+            Optional<View> viewOpt = staticViewResolver.resolve(request.getFullPath());
+            if(viewOpt.isEmpty()) {
+                ModelAndView modelAndView = invokeControllerMethod();
+                model = modelAndView.getModel();
+                viewOpt = staticViewResolver.resolve(modelAndView.getViewName());
+            }
+
+            if(viewOpt.isEmpty()) throw new InvalidResourcePathException(request.getFullPath());
+
+            viewOpt.get().render(request.getVersion(), request.getContentType(), model, dos);
+        } catch (CustomException e) {
+            logger.debug(e.getMessage());
+            HttpResponse httpResponse = HttpResponse.ofWithStatusOnly(request.getVersion(), e.getHttpStatus());
+            httpResponse.sendResponse(dos);
         }
+    }
 
+    private ModelAndView invokeControllerMethod() {
         ControllerMapper controllerMapper = new ControllerMapper();
         WebController controller = controllerMapper.getController(request);
 
         Method method = controllerMapper.getMethod(controller, request);
-        ModelAndView modelAndView = (ModelAndView) method.invoke(controller, request);
-        viewOpt = staticViewResolver.resolve(modelAndView.getViewName());
-        View view = viewOpt.orElseThrow(() -> new InvalidResourcePathException(request.getFullPath()));
-        view.render(request.getVersion(), request.getContentType(), modelAndView.getModel(), dos);
+
+        try {
+            return (ModelAndView) method.invoke(controller, request);
+        } catch (IllegalAccessException e) {
+            throw new MethodAccessException(method.getName());
+        } catch (InvocationTargetException e) {
+            throw new MethodInvocationException(method.getName());
+        }
     }
 }
