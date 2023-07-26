413b1ac83540a697a3fa0fb62b6b0c2b3fae332d
SeoSiun <siun0816@hanyang.ac.kr>
2023-07-26 21:05:20 +0900

    test: 유저 로그인 관련 테스트코드 추가

--- src/test/java/service/UserServiceTest.java
+++ src/test/java/service/UserServiceTest.java
@@ -1,11 +1,13 @@
 package service;
 
 import db.Database;
+import dto.CreateUserRequestDto;
+import dto.LoginRequestDto;
 import model.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class UserServiceTest {
     @Test
@@ -20,9 +22,63 @@ class UserServiceTest {
         User user = new User(userId, password, name, email);
 
         // when
-        UserService.addUser(user);
+        UserService.createUser(new CreateUserRequestDto(userId, password, name, email));
 
         // then
         assertEquals(user, Database.findUserById(userId));
+    }
+
+    @Test
+    @DisplayName("id, password가 일치하는 User가 로그인하면 true를 리턴한다.")
+    void loginValid() {
+        // given
+        String userId = "userId";
+        String password = "password";
+        String name = "name";
+        String email = "email";
+        User user = new User(userId, password, name, email);
+        UserService.createUser(new CreateUserRequestDto(userId, password, name, email));
+
+        // when
+        boolean isExistUser = UserService.login(new LoginRequestDto(userId, password));
+
+        // then
+        assertTrue(isExistUser);
+    }
+
+    @Test
+    @DisplayName("password가 불일치하는 User가 로그인하면 false를 리턴한다.")
+    void loginWithInvalidPassword() {
+        // given
+        String userId = "userId";
+        String password = "password";
+        String name = "name";
+        String email = "email";
+        User user = new User(userId, password, name, email);
+        UserService.createUser(new CreateUserRequestDto(userId, password, name, email));
+
+        // when
+        boolean isExistUser = UserService.login(new LoginRequestDto(userId, "password1"));
+
+        // then
+        assertFalse(isExistUser);
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 id를 가진 User가 로그인하면 false를 리턴한다.")
+    void loginWithNoExistId() {
+        // given
+        String userId = "userId";
+        String password = "password";
+        String name = "name";
+        String email = "email";
+        User user = new User(userId, password, name, email);
+        UserService.createUser(new CreateUserRequestDto(userId, password, name, email));
+
+        // when
+        boolean isExistUser = UserService.login(new LoginRequestDto("userId1", password));
+
+        // then
+        assertFalse(isExistUser);
     }
 }
