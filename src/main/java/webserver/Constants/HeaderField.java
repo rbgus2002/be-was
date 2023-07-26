20cc1051de60d7d7088c12f4c72ff7a2a919f1f3
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-26 11:02:00 +0900

    fix: redirect를 위한 Location 요소 header field에 추가

--- src/main/java/webserver/Constants/HeaderField.java
+++ src/main/java/webserver/Constants/HeaderField.java
@@ -4,4 +4,5 @@ public class HeaderField {
 
     public static final String contentType = "Content-Type";
     public static final String contentLength = "Content-Length";
+    public static final String Location = "Location";
 }
