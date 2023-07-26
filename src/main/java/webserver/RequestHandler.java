b6a1ecf4cea5c3c719fcb91318e55c8ba551a7bd
Jaehong Kim <kjhonggg95@gmail.com>
2023-07-26 18:11:27 +0900

    refactor: RequestHandler 요청 처리 로직 FrontController 로 위임

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,16 +1,12 @@
 package webserver;
 
 import java.io.*;
-import java.lang.reflect.Method;
 import java.net.Socket;
 
-import handler.HandlerMapping;
-import controller.UserController;
 import http.HttpRequest;
 import http.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import util.Parser;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -26,23 +22,11 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+            FrontController frontController = FrontController.getInstance();
             HttpRequest httpRequest = new HttpRequest(in);
             HttpResponse httpResponse = new HttpResponse();
-            logger.debug("{}", httpRequest);
 
-            // TODO: 여기서부터 httpResponse.response 이전까지의 로직들을 다른 객체가 처리하도록 하면 좋을거같다. Front Controller? Dispatcher?
-            Method method = HandlerMapping.getHandler(httpRequest);
-
-            if (method != null) {
-                String redirectPath = (String) method.invoke(UserController.getInstance(), httpRequest, httpResponse);
-                String responsePath = Parser.parsePathFromRedirect(redirectPath);
-                httpResponse.redirect(responsePath);
-            } else {
-                String requestPath = httpRequest.getPath();
-                httpResponse.ok(requestPath);
-            }
-
-            httpResponse.response(new DataOutputStream(out));
+            frontController.doService(httpRequest, httpResponse, new DataOutputStream(out));
         } catch (Exception e) {
             logger.error(e.getMessage());
         }
