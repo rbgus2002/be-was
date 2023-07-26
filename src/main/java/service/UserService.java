83c36633cb0f55f2ed19cba44d0e87e5a9c38ce5
SeoSiun <siun0816@hanyang.ac.kr>
2023-07-26 21:07:54 +0900

    feat: 로그인 기능 구현을 위해 UserService.login 구현
    findUserById로 유저를 가져와서, 해당 유저가 존재하는지, 존재한다면 패스워드가 일치하는지 여부를 리턴

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,10 +1,19 @@
 package service;
 
 import db.Database;
+import dto.CreateUserRequestDto;
+import dto.LoginRequestDto;
 import model.User;
 
+import java.util.Optional;
+
 public class UserService {
-    public static void addUser(User user) {
-        Database.addUser(user);
+    public static void createUser(CreateUserRequestDto dto) {
+        Database.addUser(dto.toEntity());
+    }
+
+    public static boolean login(LoginRequestDto dto) {
+        Optional<User> findUser = Database.findUserById(dto.getUserId());
+        return findUser.isPresent() && findUser.get().checkPassword(dto.getPassword());
     }
 }
