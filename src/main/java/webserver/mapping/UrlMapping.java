1c9bb2e2104d867399640c8add93c2cf702cb1a7
Kwon Min A <min2010011@gmail.com>
2023-07-26 21:12:03 +0900

    refactor: ControllerMapping 제거 -> MethodHandle 사용
    - 컨트롤러 인스턴스를 생성해서 ControllerMapping에 저장해두지 않고, 컨트롤러 인스턴스를 바인딩한 MethodHandle을 저장하는 방식으로 변경

--- src/main/java/webserver/mapping/UrlMapping.java
+++ src/main/java/webserver/mapping/UrlMapping.java
@@ -1,28 +1,37 @@
 package webserver.mapping;
 
-import java.lang.reflect.Method;
+import java.lang.invoke.MethodHandle;
+import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.Map;
 
 import webserver.http.message.HttpMethod;
+import webserver.http.message.HttpStatus;
 
 public class UrlMapping {
 
-	private final Map<UrlHttpMethodPair, Method> mapping = new HashMap<>();
+	private final Map<HttpMethod, Map<String, MethodHandle>> mapping = new EnumMap<>(HttpMethod.class);
 
 	private UrlMapping() {
+		for (HttpMethod httpMethod : HttpMethod.values()) {
+			mapping.put(httpMethod, new HashMap<>());
+		}
 	}
 
 	public static UrlMapping getInstance() {
 		return LazyHolder.instance;
 	}
 
-	public void add(UrlHttpMethodPair pair, Method method) {
-		mapping.put(pair, method);
+	public void add(HttpMethod httpMethod, String path, MethodHandle methodHandle) {
+		mapping.get(httpMethod).put(path, methodHandle);
 	}
 
-	public Method find(String path, HttpMethod httpMethod) {
-		return mapping.get(UrlHttpMethodPair.of(path, httpMethod));
+	public MethodHandle getMethodHandle(HttpMethod httpMethod, String path) {
+		MethodHandle methodHandle = mapping.get(httpMethod).get(path);
+		if (methodHandle == null) {
+			throw new RuntimeException(HttpStatus.NOT_FOUND.getMessage());
+		}
+		return methodHandle;
 	}
 
 	private static class LazyHolder {
