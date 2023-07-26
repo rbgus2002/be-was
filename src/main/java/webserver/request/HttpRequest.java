794b6d64d7a90bfeb4163c62b3a61b966e07851c
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-26 17:51:54 +0900

    fix: HttpRequest에서 body를 field에 추가하고 parsing 하는 로직 추가

--- src/main/java/webserver/request/HttpRequest.java
+++ src/main/java/webserver/request/HttpRequest.java
@@ -8,12 +8,12 @@ import webserver.Constants.ContentType;
 import webserver.Constants.HttpMethod;
 import webserver.Constants.HttpVersion;
 import webserver.RequestHandler;
+import webserver.Header;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Optional;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
@@ -25,9 +25,10 @@ public class HttpRequest {
     private final HttpVersion version;
     private final RequestPath path;
     private final RequestQuery requestQuery;
-    private final RequestHeader requestHeader;
+    private final Header header;
+    private final RequestBody requestBody;
 
-    private HttpRequest(String requestLine, String header) {
+    private HttpRequest(String requestLine, String header, String body) {
 
         String[] tokens = requestLine.split(" ");
 
@@ -37,7 +38,8 @@ public class HttpRequest {
         this.version = HttpVersion.of(tokens[2]);
         this.path = RequestPath.of(pathAndQueries[0]);
         this.requestQuery = parseRequestQuery(pathAndQueries);
-        this.requestHeader = RequestHeader.of(header);
+        this.header = Header.of(header);
+        this.requestBody = parseRequestBody(body);
     }
 
     public static HttpRequest of(final InputStream in) throws IOException {
@@ -46,21 +48,36 @@ public class HttpRequest {
         String requestLine = line;
         logger.debug("request line: {}", requestLine);
 
+        int contentLength = -1;
+
         StringBuilder header = new StringBuilder();
         while (!line.equals("")) {
             line = br.readLine();
+
+            if(line.startsWith("Content-Length")) contentLength = Integer.parseInt(line.substring(16));
             logger.debug("header: {}", line);
             header.append(line).append(StringUtils.NEWLINE);
         }
 
-        return new HttpRequest(requestLine, header.toString());
+        String body = null;
+        if(contentLength != -1) {
+            char[] buffer = new char[contentLength];
+            br.read(buffer, 0, contentLength);
+            body = new String(buffer, 0, contentLength);
+        }
+
+        return new HttpRequest(requestLine, header.toString(), body);
     }
 
     private RequestQuery parseRequestQuery(final String[] pathAndQueries) {
-        return Optional.of(pathAndQueries)
-                .filter(p -> p.length == 2)
-                .map(p -> RequestQuery.of(p[1]))
-                .orElse(null);
+        if(pathAndQueries.length < 2) return null;
+        return RequestQuery.of(pathAndQueries[1]);
+    }
+
+
+    private RequestBody parseRequestBody(final String body) {
+        if(body == null) return null;
+        return RequestBody.of(body);
     }
 
     public HttpMethod getHttpMethod() {
@@ -85,7 +102,11 @@ public class HttpRequest {
 
     public RequestQuery getRequestQuery() {
         if(requestQuery == null) throw new MissingParameterException();
-
         return requestQuery;
+    }
+
+    public RequestBody getRequestBody() {
+        if(requestBody == null) throw new MissingParameterException();
+        return requestBody;
     }
 }
