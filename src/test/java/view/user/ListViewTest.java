34f24507302ac671b12e4defd1b41a9cac828d23
kwyoohae <gocksdl7979@gmail.com>
2023-07-26 10:24:39 +0900

    test: 로그인을 하지 않는다면 redirect가 되는것을 확인하는 테스트 코드 작성

--- src/test/java/view/user/ListViewTest.java
+++ src/test/java/view/user/ListViewTest.java
@@ -5,6 +5,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import org.assertj.core.api.Assertions;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -15,6 +16,7 @@ import model.User;
 import webserver.request.HttpWasRequest;
 import webserver.response.HttpWasResponse;
 import webserver.session.HttpSession;
+import webserver.utils.HttpStatus;
 
 class ListViewTest {
 
@@ -53,5 +55,26 @@ class ListViewTest {
 		softAssertions.assertThat(outputStream.toString()).as("userId가 제대로 나오는지").contains(user.getUserId());
 		softAssertions.assertThat(outputStream.toString()).as("username이 제대로 나오는지").contains(user.getName());
 		softAssertions.assertThat(outputStream.toString()).as("userEmail이 제대로 나오는지").contains(user.getEmail());
+	}
+
+	@Test
+	@DisplayName("로그인을 하지 않으면 index로 이동해야한다")
+	void notLoginUserIsRedirect() throws IOException {
+		// given
+		String input = "GET /user/list HTTP/1.1\r\n"
+			+ "\r\n";
+		InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+		final HttpWasRequest httpWasRequest = new HttpWasRequest(inputStream);
+		final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		final HttpWasResponse httpWasResponse = new HttpWasResponse(outputStream);
+
+
+
+		// when
+		listView.list(httpWasRequest, httpWasResponse);
+		httpWasResponse.doResponse();
+
+		// then
+		Assertions.assertThat(outputStream.toString()).contains(HttpStatus.FOUND.getName());
 	}
 }
