b7a96db772ed32363f74817d98f529c427060aa1
윤상현 <yshyeon102@gmail.com>
2023-07-26 17:06:57 +0900

    feat: Model이 String타입이 아닌 다른 타입도 저장할 수 있도록 Object타입으로 변경

--- src/main/java/webserver/model/Model.java
+++ src/main/java/webserver/model/Model.java
@@ -4,13 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Model {
-    private final Map<String, String> attributes = new HashMap<>();
+    private final Map<String, Object> attributes = new HashMap<>();
 
-    public void setAttribute(String key, String value) {
+    public void setAttribute(String key, Object value) {
         attributes.put(key, value);
     }
 
-    public String getAttribute(String key) {
+    public Object getAttribute(String key) {
         return attributes.get(key);
     }
 }
