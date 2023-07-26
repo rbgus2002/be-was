77b601c98c8db04dafd4f8ae90f099b29da7bfeb
SyuA0529 <silbi3408@gmail.com>
2023-07-26 11:43:19 +0900

    feat: 클라이언트에게 계시글을 보여주고, 계시글을 전달받기 위해 컨트롤러 추가 및 html 수정

--- src/main/java/controller/StaticController.java
+++ src/main/java/controller/StaticController.java
@@ -1,6 +1,8 @@
 package controller;
 
+import db.PostTable;
 import db.UserTable;
+import dto.PostFrontDto;
 import model.User;
 import webserver.http.request.HttpRequest;
 import webserver.http.response.HttpResponse;
@@ -8,6 +10,9 @@ import webserver.myframework.handler.request.annotation.Controller;
 import webserver.myframework.handler.request.annotation.RequestMapping;
 import webserver.myframework.model.Model;
 import webserver.myframework.session.Session;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SuppressWarnings("unused")
 @Controller
@@ -19,6 +24,13 @@ public class StaticController {
             User user = UserTable.findUserById((String) session.getAttribute("userId"));
             model.addParameter("user", user);
         }
+        model.addParameter("postDtos", getAllPostDtos(model));
         httpResponse.setUri("/index.html");
+    }
+
+    private static List<PostFrontDto> getAllPostDtos(Model model) {
+        return PostTable.findAll().stream()
+                .map(PostFrontDto::new)
+                .collect(Collectors.toList());
     }
 }
