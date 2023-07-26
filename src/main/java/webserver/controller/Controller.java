7c10d22195ba429d31239c4b7b538efeb269f5d5
SeoSiun <siun0816@hanyang.ac.kr>
2023-07-26 22:51:21 +0900

    feat: POST /user/login 요청에 대한 핸들러 메소드 구현
    - UserService.login으로 찾은 유저가 존재한다면(로그인 성공) 세션을 추가하고, SetCookie 설정을 해주고, index.html로 리다이렉트하도록 함.
    
    - 로그인 실패시에는 /user/login_failed.html로 리다이렉트 하도록 함.

--- src/main/java/webserver/controller/Controller.java
+++ src/main/java/webserver/controller/Controller.java
@@ -1,9 +1,13 @@
 package webserver.controller;
 
+import dto.CreateUserRequestDto;
+import dto.LoginRequestDto;
+import model.User;
 import service.FileService;
 import webserver.annotaion.RequestMapping;
 import webserver.http.HttpMethod;
 import webserver.http.HttpStatusCode;
+import webserver.http.Session;
 import webserver.http.request.HttpRequest;
 import webserver.http.response.ResponseBody;
 import webserver.http.response.HttpResponse;
@@ -11,6 +15,8 @@ import service.UserService;
 
 import java.io.IOException;
 import java.nio.file.NoSuchFileException;
+import java.util.Optional;
+import java.util.UUID;
 
 public class Controller {
     private static final Controller controller = new Controller();
@@ -24,8 +30,21 @@ public class Controller {
 
     @RequestMapping(method = HttpMethod.POST, path = "/user/create")
     public HttpResponse createUser(HttpRequest request) {
-        UserService.addUser(request.createUserFromBody());
+        UserService.createUser(new CreateUserRequestDto(request.getParams()));
         return HttpResponse.redirectResponse(request.getVersion(), "/index.html");
+    }
+
+    @RequestMapping(method = HttpMethod.POST, path = "/user/login")
+    public HttpResponse login(HttpRequest request) {
+        Optional<User> user = UserService.login(new LoginRequestDto(request.getParams()));
+        if (user.isPresent()) {
+            HttpResponse response = HttpResponse.redirectResponse(request.getVersion(), "/index.html");
+            String sessionKey = UUID.randomUUID().toString();
+            Session.put(sessionKey, user.get());
+            response.setCookie(sessionKey, "/");
+            return response;
+        }
+        return HttpResponse.redirectResponse(request.getVersion(), "/user/login_failed.html");
     }
 
     public HttpResponse getStaticResource(HttpRequest request) {
