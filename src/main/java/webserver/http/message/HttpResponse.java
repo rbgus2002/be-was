66b5b7025f372c7f1b4e423d91805922676f754b
Kwon Min A <min2010011@gmail.com>
2023-07-26 23:12:18 +0900

    feat: HttpResponseBuilder에 view 필드 추가

--- src/main/java/webserver/http/message/HttpResponse.java
+++ src/main/java/webserver/http/message/HttpResponse.java
@@ -1,58 +1,79 @@
 package webserver.http.message;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import webserver.http.utils.FileMapper;
+
 public class HttpResponse {
 
-	private HttpStatus status;
-	private HttpHeaderFields headerFields;
-	private byte[] body;
+	private final HttpStatus status;
+	private final HttpHeaderFields headerFields;
+	private final byte[] body;
 
-	private HttpResponse(Builder builder) {
+	private HttpResponse(HttpResponseBuilder builder) {
 		this.status = builder.status;
 		this.headerFields = builder.headerFields;
 		this.body = builder.body;
 	}
 
-	public static Builder builder() {
-		return new Builder();
+	public static HttpResponseBuilder builder() {
+		return new HttpResponseBuilder();
 	}
 
-	public static class Builder {
+	public static class HttpResponseBuilder {
 
+		private final HttpHeaderFields headerFields;
 		private HttpStatus status;
-		private HttpHeaderFields headerFields;
 		private byte[] body;
 
-		private Builder() {
-			this.status = HttpStatus.OK;
+		private HttpResponseBuilder() {
+			status = HttpStatus.OK;
 			headerFields = new HttpHeaderFields();
+			body = null;
 			// TODO : 요청의 general header는 응답에도 바로 저장하기
 		}
 
-		public Builder status(HttpStatus status) {
-			this.status = status;
+		public HttpResponseBuilder status(HttpStatus status) {
+			if (status == HttpStatus.OK) {
+				this.status = status;
+			}
 			return this;
 		}
 
-		public Builder headerFields(HttpHeaderFields headerFields) {
-			this.headerFields = headerFields;
+		public HttpResponseBuilder headerField(String key, String value) {
+			if (headerFields.getValue(value) == null) {
+				this.headerFields.add(key, value);
+			}
 			return this;
 		}
 
-		public Builder headerField(String key, String value) {
-			this.headerFields.add(key, value);
-			return this;
+		public HttpResponseBuilder body(Path resourcePath) {
+			if (body != null) {
+				return this;
+			}
+			try {
+				this.body = Files.readAllBytes(resourcePath);
+				this.headerFields.add("Content-Type", Files.probeContentType(resourcePath));
+				this.headerFields.add("Content-Length", String.valueOf(body.length));
+				return this;
+			} catch (IOException e) {
+				this.status = HttpStatus.SERVER_ERROR;
+				return this;
+			}
 		}
 
-		public Builder body(Path resourcePath) throws IOException {
-			byte[] body = Files.readAllBytes(resourcePath);
-			this.body = body;
-			this.headerFields.add("Content-Type", Files.probeContentType(resourcePath));
-			this.headerFields.add("Content-Length", String.valueOf(body.length));
-			return this;
+		public HttpResponseBuilder view(String viewName) {
+			try {
+				File view = FileMapper.findFile(viewName + ".html");
+				return body(view.toPath());
+			} catch (FileNotFoundException e) {
+				this.status = HttpStatus.SERVER_ERROR;
+				return this;
+			}
 		}
 
 		public HttpResponse build() {
