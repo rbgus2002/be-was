450a2f99a4f1d64a3ef4f1cf9ccec54cdaa4fb02
Kwon Min A <min2010011@gmail.com>
2023-07-26 23:14:17 +0900

    refactor: 파일 조회 기능 FileMapper로 분리

--- src/main/java/webserver/http/controller/StaticFileResolver.java
+++ src/main/java/webserver/http/controller/StaticFileResolver.java
@@ -2,38 +2,15 @@ package webserver.http.controller;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import webserver.http.message.HttpResponse;
 import webserver.http.message.HttpStatus;
+import webserver.http.utils.FileMapper;
 
 public class StaticFileResolver {
 
-	private final List<String> resourcePathList;
-
-	public StaticFileResolver() {
-		resourcePathList = new ArrayList<>();
-		resourcePathList.add("src/main/resources/static");
-		resourcePathList.add("src/main/resources/templates");
-	}
-
-	public File findFile(String url) {
-		File file;
-		for (String resourcePath : resourcePathList) {
-			file = new File(resourcePath + url);
-			if (file.exists() && file.isFile()) {
-				return file;
-			}
-		}
-		return null;
-	}
-
 	public HttpResponse resolve(String url) throws IOException {
-		File file = findFile(url);
-		if (file == null) {
-			return null;
-		}
+		File file = FileMapper.findFile(url);
 		return HttpResponse.builder()
 			.status(HttpStatus.OK)
 			.body(file.toPath())
