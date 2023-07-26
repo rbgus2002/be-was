c8772d741d79ca72e3cdf62a8bdff870c5214fa3
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-26 21:50:57 +0900

    fix: HeaderOption에 Cookie 요소 추가

--- src/main/java/webserver/http/Constants/HeaderOption.java
+++ src/main/java/webserver/http/Constants/HeaderOption.java
@@ -5,4 +5,5 @@ public class HeaderOption {
     public static final String CONTENT_TYPE = "Content-Type";
     public static final String CONTENT_LENGTH = "Content-Length";
     public static final String LOCATION = "Location";
+    public static final String COOKIE = "Cookie";
 }
