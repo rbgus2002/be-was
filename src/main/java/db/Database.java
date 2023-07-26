aba4149cdca16128b9c2ba5e3c509824657e4680
Hypersand <on8214@gmail.com>
2023-07-26 16:44:55 +0900

    feat: Database 생성자 private으로 변경

--- src/main/java/db/Database.java
+++ src/main/java/db/Database.java
@@ -7,7 +7,10 @@ import java.util.Collection;
 import java.util.Map;
 
 public class Database {
-    private static Map<String, User> users = Maps.newConcurrentMap();
+    private Database() {
+
+    }
+    private static final Map<String, User> users = Maps.newConcurrentMap();
 
     public static void addUser(User user) {
         users.put(user.getUserId(), user);
