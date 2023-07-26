60b6e6de2ed2ba6eaec8b86862f829f400962b02
Kwon Min A <min2010011@gmail.com>
2023-07-27 03:10:53 +0900

    feat: 회원가입을 처리하는 UserController 추가

--- src/main/java/webapp/controller/HomeController.java
+++ src/main/java/webapp/controller/HomeController.java
@@ -1,13 +1,9 @@
 package webapp.controller;
 
-import webapp.db.Database;
-import webapp.model.User;
 import webserver.annotation.Controller;
-import webserver.annotation.RequestBody;
 import webserver.annotation.RequestMapping;
 import webserver.http.message.HttpMethod;
 import webserver.http.message.HttpResponse;
-import webserver.http.message.HttpStatus;
 
 @Controller
 public class HomeController {
@@ -19,16 +15,4 @@ public class HomeController {
 			.build();
 	}
 
-	@RequestMapping(method = HttpMethod.POST, path = "/user/create")
-	public HttpResponse signUp(@RequestBody(name = "userId") String userId,
-		@RequestBody(name = "password") String password,
-		@RequestBody(name = "name") String name,
-		@RequestBody(name = "email") String email) {
-		User user = new User(userId, password, name, email);
-		Database.addUser(user);
-		return HttpResponse.builder()
-			.status(HttpStatus.CREATED)
-			.view("/user/login")
-			.build();
-	}
 }
---
+++ src/main/java/webapp/controller/UserController.java
@@ -0,0 +1,27 @@
+package webapp.controller;
+
+import webapp.db.Database;
+import webapp.model.User;
+import webserver.annotation.Controller;
+import webserver.annotation.RequestBody;
+import webserver.annotation.RequestMapping;
+import webserver.http.message.HttpMethod;
+import webserver.http.message.HttpResponse;
+import webserver.http.message.HttpStatus;
+
+@Controller
+public class UserController {
+
+	@RequestMapping(method = HttpMethod.POST, path = "/user/create")
+	public HttpResponse signUp(@RequestBody(name = "userId") String userId,
+		@RequestBody(name = "password") String password,
+		@RequestBody(name = "name") String name,
+		@RequestBody(name = "email") String email) {
+		User user = new User(userId, password, name, email);
+		Database.addUser(user);
+		return HttpResponse.builder()
+			.status(HttpStatus.CREATED)
+			.view("/user/login")
+			.build();
+	}
+}
