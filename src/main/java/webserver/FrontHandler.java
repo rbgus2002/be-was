e9a54bf0528f975cbca66a3d13872982930b4e6c
윤상현 <yshyeon102@gmail.com>
2023-07-27 00:39:30 +0900

    refactor: Request에서 쿠키 값을 가져오는 로직을 Request클래스로 이동

--- src/main/java/webserver/FrontHandler.java
+++ src/main/java/webserver/FrontHandler.java
@@ -4,17 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import webserver.handlers.Handler;
 import webserver.http.message.Cookie;
-import webserver.http.message.HttpHeaders;
 import webserver.http.message.HttpRequest;
 import webserver.http.message.HttpResponse;
 import webserver.session.Session;
 import webserver.session.SessionManager;
-
-import java.util.Arrays;
-import java.util.List;
-
-import static webserver.http.message.HttpHeaders.COOKIE;
-import static webserver.http.message.HttpHeaders.SEMI_COLON;
 
 public class FrontHandler {
     public static final Logger logger = LoggerFactory.getLogger(FrontHandler.class);
@@ -34,20 +27,10 @@ public class FrontHandler {
     }
 
     private Session getSession(HttpRequest httpRequest) {
-        Cookie cookie = getCookie(httpRequest);
+        Cookie cookie = httpRequest.getCookie();
 
-        String sessionId = cookie.getSessionValue();
+        String sessionId = cookie.getSessionId();
 
         return sessionManager.getSession(sessionId);
-    }
-
-    private Cookie getCookie(HttpRequest httpRequest) {
-        HttpHeaders headers = httpRequest.getHttpHeaders();
-        if (headers.contains(COOKIE)) {
-            String cookieString = headers.getSingleValue(COOKIE);
-            List<String> cookies = Arrays.asList(cookieString.split(SEMI_COLON));
-            return Cookie.from(cookies);
-        }
-        return Cookie.from(List.of());
     }
 }
