d6187bc4e243c61912ebce7121ab84c5af9aec41
socra167 <socra167@gmail.com>
2023-07-26 19:06:42 +0900

    feat: tr태그의 repeat 속성을 부여해 ModelView의 Attribute의 값을 순회출력할 수 있도록 수정

--- src/main/java/webserver/view/ViewPolicy.java
+++ src/main/java/webserver/view/ViewPolicy.java
@@ -1,5 +1,10 @@
 package webserver.view;
 
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ViewPolicy {
 	/**
 	 * Annotation 메소드 정의로 HTML 뷰의 렌더링 정책을 지정할 수 있다.
@@ -7,7 +12,7 @@ public class ViewPolicy {
 	 * @param model Controller에서 attribute가 추가된 모델
 	 * @return body에서 regex에 해당하는 부분의 대체 결과가 될 문장
 	 */
-	@annotations.ViewPolicy(regex = "<li only_non_user=\"true\">.*<\\/li>")
+	@annotations.ViewPolicy(regex = "<li only_non_user=\"true\">.*?<\\/li>")
 	public String onlyShowForNonUser(String line, ModelView modelView) {
 		if (modelView.containsAttribute("login")) {
 			if (modelView.getAttribute("login").equals("true")) {
@@ -17,7 +22,7 @@ public class ViewPolicy {
 		return line;
 	}
 
-	@annotations.ViewPolicy(regex = "<li only_login_user=\"true\">.*<\\/li>")
+	@annotations.ViewPolicy(regex = "<li only_login_user=\"true\">.*?<\\/li>")
 	public String onlyShowForLoginUser(String line, ModelView modelView) {
 		if (modelView.containsAttribute("login")) {
 			if (modelView.getAttribute("login").equals("true")) {
@@ -25,5 +30,32 @@ public class ViewPolicy {
 			}
 		}
 		return "";
+	}
+
+	@annotations.ViewPolicy(regex = "<tr repeat=\".*\">(.|\\s)*?<\\/tr>")
+	public String repeatForAttributes(String line, ModelView modelView) {
+		Pattern pattern = Pattern.compile("repeat=\"(.*?)\"");
+		Matcher matcher = pattern.matcher(line);
+		String attributeName = "";
+		while (matcher.find()) {
+			attributeName = matcher.group(1);
+		}
+		List<Map<String, String>> attributes = (List<Map<String, String>>)modelView.getAttribute(attributeName);
+
+		StringBuilder result = new StringBuilder();
+		StringBuilder stringBuilder = new StringBuilder();
+		int cnt = 1;
+		for (Map<String, String> attribute : attributes) {
+			stringBuilder.setLength(0);
+			stringBuilder.append(line);
+			String appended = stringBuilder.toString();
+			appended = appended.replaceAll("\\$\\{iter\\}", String.valueOf(cnt));
+			for (String at : attribute.keySet()) {
+				appended = appended.replaceAll("\\$\\{" + at + "\\}", attribute.get(at));
+			}
+			result.append(appended);
+		}
+
+		return result.toString();
 	}
 }
