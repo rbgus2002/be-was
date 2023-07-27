46c00afc1bf868005197b03126ea41e5c5ce6950
Hypersand <on8214@gmail.com>
2023-07-27 15:18:50 +0900

    feat: Model 객체 및 User 객체 null 처리 추가

--- src/main/java/controller/FrontController.java
+++ src/main/java/controller/FrontController.java
@@ -1,71 +1,68 @@
 package controller;
 
+import http.Cookie;
+import http.request.HttpRequest;
+import http.response.HttpResponse;
 import model.Model;
 import model.User;
 import session.Session;
 import session.SessionManager;
-import http.Cookie;
-import http.request.HttpRequest;
-import http.response.HttpResponse;
 
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Optional;
 
 public class FrontController {
     private static final String REDIRECT = "redirect:";
     private static FrontController frontController;
-    private final HttpRequest httpRequest;
-    private final DataOutputStream dos;
 
-    public FrontController(HttpRequest httpRequest, DataOutputStream dos) {
-        this.httpRequest = httpRequest;
-        this.dos = dos;
-    }
+    private FrontController() {}
 
-    public static FrontController getInstance(HttpRequest httpRequest, DataOutputStream dos) {
+    public static FrontController getInstance() {
         if (frontController == null) {
             synchronized(FrontController.class)
             {
-                frontController = new FrontController(httpRequest, dos);
+                frontController = new FrontController();
             }
         }
-
         return frontController;
     }
 
-    public void doDispatch() throws IOException, InvocationTargetException, IllegalAccessException, InstantiationException, NoSuchMethodException {
+    public HttpResponse doDispatch(HttpRequest httpRequest) throws IOException, InvocationTargetException, IllegalAccessException, InstantiationException, NoSuchMethodException {
         if (!ControllerMapper.hasHandler(httpRequest)) {
-            HttpResponse httpResponse = HttpResponse.createStatic(httpRequest.getRequestUri());
-            httpResponse.responseStatic(dos);
-            return;
+            return HttpResponse.createStatic(httpRequest.getRequestUri());
         }
 
         Method method = ControllerMapper.getHandlerMethod(httpRequest);
         ModelAndView modelAndView = ControllerAdapter.runHandlerMethod(method, httpRequest);
 
         if (isRedirect(modelAndView.getViewPath())) {
-            String sessionId = createSession(modelAndView);
-            Session session = SessionManager.getSession(sessionId);
-            Cookie cookie = Cookie.create(sessionId, session.getExpires());
-            HttpResponse httpResponse = HttpResponse.createRedirect(modelAndView.getViewPath(), cookie);
-            httpResponse.responseRedirect(dos);
-            return;
+            Cookie cookie = createCookie(modelAndView);
+            return HttpResponse.createRedirect(modelAndView.getViewPath(), cookie);
         }
 
-        HttpResponse httpResponse = HttpResponse.createDynamic(modelAndView.getViewPath());
-        httpResponse.responseDynamic(dos);
+        return HttpResponse.createDynamic(modelAndView.getViewPath());
     }
 
+
     //TODO: ModelAndView랑 세션 종속성 끊기
-    private String createSession(ModelAndView modelAndView) {
-        Model model = modelAndView.getModel();
+    private Cookie createCookie(ModelAndView modelAndView) {
+        Optional<Model> optionalModel = modelAndView.getModel();
+
+        if (optionalModel.isEmpty()) {
+            return null;
+        }
+
+        Model model = optionalModel.get();
         User user = (User) model.getValue("user");
         if (user == null) {
             return null;
         }
-        return SessionManager.addSession(user);
+
+        String sessionId = SessionManager.addSession(user);
+        Session session = SessionManager.getSession(sessionId);
+        return Cookie.create(sessionId, session.getExpires());
     }
 
     private boolean isRedirect(String viewPath) {
