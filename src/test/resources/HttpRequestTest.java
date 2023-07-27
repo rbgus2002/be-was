9bc48a959343c587fa2a8110c02670c39480eced
jst0951 <jst0951@gmail.com>
2023-07-27 17:24:33 +0900

    test: Http Post 요청 Test 생성

--- src/test/resources/HttpRequestTest.java
+++ src/test/resources/HttpRequestTest.java
@@ -3,11 +3,14 @@ package resources;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Test;
 import webserver.http.model.Request;
+import webserver.http.model.Request.Method;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.Map;
 
 import static webserver.RequestHandler.parseRequest;
+import static webserver.http.HttpParser.parseBodyParameter;
 
 public class HttpRequestTest {
     private String testDirectory = "./src/test/resources/";
@@ -18,10 +21,23 @@ public class HttpRequestTest {
         Request request = parseRequest(in);
 
         SoftAssertions assertions = new SoftAssertions();
-        assertions.assertThat(request.getMethod()).isEqualTo(Request.Method.GET);
+        assertions.assertThat(request.getMethod()).isEqualTo(Method.GET);
         assertions.assertThat(request.getPath()).isEqualTo("/user/create");
         assertions.assertThat(request.getHeader("Connection")).isEqualTo("keep-alive");
         assertions.assertThat(request.getQueryParameter("userId")).isEqualTo("javajigi");
+        assertions.assertAll();
+    }
+    @Test
+    public void request_POST() throws Exception {
+        InputStream in = new FileInputStream(testDirectory + "Http_POST.txt");
+        Request request = parseRequest(in);
+
+        SoftAssertions assertions = new SoftAssertions();
+        assertions.assertThat(request.getMethod()).isEqualTo(Method.POST);
+        assertions.assertThat(request.getPath()).isEqualTo("/user/create");
+        assertions.assertThat(request.getHeader("Connection")).isEqualTo("keep-alive");
+        Map<String, String> parameterMap = parseBodyParameter(request.getBody());
+        assertions.assertThat(parameterMap.get("userId")).isEqualTo("javajigi");
         assertions.assertAll();
     }
 }
