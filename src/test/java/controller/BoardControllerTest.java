e754785ac02b2e431a1e5453b6ded3119532a007
kimahhh <kimahyunn132@gmail.com>
2023-07-27 10:34:38 +0900

    Test: 글쓰기 검증

---
+++ src/test/java/controller/BoardControllerTest.java
@@ -0,0 +1,59 @@
+package controller;
+
+import http.HttpRequest;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static db.BoardDatabase.*;
+import static http.HttpMethod.POST;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardControllerTest {
+
+    BoardController boardController;
+    SoftAssertions softAssertions;
+
+    @BeforeEach
+    void setup() {
+        boardController = BoardController.getInstance();
+        softAssertions = new SoftAssertions();
+    }
+
+    @AfterEach
+    void after() {
+        softAssertions.assertAll();
+    }
+
+    @Test
+    @DisplayName("글 작성 시, Database에 정보가 추가되어야 한다")
+    void createBoardTest() {
+        // Given
+        clearBoards();
+        String uri = "/qna/create";
+        String body = "writer=kimahhh&title=제목&contents=본문";
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Cookie", "Idea-b0842ea5=7bc847be-5914-47b7-be63-da8616787eb6; SID=4bc504ae-a64f-4fba-a3df-4466c012915a");
+        HttpRequest httpRequest = new HttpRequest.RequestBuilder(POST, uri, "HTTP/1.1")
+                .setBody(body)
+                .setHeader(headers)
+                .build();
+
+        // When
+        boardController.loadFileByRequest(httpRequest);
+
+        // Then
+        softAssertions.assertThat(findAllBoards().size())
+                .as("Database의 크기가 1이 아닙니다.")
+                .isEqualTo(1);
+        softAssertions.assertThat(findBoardById((long) 0).getTitle())
+                .as("Id로 Board를 찾을 수 없습니다.")
+                .isEqualTo("제목");
+
+    }
+}
---
+++ src/test/java/service/BoardServiceTest.java
@@ -0,0 +1,69 @@
+package service;
+
+import model.Board;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static db.BoardDatabase.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardServiceTest {
+    BoardService boardService;
+    SoftAssertions softAssertions;
+
+    @BeforeEach
+    void setup() {
+        boardService = new BoardService();
+        softAssertions = new SoftAssertions();
+        clearBoards();
+    }
+
+    @AfterEach
+    void after() {
+        softAssertions.assertAll();
+    }
+
+    @Test
+    @DisplayName("Board를 생성해서 DB에 저장할 수 있어야 한다")
+    void createBoardTest() {
+        // Given
+        Map<String, String> board1 = new HashMap<>();
+        board1.put("writer", "kimahhh");
+        board1.put("title", "제목");
+        board1.put("contents", "본문");
+
+        // When
+        boardService.createBoard(board1);
+
+        // Then
+        softAssertions.assertThat(findAllBoards().size())
+                .as("Database의 크기가 1이 아닙니다.")
+                .isEqualTo(1);
+        softAssertions.assertThat(findBoardById((long) 0).getTitle())
+                .as("Id로 Board를 찾을 수 없습니다.")
+                .isEqualTo("제목");
+
+        // Given
+        Map<String, String> board2 = new HashMap<>();
+        board2.put("writer", "gildong");
+        board2.put("title", "내 이름이 뭘까요");
+        board2.put("contents", "내 이름은 홍길동");
+
+        // When
+        boardService.createBoard(board2);
+
+        // Then
+        softAssertions.assertThat(findAllBoards().size())
+                .as("Database의 크기가 2가 아닙니다.")
+                .isEqualTo(2);
+        softAssertions.assertThat(findBoardById((long) 1).getWriter())
+                .as("Id로 Board를 찾을 수 없습니다.")
+                .isEqualTo("gildong");
+    }
+}
