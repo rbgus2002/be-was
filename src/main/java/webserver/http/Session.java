7d20472ad0e084e579bfd2e779e859871ddc2152
SeoSiun <siun0816@hanyang.ac.kr>
2023-07-27 20:19:30 +0900

    feat: 랜덤한 키를 생성하여 key, value를 저장하는 Session.addSession 메소드 구현

--- src/main/java/webserver/http/Session.java
+++ src/main/java/webserver/http/Session.java
@@ -3,12 +3,15 @@ package webserver.http;
 import com.google.common.collect.Maps;
 
 import java.util.Map;
+import java.util.UUID;
 
 public class Session {
     private static final Map<String, Object> sessionStorage = Maps.newConcurrentMap();
 
-    public static void put(String key, Object value) {
+    public static String addSession(Object value) {
+        String key = UUID.randomUUID().toString();
         sessionStorage.put(key, value);
+        return key;
     }
 
     public static Object get(String key) {
