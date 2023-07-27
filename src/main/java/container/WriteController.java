fbc5638f19aff3db6f63c98ce29f77535b1f689a
dydwo0740 <dydwo0740@naver.com>
2023-07-27 10:37:54 +0900

    글쓰기 내용을 저장하기 위한 데이터베이스 생성

--- src/main/java/container/WriteController.java
+++ src/main/java/container/WriteController.java
@@ -1,12 +1,28 @@
 package container;
 
 import annotation.RequestMapping;
+import db.SessionManager;
+import util.PathList;
 import webserver.HTTPServletRequest;
 import webserver.HTTPServletResponse;
+
+import static util.PathList.*;
+
 @RequestMapping(path = "/qna/form")
 public class WriteController implements Controller{
     @Override
     public String process(HTTPServletRequest request, HTTPServletResponse response) {
-        return null;
+        if (request.getMethod().equals("POST")) {
+            return HOME_PATH.getPath();
+        }
+
+        if (isLogInStatus(request)) {
+           return "/qna/form.html";
+        }
+        return "/user/login.html";
+    }
+
+    private boolean isLogInStatus(HTTPServletRequest request) {
+        return SessionManager.getSession(request) != null;
     }
 }
