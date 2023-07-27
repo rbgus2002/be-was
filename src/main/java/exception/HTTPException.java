cf2d2703410efa5235283003192ce701a89785d7
jst0951 <jst0951@gmail.com>
2023-07-27 11:02:26 +0900

    bugfix: content type 헤더에 설정하여 올바르게 표시되도록 수정

--- src/main/java/exception/HTTPException.java
+++ src/main/java/exception/HTTPException.java
@@ -6,7 +6,8 @@ import webserver.http.model.Response;
 import java.util.HashMap;
 import java.util.Map;
 
-import static webserver.http.HttpUtil.HEADER_CONTENT_LENGTH;
+import static webserver.http.HttpUtil.*;
+import static webserver.http.HttpUtil.MIME.HTML;
 
 public abstract class HTTPException extends RuntimeException {
     private final STATUS status;
@@ -25,6 +26,7 @@ public abstract class HTTPException extends RuntimeException {
         byte[] body = errorMessage.getBytes();
 
         Map<String, String> headerMap = new HashMap<>();
+        headerMap.put(HEADER_CONTENT_TYPE, HTML.getMime() + HEADER_CHARSET);
         headerMap.put(HEADER_CONTENT_LENGTH, String.valueOf(body.length));
         return new Response(status, headerMap, body);
     }
