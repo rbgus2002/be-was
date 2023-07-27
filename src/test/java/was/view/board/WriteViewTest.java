2f6b5ccf11f91e8f2d25f4ab604421de82165a63
kwyoohae <gocksdl7979@gmail.com>
2023-07-27 17:07:00 +0900

    test: 로그인 하지 않는다면 write 페이지로 못가도록 확인하는 테스트 작성

---
+++ src/test/java/was/view/board/WriteViewTest.java
@@ -0,0 +1,41 @@
+package was.view.board;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import was.webserver.request.HttpWasRequest;
+import was.webserver.response.HttpWasResponse;
+import was.webserver.utils.HttpStatus;
+
+class WriteViewTest {
+
+	private final WriteView writeView = new WriteView();
+
+	@Test
+	@DisplayName("로그인 하지 않으면 write가 아닌 로그인 페이지로 이동한다")
+	void wirteMustUsingLogin() throws IOException {
+		// given
+		String input = "GET /board HTTP/1.1\r\n"
+			+ "\r\n";
+		InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+		final HttpWasRequest httpWasRequest = new HttpWasRequest(inputStream);
+		final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		final HttpWasResponse httpWasResponse = new HttpWasResponse(outputStream);
+
+		// when
+		writeView.write(httpWasRequest, httpWasResponse);
+		httpWasResponse.doResponse();
+
+		// then
+		Assertions.assertThat(outputStream.toString()).contains(HttpStatus.FOUND.getName());
+		Assertions.assertThat(outputStream.toString()).contains("/login");
+	}
+}
