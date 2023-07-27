c62d7ebdab4db308865ea6bbfa666c982b602d4c
jst0951 <jst0951@gmail.com>
2023-07-27 11:12:13 +0900

    feat: 500 에러 Exception 생성

--- src/main/java/webserver/http/HttpUtil.java
+++ src/main/java/webserver/http/HttpUtil.java
@@ -45,7 +45,8 @@ public class HttpUtil {
         PERMANENT_REDIRECT(308, "Permanent Redirect"),
         BAD_REQUEST(400, "Bad Request"),
         UNAUTHORIZED(401, "Unauthorized"),
-        NOT_FOUND(404, "Not Found");
+        NOT_FOUND(404, "Not Found"),
+        INTERNAL_SERVER_ERROR(500, "Internal Server Error");
 
         private final int statusCode;
         private final String statusMessage;
