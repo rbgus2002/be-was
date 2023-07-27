6886f2b72abfee4ed7d2a2fe284615f21ac8f061
SeoSiun <siun0816@hanyang.ac.kr>
2023-07-27 20:33:55 +0900

    feat: 동적 html이 필요한 경우를 처리하기 위해 경로, 클래스 맵 추가
    - 동적 html이 필요하다면 해당 클래스의 render 메소드를 호출하도록 함.

--- src/main/java/service/FileService.java
+++ src/main/java/service/FileService.java
@@ -1,24 +1,51 @@
 package service;
 
+import model.User;
+import service.html.*;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class FileService {
     private static final List<String> PATH_LIST = new ArrayList<>();
+    private static final Map<String, Class> AUTH_PATH_MAP = new HashMap<>();
 
-    private FileService(){}
+    private FileService() {
+    }
 
     static {
         PATH_LIST.add("src/main/resources/templates");
         PATH_LIST.add("src/main/resources/static");
+
+        AUTH_PATH_MAP.put("/index.html", IndexHtmlService.class);
+        AUTH_PATH_MAP.put("/user/form.html", UserFormHtmlService.class);
+        AUTH_PATH_MAP.put("/user/list.html", UserListHtmlService.class);
+        AUTH_PATH_MAP.put("/user/login.html", UserLoginHtmlService.class);
+        AUTH_PATH_MAP.put("/user/login_failed.html", UserLoginFailedHtmlService.class);
+    }
+
+    public static byte[] getStaticResource(String path, User sessionUser) throws IOException, InstantiationException, IllegalAccessException {
+        for (String rootPath : PATH_LIST) {
+            File file = new File(rootPath + path);
+            if (file.exists() && file.isFile()) {
+                if (AUTH_PATH_MAP.containsKey(path)) {
+                    HtmlService htmlService = (HtmlService) AUTH_PATH_MAP.get(path).newInstance();
+                    return htmlService.render(sessionUser);
+                }
+                return Files.readAllBytes(file.toPath());
+            }
+        }
+        throw new NoSuchFileException(path);
     }
 
     public static byte[] getStaticResource(String path) throws IOException {
-        for (String rootPath: PATH_LIST) {
+        for (String rootPath : PATH_LIST) {
             File file = new File(rootPath + path);
             if (file.exists() && file.isFile()) {
                 return Files.readAllBytes(file.toPath());
