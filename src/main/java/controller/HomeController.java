6eca118ebae9b9f963b55271d5732173f6db8046
Hypersand <on8214@gmail.com>
2023-07-27 21:01:36 +0900

    feat: 동적인 뷰의 viewPath는 .html을 제거하고 반환

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -15,12 +15,12 @@ public class HomeController {
     public ModelAndView showHome(HttpRequest httpRequest) {
         User user = SessionManager.getUser(httpRequest.getSessionId());
         if (user == null) {
-            return new ModelAndView("/index.html");
+            return new ModelAndView("/index");
         }
 
         Model model = new Model();
         model.addAttribute("user", user);
 
-        return new ModelAndView("/index.html", model);
+        return new ModelAndView("/index", model);
     }
 }
--- src/main/java/http/Path.java
+++ src/main/java/http/Path.java
@@ -2,7 +2,8 @@ package http;
 
 public enum Path {
     STATIC("src/main/resources/static"),
-    TEMPLATES("src/main/resources/templates");
+    TEMPLATES("src/main/resources/templates"),
+    DYNAMIC("src/main/java/view");
 
     private final String path;
 
