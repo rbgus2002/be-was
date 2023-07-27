57cd7174c033671f49a0d93cc6ff9aa346af17ca
jst0951 <jst0951@gmail.com>
2023-07-27 19:42:23 +0900

    test: index.html 리다이렉트에 대한 테스트 추가

--- src/test/resources/HttpRequestTest.java
+++ src/test/resources/HttpRequestTest.java
@@ -2,11 +2,20 @@ package resources;
 
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Test;
+import router.Router;
+import webserver.http.HttpUtil;
 import webserver.http.model.Request;
 import webserver.http.model.Request.Method;
+import webserver.http.model.Response;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static webserver.http.HttpUtil.HEADER_REDIRECT_LOCATION;
 
 public class HttpRequestTest {
     private String testDirectory = "./src/test/resources/";
@@ -23,6 +32,7 @@ public class HttpRequestTest {
         assertions.assertThat(request.getParameter("userId")).isEqualTo("javajigi");
         assertions.assertAll();
     }
+
     @Test
     public void request_POST() throws Exception {
         InputStream in = new FileInputStream(testDirectory + "Http_POST.txt");
@@ -48,5 +58,24 @@ public class HttpRequestTest {
         assertions.assertThat(request.getParameter("id")).isEqualTo("1");
         assertions.assertThat(request.getParameter("userId")).isEqualTo("javajigi");
         assertions.assertAll();
+    }
+
+    @Test
+    public void responseForward() throws Exception {
+        // Given
+        Map<String, String> headerMap = new HashMap<>();
+        headerMap.put(HEADER_REDIRECT_LOCATION, "/index.html");
+        Response response = new Response(HttpUtil.STATUS.SEE_OTHER, headerMap, null);
+
+        // When
+        response.sendResponse(createOutputStream("HTTP_Forward.txt"));
+
+        // Then
+        String body = new String(Files.readAllBytes(Paths.get(testDirectory + "HTTP_Forward.txt")));
+        assertThat(body).contains("/index.html");
+    }
+
+    private OutputStream createOutputStream(String filename) throws FileNotFoundException {
+        return new FileOutputStream(testDirectory + filename);
     }
 }
