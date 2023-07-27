fd02e2e03d91bbbbd0c4d751e2f99c76e637646a
socra167 <socra167@gmail.com>
2023-07-27 17:53:44 +0900

    test: 중복 회원가입 시 예외발생을 확인하던 테스트를 리다이렉트를 확인하는 것으로 변경

--- src/test/java/controllers/ControllerTest.java
+++ src/test/java/controllers/ControllerTest.java
@@ -40,35 +40,25 @@ class ControllerTest {
 		void registerUser() {
 			final HttpParameter httpParameter = newParameter("testID", "testPassword", "testName", "test@email.com");
 
-			controller.createUser(new HttpRequest(httpParameter), new HttpResponse(),  ModelView.from(null));
+			controller.createUser(new HttpRequest(httpParameter), new HttpResponse(), ModelView.from(null));
 
 			verifyRegister(httpParameter);
 		}
 
 		@Test
-		@DisplayName("이미 데이터베이스에 존재하는 userId로 가입하려고 하면 예외가 발생하고 저장되지 않아야 한다")
+		@DisplayName("이미 데이터베이스에 존재하는 userId로 가입하려고 하면 회원가입 페이지로 리다이렉트되어야 한다")
 		void duplicateRegistration() {
 			final String userId = "testID";
 			final String password = "testPassword";
 			final String name = "testName";
 			final String email = "test@email.com";
 			final User existingUser = setUpUser(userId, password, name, email);
+			final String expectedPath = "redirect:/user/form.html";
 			HttpParameter httpParameter = newParameter("testID", "testPassword2", "testName2", "test2@email.com");
 
-			SoftAssertions softAssertions = new SoftAssertions();
-			softAssertions.assertThatThrownBy(() -> {
-				controller.createUser(new HttpRequest(httpParameter), new HttpResponse(), ModelView.from(null));
-			}).isInstanceOf(Exception.class).hasMessage(UserDatabase.USERID_ALREADY_EXISTS_MESSAGE);
-
-			softAssertions.assertThat(UserDatabase.findUserById(existingUser.getUserId()).getPassword())
-				.isEqualTo(existingUser.getPassword());
-			softAssertions.assertThat(UserDatabase.findUserById(existingUser.getUserId()).getName())
-				.isEqualTo(existingUser.getName());
-			softAssertions.assertThat(UserDatabase.findUserById(existingUser.getUserId()).getEmail())
-				.isEqualTo(existingUser.getEmail());
-			softAssertions.assertThat(UserDatabase.findAll().size()).isEqualTo(1);
-
-			softAssertions.assertAll();
+			ModelView modelView = controller.createUser(new HttpRequest(httpParameter), new HttpResponse(),
+				ModelView.from(null));
+			assertThat(modelView.getPath()).isEqualTo(expectedPath);
 		}
 
 		private User setUpUser(final String userId, final String password, final String name, final String email) {
@@ -102,7 +92,8 @@ class ControllerTest {
 		@DisplayName("회원가입 성공 이후 메인 페이지로 리다이렉트 되어야 한다")
 		void sendRedirect() {
 			final HttpParameter httpParameter = newParameter("testID", "testPassword", "testName", "test@email.com");
-			ModelView modelView = controller.createUser(new HttpRequest(httpParameter), new HttpResponse(), ModelView.from(null));
+			ModelView modelView = controller.createUser(new HttpRequest(httpParameter), new HttpResponse(),
+				ModelView.from(null));
 			assertThat(modelView.getPath()).isEqualTo("redirect:/");
 		}
 
