23b1e335f3d905f8890ad76c70ce32f24d87c833
Hypersand <on8214@gmail.com>
2023-07-27 15:27:08 +0900

    feat: HttpResponse의 역할 분리

--- src/main/java/http/response/HttpResponse.java
+++ src/main/java/http/response/HttpResponse.java
@@ -1,74 +1,37 @@
 package http.response;
 
 import http.Cookie;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import utils.Parser;
 import http.HttpHeaders;
+import utils.Parser;
 
-import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
+import static http.response.ResponseType.*;
+
 public class HttpResponse {
-    private static final Logger logger = LoggerFactory.getLogger(HttpResponse.class);
     private final HttpStatusLine httpStatusLine;
     private final HttpHeaders httpHeaders;
     private final byte[] body;
+    private final ResponseType responseType;
 
-    private HttpResponse(HttpStatusLine httpStatusLine, HttpHeaders httpHeaders, byte[] body) {
+    private HttpResponse(HttpStatusLine httpStatusLine, HttpHeaders httpHeaders, byte[] body, ResponseType responseType) {
         this.httpStatusLine = httpStatusLine;
         this.httpHeaders = httpHeaders;
         this.body = body;
+        this.responseType = responseType;
+    }
+    public byte[] getBody() {
+        return body;
     }
 
-    public void responseStatic(DataOutputStream dos) {
-        response200Header(dos);
-        responseBody(dos);
-    }
-
-    public void responseDynamic(DataOutputStream dos) {
-        response200Header(dos);
-        responseBody(dos);
-    }
-
-    public void responseRedirect(DataOutputStream dos) {
-        response302Header(dos);
-    }
-
-    //TODO: dos 하드코딩 느낌으로 쓰는 것 보다는 좀 더 유연성있게 만들기
-    private void response200Header(DataOutputStream dos) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: " + httpHeaders.getContentType() +  "\r\n");
-            dos.writeBytes("Content-Length: " + body.length + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void response302Header(DataOutputStream dos) {
-        try {
-            dos.writeBytes("HTTP/1.1 302 Found \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: 0" + "\r\n");
-            dos.writeBytes("Location: " + httpHeaders.getLocation() + "\r\n");
-            dos.writeBytes("Set-Cookie: " + httpHeaders.getResponseCookie() + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void responseBody(DataOutputStream dos) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
+    public String writeHttpResponse() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(httpStatusLine.writeHttpStatusLine());
+        sb.append(httpHeaders.writeHttpHeaders(responseType));
+        sb.append("\r\n");
+        return sb.toString();
     }
 
     public static HttpResponse createStatic(String requestUri) throws IOException {
@@ -76,7 +39,8 @@ public class HttpResponse {
         return new HttpResponse(
                 HttpStatusLine.createStaticStatusLine(),
                 HttpHeaders.createStaticStatusHeaders(body.length, requestUri),
-                body
+                body,
+                STATIC
         );
     }
 
@@ -85,7 +49,8 @@ public class HttpResponse {
         return new HttpResponse(
                 HttpStatusLine.createRedirectStatusLine(),
                 HttpHeaders.createRedirectStatusHeaders(viewPath, cookie),
-                emptyBody
+                emptyBody,
+                REDIRECT
         );
     }
 
@@ -94,7 +59,8 @@ public class HttpResponse {
         return new HttpResponse(
                 HttpStatusLine.createStaticStatusLine(),
                 HttpHeaders.createStaticStatusHeaders(body.length, viewPath),
-                body
+                body,
+                DYNAMIC
         );
     }
 }
