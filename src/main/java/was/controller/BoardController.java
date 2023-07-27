f20327a2af494774dfafd69e2edc540c957e50d6
kwyoohae <gocksdl7979@gmail.com>
2023-07-27 11:04:32 +0900

    feat: 글쓰기 로직 작성

---
+++ src/main/java/was/controller/BoardController.java
@@ -0,0 +1,28 @@
+package was.controller;
+
+import was.controller.annotation.RequestMapping;
+import was.db.Database;
+import was.model.Board;
+import was.webserver.annotation.Controller;
+import was.webserver.request.HttpWasRequest;
+import was.webserver.response.HttpWasResponse;
+import was.webserver.utils.HttpHeader;
+import was.webserver.utils.HttpMethod;
+import was.webserver.utils.HttpStatus;
+
+@Controller
+public class BoardController {
+
+	@RequestMapping(method = HttpMethod.POST, path = "/post")
+	public void writeBoard(HttpWasRequest request, HttpWasResponse response) {
+		final String writer = request.getParameter("writer");
+		final String title = request.getParameter("title");
+		final String contents = request.getParameter("contents");
+		final Board board = new Board(writer, title, contents);
+
+		Database.addBoard(board);
+
+		response.setHttpStatus(HttpStatus.FOUND);
+		response.addHeader(HttpHeader.LOCATION, "http://localhost:8080/index.html");
+	}
+}
